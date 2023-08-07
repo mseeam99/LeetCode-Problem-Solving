faa51e1a81a21389f3c9febef622cec3a407ecf1
Mohammad Seeam <112004640+mseeam99@users.noreply.github.com>
2023-08-07 22:35:32 +0600

    Time: 97 ms, Memory: 15.4 MB - LeetHub

---
+++ single-number/single-number.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        nums.sort()
+        length = len(nums)
+        
+        if length==1:
+            return nums[0]
+        else:
+            if nums[0] != nums[1]:
+                return nums[0]
+            elif nums[length - 1] != nums[length - 2]:
+                return nums[length - 1]
+
+            for i in range(1, length - 1):
+                if nums[i] != nums[i - 1] and nums[i] != nums[i + 1]:
+                    return nums[i]
+            
+        
+        
+       
+        
+        # If the single number is at the end of the array
+        return nums[length - 1]
