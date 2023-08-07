b094ba18878b2e94c1559c948ecc6cf861ec2154
Mohammad Seeam <112004640+mseeam99@users.noreply.github.com>
2023-08-07 22:39:04 +0600

    Time: 102 ms, Memory: 15.5 MB - LeetHub

--- single-number/single-number.py
+++ single-number/single-number.py
@@ -5,16 +5,16 @@ class Solution(object):
         length = len(nums)
         
         if length==1:
-            return nums[0]
+            return nums[0];
         else:
             if nums[0] != nums[1]:
-                return nums[0]
+                return nums[0];
             elif nums[length - 1] != nums[length - 2]:
-                return nums[length - 1]
+                return nums[length - 1];
 
             for i in range(1, length - 2):
                 if nums[i] != nums[i-1] and nums[i] != nums[i+1]:
-                    return nums[i]
+                    return nums[i];
              
         
        
