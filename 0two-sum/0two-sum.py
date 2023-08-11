7daf6aa2d0eb91ec6415f85e580e67fec60529ea
Mohammad Seeam <112004640+mseeam99@users.noreply.github.com>
2023-08-12 04:29:05 +0600

    Time: 2184 ms, Memory: 14.3 MB - LeetHub

--- 0two-sum/0two-sum.py
+++ 0two-sum/0two-sum.py
@@ -6,3 +6,8 @@ class Solution(object):
             for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i, j]
+                
+                
+
+
+
