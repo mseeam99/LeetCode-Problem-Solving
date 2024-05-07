f4298196718e493b5b314d12130442b2f28c43c3
Mohammad Seeam <112004640+mseeam99@users.noreply.github.com>
2024-05-07 17:39:22 -0500

    Time: 58 ms (55.27%), Space: 17.6 MB (50.67%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,18 +1,15 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         profit = 0
-        left  = 0
-        right = left + 1
-
-        while right < len(prices):
-            if prices[right] < prices[left]:
-                left  += 1
-                right = left + 1
-            elif prices[right] >= prices[left]:
-                tempDifference = prices[right] - prices[left]
+        leftPointer = 0
+        rightPointer = leftPointer + 1
+        while rightPointer < len(prices):
+            if prices[leftPointer] <= prices[rightPointer]:
+                tempDifference = prices[rightPointer] - prices[leftPointer]
                 profit += tempDifference
-                left  += 1
-                right = left + 1
-
+                leftPointer += 1
+                rightPointer = leftPointer + 1
+            elif prices[leftPointer] > prices[rightPointer]:
+                leftPointer += 1
+                rightPointer = leftPointer + 1
         return profit
-        
