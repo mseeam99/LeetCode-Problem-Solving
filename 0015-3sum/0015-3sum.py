6d8fabc25353bfd7dafdc1922d9e33621d42d4ca
Mohammad Seeam <112004640+mseeam99@users.noreply.github.com>
2024-05-08 18:34:38 -0500

    Time: 1142 ms (27.23%), Space: 19.9 MB (98.64%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,7 +1,33 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        '''
         answer = set()
+        for i in range(len(nums)-2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            leftPointer = i+1
+            rightPointer = leftPointer+1
+            while leftPointer < len(nums)-1:
+                value = nums[i] + nums[leftPointer] + nums[rightPointer]
+                if value == 0:
+                    answer.add(tuple(sorted([nums[i],nums[leftPointer], nums[rightPointer]])))
+                    if rightPointer != len(nums)-1:
+                        rightPointer+=1
+                    elif rightPointer == len(nums)-1:
+                        leftPointer +=1
+                        rightPointer = leftPointer+1
+                else:
+                    if rightPointer != len(nums)-1:
+                        rightPointer+=1
+                    elif rightPointer == len(nums)-1:
+                        leftPointer +=1
+                        rightPointer = leftPointer+1
+        answer = list(answer)
+        return answer
+        '''
+        
         nums.sort()
+        answer = set()
         for i in range(len(nums)):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
@@ -13,10 +39,8 @@ class Solution:
                     leftPointer+=1
                     rightPointer-=1
                 elif nums[i] + nums[leftPointer] + nums[rightPointer] < 0:
-                    leftPointer += 1
+                    leftPointer+=1
                 elif nums[i] + nums[leftPointer] + nums[rightPointer] > 0:
-                    rightPointer -= 1
+                    rightPointer-=1
         answer = list(answer)
         return answer
-                
-        
