a6a673934e187acb2f758cf11584658902cb809e
Mohammad Seeam <112004640+mseeam99@users.noreply.github.com>
2024-05-18 23:50:23 -0500

    Time: 40 ms (25.53%), Space: 16.7 MB (12.03%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -3,7 +3,7 @@ class Solution:
         bigPointer = 0
         smallPointer = 0
         while bigPointer < len(t) and smallPointer<len(s):
-            if smallPointer == len(s): return True
+            if smallPointer == len(s): break
             if t[bigPointer] == s[smallPointer]:
                 bigPointer+=1
                 smallPointer+=1
