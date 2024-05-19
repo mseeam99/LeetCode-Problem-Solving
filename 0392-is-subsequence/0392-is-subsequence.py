7c1f5b448afeac3210184533fae6f271727ccf0b
Mohammad Seeam <112004640+mseeam99@users.noreply.github.com>
2024-05-18 23:50:47 -0500

    Time: 34 ms (70.86%), Space: 16.7 MB (40.09%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -2,6 +2,7 @@ class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
         bigPointer = 0
         smallPointer = 0
+        
         while bigPointer < len(t) and smallPointer<len(s):
             if smallPointer == len(s): break
             if t[bigPointer] == s[smallPointer]:
@@ -9,5 +10,6 @@ class Solution:
                 smallPointer+=1
             else:
                 bigPointer+=1
+
         if smallPointer == len(s): return True
         else: return False
