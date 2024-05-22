935eca89f4cb82e516e7a14ce9526bdf34864fd4
Mohammad Seeam <112004640+mseeam99@users.noreply.github.com>
2024-05-22 17:50:26 -0500

    Time: 46 ms (9.36%), Space: 16.5 MB (91.11%) - LeetHub

---
+++ 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        hashMap = {
+            'b': 0,
+            'a': 0,
+            'l': 0,
+            'o': 0,
+            'n': 0
+        }
+        for char in text:
+            if char in hashMap:
+                hashMap[char] += 1
+
+        hashMap['l'] //= 2  
+        hashMap['o'] //= 2  
+
+        return min(hashMap.values())
