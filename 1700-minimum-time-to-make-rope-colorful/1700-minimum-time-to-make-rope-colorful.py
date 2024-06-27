e0bdc3eb744991fccc826e38143d19c9affa4c71
Mohammad Seeam <112004640+mseeam99@users.noreply.github.com>
2024-06-27 12:45:49 -0500

    Time: 881 ms (13.55%), Space: 27.3 MB (49.44%) - LeetHub

--- 1700-minimum-time-to-make-rope-colorful/1700-minimum-time-to-make-rope-colorful.py
+++ 1700-minimum-time-to-make-rope-colorful/1700-minimum-time-to-make-rope-colorful.py
@@ -1,21 +1,20 @@
 class Solution:
     def minCost(self, colors: str, neededTime: List[int]) -> int:
-        answer = 0
-        i = 0
-        while i < len(colors):
-            current_char = colors[i]
+
+        answer  = 0
+        pointer = 0
+
+        while pointer < len(colors):
+            tempColor  = colors[pointer]
+            max_time   = 0
             total_time = 0
-            max_time = 0
-            while i < len(colors) and colors[i] == current_char:
-                total_time += neededTime[i]
-                max_time = max(max_time, neededTime[i])
-                i += 1
-            answer += total_time - max_time
+            while pointer < len(colors) and colors[pointer] == tempColor:
+                total_time += neededTime[pointer]
+                max_time = max(max_time,neededTime[pointer])
+                pointer+=1
+            answer += (total_time-max_time)
         return answer
 
+        
 
-
-
-
-
-
+        
