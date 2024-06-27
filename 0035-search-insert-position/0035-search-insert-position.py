d156bd6f373fece3117ad57ba0bae8c25abbf252
Mohammad Seeam <112004640+mseeam99@users.noreply.github.com>
2024-06-27 13:03:50 -0500

    Time: 51 ms (47.35%), Space: 17.2 MB (92.51%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,20 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+       
+        if len(nums) == 1 and target == 0:
+            return 0
+        
+        if len(nums) == 1 and target == 1:
+            return 0
+
+        answer = 0
+        for i in range(len(nums)):
+            if target <= nums[i] :
+                answer =  i
+                return answer
+           
+        if answer == 0:
+            return len(nums)
+        return answer
+
+        
