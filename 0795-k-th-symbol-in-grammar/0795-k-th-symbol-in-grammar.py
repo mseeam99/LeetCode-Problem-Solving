8e0f0d5f892b0eb29ed22b0245b74187e8371b48
Mohammad Seeam <112004640+mseeam99@users.noreply.github.com>
2024-06-28 10:42:08 -0500

    Time: 39 ms (26.07%), Space: 16.5 MB (31.88%) - LeetHub

---
+++ 0795-k-th-symbol-in-grammar/0795-k-th-symbol-in-grammar.py
@@ -0,0 +1,39 @@
+'''
+class Solution:
+    def kthGrammar(self, n: int, k: int) -> int:
+        tableAsList = []
+
+        tableAsList.append("0")
+        tempValue = ""
+
+        index = 1
+        while index < n:
+            tempListValue = tableAsList[index-1]
+            newValue = ""
+            innderIndex = 0
+            while innderIndex < len(tempListValue):
+                if tempListValue[innderIndex] == "0":
+                    newValue+="01"
+                elif tempListValue[innderIndex] == "1":
+                    newValue+="10"
+                innderIndex+=1
+            tableAsList.append(newValue)
+            index+=1
+        
+        strAnswer = tableAsList[n-1][k-1]
+        answer = int(strAnswer)
+        return answer
+'''
+class Solution:
+    def kthGrammar(self, n: int, k: int) -> int:
+        if n == 1:
+            return 0
+        length = 2 ** (n - 1)
+        
+        mid = length // 2
+        
+        if k <= mid:
+            return self.kthGrammar(n - 1, k)
+        else:
+            return 1 - self.kthGrammar(n - 1, k - mid)
+
