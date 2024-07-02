b4c95833d117d0d99b26a50864ab7debcda2125a
Mohammad Seeam <112004640+mseeam99@users.noreply.github.com>
2024-07-02 00:00:33 -0500

    Time: 46 ms (5.5%), Space: 16.4 MB (65.34%) - LeetHub

---
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.py
@@ -0,0 +1,26 @@
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        
+        pointerOne = 0
+        pointerTwo = pointerOne + 1
+
+        count = 0
+
+        while pointerOne < len(nums)-1:
+            if  nums[pointerOne] == nums[pointerTwo]:
+                count+=1
+                pointerTwo+=1
+            else:
+                pointerTwo+=1
+            
+            if pointerTwo == len(nums):
+                pointerOne+=1
+                pointerTwo = pointerOne + 1
+
+        return count
+
+
+
+        
+
+        
