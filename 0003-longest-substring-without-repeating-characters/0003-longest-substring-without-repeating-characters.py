2cb3a8a15041433c82c452869032116727a1f3af
Mohammad Seeam <112004640+mseeam99@users.noreply.github.com>
2024-07-02 00:56:51 -0500

    Time: 37 ms (99.5%), Space: 16.6 MB (79.72%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -16,10 +16,40 @@ class Solution:
 
         answer = max(hashMap)
         return answer
-            
-            
-            
 
 
 
+'''     
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+
+        if len(s) == 0:
+            return 0
+
+        if len(s) == 1:
+            return 1
+
+        pointerOne = 0
+        pointerTwo = pointerOne 
+
+        tempString = ""
+        hashMap = {}
+
+        while pointerOne < len(s) and pointerTwo < len(s):
+            if s[pointerTwo] not in tempString:
+                tempString+=s[pointerTwo]
+                pointerTwo+=1
+            elif s[pointerTwo] in tempString:
+                hashMap[len(tempString)] = tempString
+                tempString = ""
+                pointerOne+=1
+                pointerTwo = pointerOne 
+
+        hashMap[len(tempString)] = tempString
         
+        if len(hashMap) != 0 :
+            answer = max(hashMap)
+            return answer
+        else:
+            return len(s)
+'''     
