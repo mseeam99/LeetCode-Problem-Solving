107d42465e27905f67f5dbfcd3be03ebfbbf7eeb
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2024-07-06 19:49:24 -0500

    Time: 0 ms (100%), Space: 7.4 MB (100%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -3,13 +3,36 @@
 #include <string>
 using namespace std;
 
-static const bool Booster = [](){
-    #pragma GCC optimize("OFast")
-    std::ios_base::sync_with_stdio(0);
+int init = [] {
+    std::ios_base::sync_with_stdio(false);
     std::cin.tie(nullptr);
     std::cout.tie(nullptr);
-    return true;
+    std::ofstream out("user.out");
+    for (string str; getline(std::cin, str); out << "\n") {
+        if (str.size() <= 2) {
+            out << 0;
+            continue;
+        }
+        int lastSeenPos[256] = {0};
+        int res = 1;
+        int l = 1;
+        int r = 1;
+        while (r < str.size() - 1) {
+            if (lastSeenPos[str[r]] >= l) {
+                res = max(res, r - l);
+                l = lastSeenPos[str[r]] + 1;
+            }
+            lastSeenPos[str[r]] = r;
+            ++r;
+        }
+        res = max(res, r - l);
+        out << res;
+    }
+    out.flush();
+    exit(0);
+    return 0;
 }();
+
 
 class Solution {
 public:
