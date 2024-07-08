8aee04db26e95def6ef4e5f625bc609136956cb6
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2024-07-08 03:23:09 -0500

    Time: 15 ms (93.33%), Space: 11.4 MB (20.73%) - LeetHub

---
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -0,0 +1,38 @@
+static const bool Booster = [](){
+    #pragma GCC optimize("OFast")
+    std::ios_base::sync_with_stdio(0);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return true;
+}();
+
+class Solution {
+public:
+    int maxVowels(string s, int k) {
+
+        int maxVowel = 0;
+        int windowVowel = 0;
+
+        unordered_set<char> myHashMap = {'a','e','i','o','u'};
+
+        for(int i=0; i<k; i++){
+            if(myHashMap.find(s[i]) != myHashMap.end()){
+                windowVowel++;
+            }
+        }
+
+        maxVowel = windowVowel;
+
+        for(int i=k; i<s.size();i++){
+            if(myHashMap.find(s[i-k]) != myHashMap.end()){
+                windowVowel--;  
+            }
+
+            if(myHashMap.find(s[i]) != myHashMap.end()){
+                windowVowel++;  
+            }
+            maxVowel = max(maxVowel,windowVowel);
+        }
+        return maxVowel ;
+    }
+};
