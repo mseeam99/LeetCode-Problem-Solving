cb7e45efb946036ae5e4f4c36b5da47210da435a
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2024-07-09 01:37:11 -0500

    Time: 4 ms (69.85%), Space: 24.8 MB (97.67%) - LeetHub

--- 3471-minimum-average-of-smallest-and-largest-elements/3471-minimum-average-of-smallest-and-largest-elements.cpp
+++ 3471-minimum-average-of-smallest-and-largest-elements/3471-minimum-average-of-smallest-and-largest-elements.cpp
@@ -13,12 +13,10 @@ class Solution {
 public:
     double minimumAverage(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-
         int leftPointer  = 0;
         int rightPointer = nums.size()-1;
         float answer = ceil(1000000000000000.500);
         float currentValue = 0;
-       
         while(leftPointer < rightPointer){
             currentValue = ceil(nums[leftPointer] + nums[rightPointer]) / 2;
             answer = min(answer,currentValue);
