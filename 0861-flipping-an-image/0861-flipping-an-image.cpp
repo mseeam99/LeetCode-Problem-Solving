216024be03bf38205987efcba2399d2f4ef43128
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2024-07-10 00:05:07 -0500

    Time: 3 ms (72.9%), Space: 11.3 MB (11.84%) - LeetHub

---
+++ 0861-flipping-an-image/0861-flipping-an-image.cpp
@@ -0,0 +1,28 @@
+static const bool Booster = [](){
+    #pragma GCC optimize("OFast")
+    std::ios_base::sync_with_stdio(0);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return true;
+}();
+
+class Solution {
+public:
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
+
+        for(int i=0; i<image.size(); i++){
+            reverse(image[i].begin(),image[i].end());
+        }
+
+        for(int i=0;i<image.size();i++){
+            for(int j=0;j<image[i].size();j++){
+                if(image[i][j] == 1){
+                    image[i][j] = 0;
+                }else if(image[i][j] == 0){
+                    image[i][j] = 1;
+                }
+            }
+        }
+        return image;
+    }
+};
