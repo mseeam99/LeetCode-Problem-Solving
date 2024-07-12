9241662712dc4bbd961d28a9ca25ee34cca0ee00
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2024-07-11 19:37:37 -0500

    Time: 3 ms (68.44%), Space: 12.2 MB (10.38%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.cpp
@@ -0,0 +1,46 @@
+static const bool Booster = [](){
+    #pragma GCC optimize("OFast")
+    std::ios_base::sync_with_stdio(0);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return true;
+}();
+
+#include <vector>
+#include <string>
+#include <numeric>
+using namespace std;
+
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+
+        vector<int> newVector;
+
+        for (int i = 0; i < operations.size(); i++) {
+            if (operations[i] == "+" ) {
+                if (newVector.size() >= 2) {
+                    int last = newVector[newVector.size() - 1];
+                    int leftLast = newVector[newVector.size() - 2];
+                    int tempSum = last + leftLast;
+                    newVector.push_back(tempSum);
+                }
+            }else if (operations[i] == "D") {
+                if (newVector.size() > 0) {
+                    int tempValue = newVector[newVector.size() - 1];
+                    tempValue = tempValue * 2;
+                    newVector.push_back(tempValue);
+                }
+            }else if (operations[i] == "C" && !newVector.empty()) {
+                newVector.pop_back();
+            }else {
+                newVector.push_back(stoi(operations[i]));
+            }
+        }
+        int answer = accumulate(newVector.begin(), newVector.end(), 0);
+        return answer;
+    }
+};
+
+
+
