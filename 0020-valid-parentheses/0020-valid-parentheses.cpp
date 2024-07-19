0f48612b5c18124aa4471a392aa6fad725a1737d
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2024-07-19 08:42:11 -0500

    Time: 3 ms (41.3%), Space: 7.9 MB (27.57%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,54 @@
+static const bool Booster = [](){
+    #pragma GCC optimize("OFast")
+    std::ios_base::sync_with_stdio(0);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return true;
+}();
+
+class Solution {
+public:
+    bool isValid(string s) {
+
+        bool answer = false;
+
+        vector <char> myVector;
+
+        for(int i=0;i<s.size();i++){
+            if(s[i] == '(' || s[i] == '{' || s[i] == '['){
+                myVector.push_back(s[i]);
+            }
+
+            if(s[i] == ')'){
+                if(myVector.size()!= 0 && myVector[myVector.size()-1] == '('){
+                    myVector.pop_back();
+                }else{
+                    myVector.push_back(s[i]);
+                }
+            }
+
+            if(s[i] == '}'){
+                if(myVector.size()!= 0 && myVector[myVector.size()-1] == '{'){
+                    myVector.pop_back();
+                }else{
+                    myVector.push_back(s[i]);
+                }
+            }
+
+            if(s[i] == ']'){
+                if(myVector.size()!= 0 && myVector[myVector.size()-1] == '['){
+                    myVector.pop_back();
+                }else{
+                    myVector.push_back(s[i]);
+                }
+            }
+        }
+
+        if(myVector.size() == 0){
+            answer = true;
+        }
+
+        return answer;
+
+    }
+};
