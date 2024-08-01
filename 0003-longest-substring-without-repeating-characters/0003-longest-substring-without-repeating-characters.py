4719c060236ae5b1e313befd4968efd6b9c25b1d
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2024-08-01 18:13:14 -0500

    Time: 48 ms (87.04%), Space: 16.5 MB (76.51%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,55 +1,18 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-
-        if len(s) == 0:
-            return 0
-
-        hashMap = {}
-        setOfChar = set()
+        n = len(s)
+        maxLength = 0
+        charSet = set()
         left = 0
-        for right in range(len(s)):
-            while s[right] in setOfChar:
-                setOfChar.remove(s[left])
-                left+=1
-            setOfChar.add(s[right])
-            hashMap[len(setOfChar)] = setOfChar
-
-        answer = max(hashMap)
-        return answer
-
-
-
-'''     
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-
-        if len(s) == 0:
-            return 0
-
-        if len(s) == 1:
-            return 1
-
-        pointerOne = 0
-        pointerTwo = pointerOne 
-
-        tempString = ""
-        hashMap = {}
-
-        while pointerOne < len(s) and pointerTwo < len(s):
-            if s[pointerTwo] not in tempString:
-                tempString+=s[pointerTwo]
-                pointerTwo+=1
-            elif s[pointerTwo] in tempString:
-                hashMap[len(tempString)] = tempString
-                tempString = ""
-                pointerOne+=1
-                pointerTwo = pointerOne 
-
-        hashMap[len(tempString)] = tempString
         
-        if len(hashMap) != 0 :
-            answer = max(hashMap)
-            return answer
-        else:
-            return len(s)
-'''     
+        for right in range(n):
+            if s[right] not in charSet:
+                charSet.add(s[right])
+                maxLength = max(maxLength, right - left + 1)
+            else:
+                while s[right] in charSet:
+                    charSet.remove(s[left])
+                    left += 1
+                charSet.add(s[right])
+        
+        return maxLength
