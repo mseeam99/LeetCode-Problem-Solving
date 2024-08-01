7c54efa4d5e19eed961f171c845d8615a2a37c6d
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2024-07-31 20:19:50 -0500

    Time: 168 ms (99.87%), Space: 22.1 MB (34.79%) - LeetHub

--- 0969-number-of-recent-calls/0969-number-of-recent-calls.py
+++ 0969-number-of-recent-calls/0969-number-of-recent-calls.py
@@ -1,16 +1,11 @@
-from collections import deque 
+from collections import deque
+
 class RecentCounter:
     def __init__(self):
-        global myList
-        myList = deque()
+        self.myList = deque()
         
     def ping(self, t: int) -> int:
-        myList.append(t)
-        while myList[0] < t-3000:
-            myList.popleft()
-        return len(myList)
-
-
-
-
-        
+        self.myList.append(t)
+        while self.myList[0] < t - 3000:
+            self.myList.popleft()
+        return len(self.myList)
