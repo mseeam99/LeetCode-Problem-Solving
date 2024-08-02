1ec679156c4aceeb633e898e892d9eb5fe7734df
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2024-08-02 04:10:36 -0500

    Time: 28 ms (38.12%), Space: 24.1 MB (7.13%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -1,9 +1,9 @@
-static const bool Booster = [](){
+static const int Booster = [](){
     #pragma GCC optimize("OFast")
     std::ios_base::sync_with_stdio(0);
     std::cin.tie(nullptr);
     std::cout.tie(nullptr);
-    return true;
+    return 0;
 }();
 
 class Solution {
