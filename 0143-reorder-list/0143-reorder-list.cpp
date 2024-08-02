0b89bb43a2285d1a7c0930606c044cd5a3bc9a70
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2024-08-02 04:06:43 -0500

    Time: 24 ms (66.42%), Space: 23.9 MB (10.19%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+
+        vector<int> values;
+        ListNode* actualHead = head;
+
+        while(head != NULL){
+            values.push_back(head->val);
+            head = head->next;
+        }head = actualHead;
+
+        vector<int> reordered;
+        int left = 0, right = values.size() - 1;
+        while (left <= right) {
+            if (left == right) {
+                reordered.push_back(values[left]);
+                break;
+            }
+            reordered.push_back(values[left++]);
+            reordered.push_back(values[right--]);
+        }
+
+        int count = 0;
+        while(head!=NULL){
+            head->val = reordered[count++];
+            head = head->next;
+        }head = actualHead;
+        
+    }
+};
