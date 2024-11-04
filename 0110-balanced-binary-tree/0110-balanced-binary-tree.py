26e6f774692056e20e807d3447798a7d68cdb925
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2024-11-04 17:05:39 -0600

    Time: 0 ms (100%), Space: 17.9 MB (24.71%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -1,23 +1,12 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-
-
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
         answer = [True]
         self.recursion(root,answer)
         return answer[0]
         
-
     def recursion(self,root,answer):
-
         if root == None:
             return 0
-
         left  = self.recursion(root.left, answer)
         right = self.recursion(root.right,answer)
 
