47ca089dd34b72ffe3d335c26a7e87375e49a095
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2024-11-11 19:27:16 -0600

    Time: 3 ms (25.75%), Space: 17.5 MB (12.4%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -2,11 +2,9 @@ class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         answer = [] 
         indexArray = [0]
-
         self.recursionAgain(root,indexArray)
         for i in range(indexArray[0]):
             answer.append([])
-
         self.recursion(root,answer,0)
         finalAnswer = []
         for i in range(len(answer)):
@@ -15,23 +13,16 @@ class Solution:
         return finalAnswer
 
     def recursion(self,root,answer,index):
-
         if root == None:
             return
-
         self.recursion(root.left, answer,index+1)
         self.recursion(root.right,answer,index+1)
-
         answer[index].append(root.val)
 
-
     def recursionAgain(self,root,indexArray):
-
         if root == None:
             return
-
         indexArray[0] = indexArray[0] + 1
-
         self.recursionAgain(root.left, indexArray)
         self.recursionAgain(root.right,indexArray)
         
