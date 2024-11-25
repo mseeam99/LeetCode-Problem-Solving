de432322011215a00b23d61fcdf0fb18047b18be
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2024-11-25 00:52:03 -0600

    Time: 24 ms (13.83%), Space: 16.8 MB (24.81%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.py
@@ -0,0 +1,29 @@
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        hashMap = {}
+        for i in range(len(moves)):
+            if moves[i] not in hashMap:
+                hashMap[moves[i]] = 1
+            else:
+                hashMap[moves[i]] += 1
+
+        up = 0
+        down = 0
+        left = 0
+        right = 0
+
+        keys = list(hashMap.keys())
+        values = list(hashMap.values())
+
+        for i in range(len(keys)):
+            if keys[i] == "U":
+                up += values[i]
+            if keys[i] == "D":
+                down += values[i]
+            if keys[i] == "L":
+                left += values[i]
+            if keys[i] == "R":
+                right += values[i]
+                
+        return up == down and left == right
+
