fc2877fc557d435d76daed77124f96ee3fd6a1a2
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2024-12-24 10:47:22 -0600

    Time: 532 ms (5.49%), Space: 38.7 MB (7.44%) - LeetHub

---
+++ 1604-least-number-of-unique-integers-after-k-removals/1604-least-number-of-unique-integers-after-k-removals.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
+
+        theList = []
+        hashMap = {}
+
+        for i in range(len(arr)):
+            if arr[i] not in hashMap:
+                hashMap[arr[i]] = 1
+            else:
+                hashMap[arr[i]] += 1
+
+        for val,count in hashMap.items():
+            theList.append((count,val))
+
+        heapq.heapify(theList)
+
+        print(theList)
+
+        for i in range(k):
+
+            t = heapq.heappop(theList)
+            if t[0] > 1:
+                heapq.heappush(theList,(t[0]-1,t[1]))
+
+        print(theList)
+
+        return len(theList)
