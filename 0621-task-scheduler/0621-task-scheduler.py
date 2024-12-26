1f0e48df9441613475c82003531316f347098ddc
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2024-12-26 08:06:42 -0600

    Time: 143 ms (41.52%), Space: 19.2 MB (20.76%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -1,18 +1,33 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        tasks = Counter(tasks)
-        task_values = [-i for i in tasks.values()]
-        myDeque = deque()
-        heapq.heapify(task_values)
-        time = 0
-        while len(task_values) != 0 or len(myDeque) != 0:
-            time+=1
-            if len(task_values)!=0:
-                max_value = heapq.heappop(task_values) + 1
-                if max_value:
-                    myDeque.append([max_value,time+n])
-            if len(myDeque) != 0 and myDeque[0][1] == time:
-                heapq.heappush(task_values,myDeque.popleft()[0])
-        return time
+        keyAndValue = Counter(tasks)
+        count = []
+        for values in keyAndValue.values():
+            count.append(values*-1)
+        myQueue = deque()
+        currentTime = 0
+        heapq.heapify(count)
+        while count or myQueue:
+            if count:
+                workingValue = heapq.heappop(count)
+                workingValue = workingValue + 1
+                if workingValue != 0:
+                    shouldBeAGoodTime = currentTime + n
+                    myQueue.append((shouldBeAGoodTime,workingValue))
+            if myQueue and myQueue[0][0] == currentTime:
+                mostLeftGoodTime, mostLeftValue = myQueue.popleft()
+                heapq.heappush(count,mostLeftValue)
+            currentTime+=1
+        return currentTime
+
+
+
+            
+            
+
+
+
+
+
 
         
