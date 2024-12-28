71cf925aa0f28b06d13f7936d6cfae993fb51099
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2024-12-28 12:32:07 -0600

    Time: 587 ms (61.14%), Space: 58.9 MB (100%) - LeetHub

---
+++ 2012-process-tasks-using-servers/2012-process-tasks-using-servers.py
@@ -0,0 +1,34 @@
+class Solution:
+    def assignTasks(self, servers: List[int], tasks: List[int]) -> List[int]:
+
+        for i in range(len(servers)):
+            servers[i] = (servers[i],i)
+        heapq.heapify(servers)
+        
+        currentTime = 0
+
+        serverQueue = []
+
+        answer = []
+
+        for i in range(len(tasks)):
+
+            currentTime = max(currentTime, i)  
+
+            while serverQueue and serverQueue[0][0] <= currentTime:
+                availableTime, weight, index = heapq.heappop(serverQueue)
+                heapq.heappush(servers, (weight, index))
+            
+            if not servers:
+                currentTime = serverQueue[0][0]
+
+            while serverQueue and serverQueue[0][0] <= currentTime:
+                availableTime, weight, index = heapq.heappop(serverQueue)
+                heapq.heappush(servers, (weight, index))
+
+            taskTime = tasks[i]
+            weight, index = heapq.heappop(servers)
+            answer.append(index)
+            heapq.heappush(serverQueue, (currentTime + taskTime, weight, index))
+
+        return answer
