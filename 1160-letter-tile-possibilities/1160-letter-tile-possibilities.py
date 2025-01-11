63780fdaeac543349300666dd2c898a02a195f3d
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2025-01-11 17:28:34 -0600

    Time: 19 ms (92.2%), Space: 19.6 MB (28.31%) - LeetHub

---
+++ 1160-letter-tile-possibilities/1160-letter-tile-possibilities.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numTilePossibilities(self, tiles: str) -> int:
+        unique_sequences = set()
+        
+        for length in range(1, len(tiles) + 1):
+            for perm in permutations(tiles, length):
+                unique_sequences.add(perm)
+        
+        return len(unique_sequences)
