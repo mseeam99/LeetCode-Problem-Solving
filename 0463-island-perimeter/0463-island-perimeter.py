bf69c384150c13dad603d8c2ca1257b82dea89ad
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2025-01-21 22:31:47 -0600

    Time: 155 ms (18.29%), Space: 25 MB (13.44%) - LeetHub

---
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -0,0 +1,18 @@
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        visited = set()
+        def backTracking(r,c):
+            if r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]) or grid[r][c] == 0 :
+                return 1
+            if (r,c) in visited:
+                return 0
+            visited.add((r,c))
+            totalLand = backTracking(r,c+1)
+            totalLand += backTracking(r+1,c)
+            totalLand += backTracking(r,c-1)
+            totalLand += backTracking(r-1,c)
+            return totalLand
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    return backTracking(i,j)
