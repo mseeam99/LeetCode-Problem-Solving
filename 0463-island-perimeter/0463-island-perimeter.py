a9d6c96e0c139b7f7de19743b881a8edbd8bde5d
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2025-01-22 23:11:37 -0600

    Time: 164 ms (15.57%), Space: 25 MB (12.53%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -12,6 +12,7 @@ class Solution:
             totalLand += backTracking(r,c-1)
             totalLand += backTracking(r-1,c)
             return totalLand
+            
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == 1:
