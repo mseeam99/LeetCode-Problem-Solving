29cdeada29142bc4668eb93903996ab56c90f7c3
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2025-01-29 16:20:13 -0600

    Time: 993 ms (10.12%), Space: 73.8 MB (5.06%) - LeetHub

--- 2035-count-sub-islands/2035-count-sub-islands.py
+++ 2035-count-sub-islands/2035-count-sub-islands.py
@@ -19,26 +19,25 @@ class Solution:
 
         def backTrackFirstTwo(r, c):
             if r < 0 or r >= rows or c < 0 or c >= cols or grid2[r][c] == 0:
-                return True  # Still valid if it reaches water/boundary
+                return True  
             
             if (r, c) not in coordinates:
-                return False  # If this land is not in grid1, it's not a sub-island
+                return False  
             
-            grid2[r][c] = 0  # Mark visited
+            grid2[r][c] = 0  
 
-            # Recursively check all four directions
             up = backTrackFirstTwo(r - 1, c)
             down = backTrackFirstTwo(r + 1, c)
             left = backTrackFirstTwo(r, c - 1)
             right = backTrackFirstTwo(r, c + 1)
 
-            return up and down and left and right  # Ensure all parts are valid
+            return up and down and left and right 
 
         count = 0
         for i in range(rows):
             for j in range(cols):
                 if grid2[i][j] == 1:
-                    if backTrackFirstTwo(i, j):  # If the entire island is valid, increment count
+                    if backTrackFirstTwo(i, j):
                         count += 1
 
         return count
