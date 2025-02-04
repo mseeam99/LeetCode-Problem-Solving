40cb5c180a8acdca73eaddaac2cc506f45302b49
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2025-02-04 07:30:07 -0600

    Time: 55 ms (12.23%), Space: 37 MB (5.09%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -3,42 +3,39 @@ import copy
 
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
-        copyOfBoard = copy.deepcopy(board)  # Properly copy the board
 
         visited = set()
         toChange = set()
-        isEnclosed = True  # Flag to track if the region is enclosed
+        isEnclosed = True 
 
         def backTrack(r, c):
-            nonlocal isEnclosed  # Use a flag to determine if it's enclosed
+            nonlocal isEnclosed  
 
             if r < 0 or r >= len(board) or c < 0 or c >= len(board[0]):
-                isEnclosed = False  # If it touches the border, it's not enclosed
+                isEnclosed = False
                 return
-            
+
             if board[r][c] == "X" or (r, c) in visited:
-                return  # Stop recursion if we hit a wall or revisit
+                return  
             
-            visited.add((r, c))  # Mark cell as visited
-            toChange.add((r, c))  # Track for conversion later
+            visited.add((r, c))  
+            toChange.add((r, c))  
 
-            # Explore in all 4 directions
-            backTrack(r, c + 1)
-            backTrack(r + 1, c)
-            backTrack(r, c - 1)
-            backTrack(r - 1, c)
+            backTrack(r, c + 1)  # Right
+            backTrack(r + 1, c)  # Down
+            backTrack(r, c - 1)  # Left
+            backTrack(r - 1, c)  # Up
 
-        # Iterate over the board to find "O" regions
         for i in range(len(board)):
             for j in range(len(board[0])):
                 if board[i][j] == "O" and (i, j) not in visited:
-                    toChange.clear()  # Reset tracking for each new region
-                    isEnclosed = True  # Assume it's enclosed initially
+                    toChange.clear()  
+                    isEnclosed = True  
 
-                    backTrack(i, j)  # Start backtracking
+                    backTrack(i, j) 
 
                     if isEnclosed:
                         for x, y in toChange:
-                            board[x][y] = "X"  # Only modify enclosed regions
+                            board[x][y] = "X"  
 
-        return board  # (Not necessary as function modifies in place)
+        return board 
