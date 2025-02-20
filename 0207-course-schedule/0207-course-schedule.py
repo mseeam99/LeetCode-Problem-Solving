e82c82068e443cf57e43132a5891982e9ee5ca0f
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2025-02-20 13:00:27 -0600

    Time: 7 ms (49.71%), Space: 19.5 MB (23.1%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,46 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+
+        myMap = {}
+        visitedSet = set()
+
+        for i in range(numCourses):
+            if i not in myMap:
+                myMap[i] = []
+        for i in range(len(prerequisites)):
+            myMap[prerequisites[i][1]].append(prerequisites[i][0])
+
+        def function(c):
+            if c in visitedSet:
+                return False
+
+            if myMap[c] == []:
+                return True
+            
+            visitedSet.add(c)
+            for pre in myMap[c]:
+                if function(pre) == False:
+                    return False
+            visitedSet.remove(c)
+            myMap[c] = []
+            return True
+
+
+
+        for i in range(len(prerequisites)):
+            if function(prerequisites[i][1]) == False:
+                return False
+
+        return True
+
+            
+
+
+
+
+
+
+        
+       
+
+        
