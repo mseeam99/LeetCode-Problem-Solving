880e5b82023cef00bbfecf9733e53517fa1d7fae
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2025-02-26 22:01:48 -0600

    Time: 67 ms (23.99%), Space: 24.2 MB (17.28%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.py
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.py
@@ -1,45 +1,43 @@
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
         
-        myMap = {}  # Mapping of index to its adjacent nodes
+        myMap = {} 
         for i in range(len(graph)):
             myMap[i] = graph[i]
 
-        visitedIndex = set()  # To track nodes in the current path
-        safeNode = {}  # To track the final safe/unsafe state of nodes
+        visitedIndex = set()  
+        safeNode = {}  
 
         def function(index):
-            if index in safeNode:  # If already computed, return it
+            if index in safeNode:  
                 return safeNode[index]
 
-            if myMap[index] == []:  # No outgoing edges means it's a safe node
+            if myMap[index] == []:  
                 safeNode[index] = True
                 return True
 
-            if index in visitedIndex:  # Detected a cycle
+            if index in visitedIndex: 
                 safeNode[index] = False
                 return False
 
-            visitedIndex.add(index)  # Mark as visited in the current path
+            visitedIndex.add(index) 
             theList = myMap[index]
-            isSafe = True  # Assume it's safe unless proven otherwise
+            isSafe = True  
             
             for i in range(len(theList)):
                 ans = function(theList[i])
-                if not ans:  # If any neighbor is not safe, mark this node unsafe
+                if not ans:  
                     isSafe = False
                     break
             
-            visitedIndex.remove(index)  # Remove from current path
+            visitedIndex.remove(index)  
 
-            safeNode[index] = isSafe  # Record whether this node is safe or not
+            safeNode[index] = isSafe 
             return isSafe
 
-        # Compute safety for each node in the graph
         for index in range(len(graph)):
             function(index)
 
-        # Collect all safe nodes (those that have a True value in safeNode)
         answer = [i for i in range(len(myMap)) if safeNode.get(i, False)]
 
         return answer
