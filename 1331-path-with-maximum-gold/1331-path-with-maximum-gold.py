a73218254a60e81dfdae1d1b82166b9cf06d6842
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2025-03-06 16:24:05 -0600

    Time: 3125 ms (53.41%), Space: 17.8 MB (79.38%) - LeetHub

--- 1331-path-with-maximum-gold/1331-path-with-maximum-gold.py
+++ 1331-path-with-maximum-gold/1331-path-with-maximum-gold.py
@@ -5,16 +5,9 @@ class Solution:
 
         def function(r,c):
           
-            if r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]):
+            if r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]) or (r,c) in visitedSet or grid[r][c] == 0:
                 return 0
 
-            if (r,c) in visitedSet:
-                return 0
-
-            if grid[r][c] == 0:
-                return 0
-
-    
             visitedSet.add((r,c))
 
             gold = grid[r][c]
