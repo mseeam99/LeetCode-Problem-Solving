62918cfbce5b030f2041c69501c382ec9f681277
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2025-03-06 16:23:16 -0600

    Time: 3134 ms (53.3%), Space: 18 MB (15.49%) - LeetHub

---
+++ 1331-path-with-maximum-gold/1331-path-with-maximum-gold.py
@@ -0,0 +1,40 @@
+class Solution:
+    def getMaximumGold(self, grid: List[List[int]]) -> int:
+
+        visitedSet = set()
+
+        def function(r,c):
+          
+            if r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]):
+                return 0
+
+            if (r,c) in visitedSet:
+                return 0
+
+            if grid[r][c] == 0:
+                return 0
+
+    
+            visitedSet.add((r,c))
+
+            gold = grid[r][c]
+
+            right = function(r,c+1)
+            down  = function(r+1,c)
+            left  = function(r,c-1)
+            up    = function(r-1,c)
+
+            visitedSet.remove((r,c))
+
+            return gold + max(right,down,left,up)
+
+        maxValue = 0
+
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] != 0:
+                    eachIterReturn = function(r,c)
+                    maxValue = max(maxValue,eachIterReturn)
+                     
+        return maxValue
+        
