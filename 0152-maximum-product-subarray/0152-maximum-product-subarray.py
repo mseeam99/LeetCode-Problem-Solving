3f664f3af8285e6325c01536606dfbadb507a967
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2025-04-09 02:45:02 -0500

    Time: 10 ms (59.09%), Space: 18.1 MB (96.14%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -1,28 +1,13 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
 
-        if len(nums) == 1:
-            return nums[0]
+        result = max(nums)
 
-        if len(nums) == 2:
-            if 0 in nums:
-                return max(nums)
-            return max(nums[0] * nums[1], max(nums))
+        curMax, curMin = 1, 1
 
-        dp_max = [0] * len(nums)
-        dp_min = [0] * len(nums)
-
-        dp_max[0] = nums[0]
-        dp_min[0] = nums[0]
-
-        print("dp_max:", dp_max)
-        print("dp_min:", dp_min)
-
-        for i in range(1, len(nums)):
-            dp_max[i] = max(nums[i], dp_max[i-1] * nums[i], dp_min[i-1] * nums[i])
-            dp_min[i] = min(nums[i], dp_max[i-1] * nums[i], dp_min[i-1] * nums[i])
-
-        print("dp_max:", dp_max)
-        print("dp_min:", dp_min)
-
-        return max(dp_max)
+        for i in range(len(nums)):
+            currentValue = curMax * nums[i]  
+            curMax = max(nums[i], currentValue, curMin * nums[i])
+            curMin = min(nums[i], currentValue, curMin * nums[i])
+            result = max(result, curMax)
+        return result
