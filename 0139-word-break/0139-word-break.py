b928612ebcfc7918109c66cf4b696c992d09c291
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2025-04-13 17:16:17 -0500

    Time: 15 ms (5.12%), Space: 18.2 MB (12.81%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -4,10 +4,20 @@ class Solution:
         dp = [False] * (len(s)+1)
         dp[0] = True
 
+        print(dp)
+
+        print()
+        print()
+        print()
+
+        print(wordDict)
+
 
         for i in range(len(s)):
             for word in wordDict:
                 if dp[i] == True and s[i:i+len(word)] == word:
+                    print("INDEX WE ARE PUTTING IN DP: ", (i+len(word)))
                     dp[i+len(word)] = True
+                    print(dp)
 
         return dp[-1]
