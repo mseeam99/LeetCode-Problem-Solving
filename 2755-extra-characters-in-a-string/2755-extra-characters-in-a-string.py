9b3e19ca3fe690aaf86a8b4e49a5d0582706c295
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2025-04-13 19:16:52 -0500

    Time: 83 ms (80.62%), Space: 17.8 MB (76.21%) - LeetHub

---
+++ 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minExtraChar(self, s: str, dictionary: List[str]) -> int:
+        n = len(s)
+        wordSet = set(dictionary)
+        dp = [float('inf')] * (n + 1)
+        dp[0] = 0  
+
+        for i in range(1, n + 1):
+            dp[i] = dp[i - 1] + 1
+            for j in range(i):
+                if s[j:i] in wordSet:
+                    dp[i] = min(dp[i], dp[j])
+
+        return dp[n]
