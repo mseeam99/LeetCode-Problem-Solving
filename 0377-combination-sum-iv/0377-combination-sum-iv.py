631aaa6e9b74a4a7d8e4bc3305cefad7ae2308f8
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2025-05-20 16:33:00 -0500

    Time: 46 ms (37.13%), Space: 17.6 MB (85.29%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.py
+++ 0377-combination-sum-iv/0377-combination-sum-iv.py
@@ -1,24 +1,24 @@
-from typing import List
-
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
         
-        dparray = [-1] * (target + 1)
-
-        def dp(n):
+        array = [-1] * (target+1)
+        
+        def recursion(n):
 
             if n > target:
                 return 0
-            elif n == target:
+            if n == target:
                 return 1
-            elif dparray[n] != -1:
-                return dparray[n]
+            if array[n] != -1:
+                return array[n]
 
             res = 0
-            for num in nums:
-                res += dp(n + num)
-            dparray[n] = res
 
+            for num in nums:
+                res += recursion(n + num)
+            
+            array[n] = res
             return res
 
-        return dp(0)
+        return recursion(0)
+
