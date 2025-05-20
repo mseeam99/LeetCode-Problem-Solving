7479e6a5e8cfe853365c41dd996a430cef4af04c
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2025-05-20 15:43:18 -0500

    Time: 51 ms (13.41%), Space: 17.9 MB (46.33%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.py
+++ 0377-combination-sum-iv/0377-combination-sum-iv.py
@@ -2,19 +2,20 @@ from typing import List
 
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
-        dpa = [-1] * (target + 1)
+        dparray = [-1] * (target + 1)
 
         def dp(n):
             if n > target:
                 return 0
             elif n == target:
                 return 1
-            elif dpa[n] != -1:
-                return dpa[n]
+            elif dparray[n] != -1:
+                return dparray[n]
+
             res = 0
             for num in nums:
                 res += dp(n + num)
-            dpa[n] = res
+            dparray[n] = res
             return res
 
         return dp(0)
