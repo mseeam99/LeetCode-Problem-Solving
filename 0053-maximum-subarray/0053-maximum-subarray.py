38754e7b83b8e98b773a177f833d2325c048e741
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2025-05-28 15:14:34 -0500

    Time: 100 ms (10.71%), Space: 31.8 MB (88.02%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -4,6 +4,8 @@ class Solution:
         current = nums[0]
         for i in range(1,len(nums)):
             current = max(nums[i],current+nums[i])
+            if current <= 0 :
+                current = nums[i]
             maxval  = max(maxval,current)
         return maxval
         
