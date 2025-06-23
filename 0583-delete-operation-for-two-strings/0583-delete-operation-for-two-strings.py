570d7a6acdfbc01bce21f36fd75977911fccca49
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2025-06-23 13:25:45 -0500

    Time: 276 ms (5.04%), Space: 32.7 MB (13.37%) - LeetHub

---
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        memo = {}
+        answer = 0
+        def recursion(indexOne,indexTwo):
+            nonlocal answer
+            if (indexOne,indexTwo) in memo:
+                return memo[(indexOne,indexTwo)]
+            if indexOne >= len(word1) or indexTwo >= len(word2):
+                return 0
+            elif word1[indexOne] == word2[indexTwo]:
+                return (1 + recursion(indexOne+1,indexTwo+1))
+            else:
+                x = recursion(indexOne+1,indexTwo)
+                y = recursion(indexOne,indexTwo+1)
+                memo[(indexOne,indexTwo)] = max(x,y)
+                return max(x,y)
+        answer = recursion(0,0)
+        return (len(word1) + len(word2) - answer*2)
+
+        
