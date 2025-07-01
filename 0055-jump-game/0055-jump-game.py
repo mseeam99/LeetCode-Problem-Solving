69816f279b387fe2606dcb2827ed862f33a8979e
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2025-07-01 09:46:20 -0500

    Time: 10 ms (92.33%), Space: 18.7 MB (42.03%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,38 +1,9 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-
-        leftPointer  = 0
-        prevMaxValueFound, maxValueFound = 0, 0
-        maxIndexAssociatedWithValue = 0
-
-        while leftPointer < len(nums):
-            
-            if (leftPointer + nums[leftPointer]) >= len(nums)-1:
-                return True
-
-            maxIndexAssociatedWithValue = leftPointer
-
-            for i in range(leftPointer+1,min(leftPointer+nums[leftPointer]+1,len(nums))):
-            
-                maxValueFound = max(maxValueFound,i+nums[i])
-                if maxValueFound >= prevMaxValueFound:
-                    prevMaxValueFound = maxValueFound
-                    maxValueFound = nums[i]
-                    maxIndexAssociatedWithValue = i
-            
-            print(maxValueFound)
-            nums[leftPointer] = maxValueFound
-            
-            if maxIndexAssociatedWithValue == leftPointer:
-                return False
-
-            leftPointer = maxIndexAssociatedWithValue
-            prevMaxValueFound, maxValueFound = 0, 0
-            maxIndexAssociatedWithValue = 0
-
-        return False
+        goal = len(nums)-1
+        for i in range(len(nums)-1,-1,-1):
+            if (i+nums[i]) >= goal:
+                goal = i
+        return True if goal == 0 else False
 
         
-            
-
-
