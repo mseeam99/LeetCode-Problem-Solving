4862849ca2485de3c763bf43f6f083a72bd9d725
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2025-07-03 13:57:04 -0500

    Time: 35 ms (58.65%), Space: 19.1 MB (42.9%) - LeetHub

---
+++ 0876-hand-of-straights/0876-hand-of-straights.py
@@ -0,0 +1,33 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        
+        if len(hand) % groupSize != 0:
+            return False
+
+        hashMap = {}
+        for i in range(len(hand)):
+            if hand[i] not in hashMap:
+                hashMap[hand[i]]=1
+            elif hand[i] in hashMap:
+                hashMap[hand[i]]+=1
+        minHeap = list(hashMap.keys())
+        heapq.heapify(minHeap)
+
+        while minHeap:
+            first = minHeap[0]
+
+            for i in range(first,first+groupSize):
+                if i not in hashMap:
+                    return False
+                hashMap[i]-=1
+                if hashMap[i] == 0:
+                    if i != minHeap[0]:
+                        return False
+                    heapq.heappop(minHeap)
+
+
+
+
+
+        return True
+        
