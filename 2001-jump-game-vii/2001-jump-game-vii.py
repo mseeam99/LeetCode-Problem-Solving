bd48fa2c9fab348febf4b2554d0fd243b319bf14
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2025-07-11 10:33:32 -0500

    Time: 1290 ms (5.03%), Space: 28.5 MB (5.19%) - LeetHub

--- 2001-jump-game-vii/2001-jump-game-vii.py
+++ 2001-jump-game-vii/2001-jump-game-vii.py
@@ -1,23 +1,18 @@
 class Solution:
     def canReach(self, s: str, minJump: int, maxJump: int) -> bool:
-        n = len(s)
-        visited = set()
-        queue = [0]
+        myQueue = [0]
+        mySet = set()
         farthest = 0
-
-        while queue:
-            pointer = queue.pop(0)
-
-            start = max(pointer + minJump, farthest + 1)
-            end = min(pointer + maxJump + 1, n)
-
-            for i in range(start, end):
-                if s[i] == '0' and i not in visited:
-                    if i == n - 1:
+        while myQueue:
+            pointer = myQueue.pop(0)
+            left  = max(pointer+minJump,farthest+1)
+            right = min(pointer+maxJump+1,len(s))
+            for i in range(left,right):
+                if s[i] == "0" and i not in mySet:
+                    if i == len(s)-1:
                         return True
-                    queue.append(i)
-                    visited.add(i)
-
+                    myQueue.append(i)
+                    mySet.add(i)
             farthest = max(farthest, pointer + maxJump)
-
-        return n - 1 in visited
+        return True if len(s) - 1 in mySet else False
+        
