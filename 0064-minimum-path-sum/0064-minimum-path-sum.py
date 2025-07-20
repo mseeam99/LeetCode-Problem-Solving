e0a829935fa07be70fe5053c79abbc96a483ff07
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2025-07-20 15:17:32 -0500

    Time: 22 ms (31.7%), Space: 20.2 MB (52.32%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -0,0 +1,38 @@
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+
+        outerArray = [0] * len(grid[0])     
+
+        for row in range(len(grid)-1,-1,-1):
+
+            currentRow = grid[row]
+
+            for col in range(len(grid[0])-1,-1,-1):
+
+                if col == len(grid[0])-1 and row == len(grid)-1:
+                    continue
+                elif col == len(grid[0])-1:
+                    currentRow[col] += outerArray[col] 
+                    continue
+                elif row == len(grid)-1:
+                    currentRow[col] += currentRow[col+1]
+                    continue
+
+                rightValue = 0
+                downValue = 0
+
+                if col+1 <= len(currentRow)-1:
+                    rightValue = currentRow[col+1]
+                if col <= len(outerArray)-1:
+                    downValue = outerArray[col]
+
+                minimumNeigbour = min(rightValue,downValue)
+
+                currentRow[col] += minimumNeigbour 
+            
+            print(currentRow)
+
+            outerArray = currentRow
+        
+        return outerArray[0]
+
