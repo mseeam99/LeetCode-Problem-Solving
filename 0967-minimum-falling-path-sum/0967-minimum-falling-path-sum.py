722fa44e009a5e51213e43fa6c498622a9020a52
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2025-07-20 19:43:55 -0500

    Time: 31 ms (36.78%), Space: 18.7 MB (57.8%) - LeetHub

---
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.py
@@ -0,0 +1,33 @@
+class Solution:
+    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+
+        outerArray = [0] * len(matrix[0])
+
+        for row in range(len(matrix)-1,-1,-1):
+
+            innerCurrentArray = matrix[row]
+
+            for col in range(len(matrix[0])-1,-1,-1):
+
+                rightDiagonal, down, leftDiagonal = float("inf"),float("inf"),float("inf")
+
+                if col+1 <= len(matrix[0])-1:
+                    rightDiagonal = outerArray[col+1]
+
+                down = outerArray[col]
+
+                if col-1 >= 0:
+                    leftDiagonal = outerArray[col-1]
+                
+
+                innerCurrentArray[col] += min(rightDiagonal, down, leftDiagonal)
+
+            outerArray = innerCurrentArray
+
+        return(min(outerArray))
+
+            
+
+
+
+        
