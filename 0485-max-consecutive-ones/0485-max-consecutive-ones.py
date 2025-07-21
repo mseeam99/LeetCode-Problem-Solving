0f706b635860946a44d45cee210bd10d7d169d94
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2025-07-21 16:52:26 -0500

    Time: 19 ms (61.68%), Space: 23.4 MB (5.12%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.py
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.py
@@ -3,8 +3,6 @@ class Solution:
         for i in range(1,len(nums)):
             if nums[i] != 0:
                 nums[i]+=nums[i-1]
-
-        print(nums)
         return max(nums)
             
 
