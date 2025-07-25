36426df2124892d1a2f128c1d453832a34811b5d
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2025-07-25 16:16:43 -0500

    Time: 1150 ms (37.51%), Space: 17.9 MB (89.97%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,20 +1,23 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        totalSum = sum(nums)
-        if totalSum % 2 != 0:
+
+        def subsetFunction(arr,sum):
+            previousDp = [False] * (sum+1)
+            previousDp[0] = True
+            for row in range(len(arr)):
+                currentDp = [False] * (sum+1)
+                currentDp[0] = True
+                for col in range(1,sum+1):
+                    notTake = previousDp[col]
+                    take = False
+                    if arr[row] <= col:
+                        take = previousDp[col-arr[row]]
+                    currentDp[col] = take or notTake
+                previousDp = currentDp
+            return previousDp[sum]
+        if sum(nums) % 2 != 0:
             return False
-        searchingFor = totalSum // 2
-        memo = {}
+        target = sum(nums) // 2
+        return subsetFunction(nums,target)
 
-        def dfs(index, currentSum):
-            if currentSum == searchingFor:
-                return True
-            if currentSum > searchingFor or index >= len(nums):
-                return False
-            if (index, currentSum) in memo:
-                return memo[(index, currentSum)]
-            result = dfs(index + 1, currentSum + nums[index]) or dfs(index + 1, currentSum)
-            memo[(index, currentSum)] = result
-            return result
 
-        return dfs(0, 0)
