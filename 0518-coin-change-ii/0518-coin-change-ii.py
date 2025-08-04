cbd8bfe12e9799dc4a4c67377d8ee2911068872a
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2025-08-04 10:23:02 -0500

    Time: 235 ms (56.82%), Space: 18.4 MB (54.23%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,28 +1,19 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        n = len(coins)
-        dp = [[-1 for _ in range(amount+1)] for _ in range(n)]
-
-        def make_pair(i, amount):
-            if amount == 0:
-                return 1
-            if i == 0:
-                if coins[i] <= amount and amount%coins[i] == 0:
-                    return 1
-                else:
-                    return 0
-
-            if dp[i][amount] != -1:
-                return dp[i][amount]
-            pick = 0
-            if coins[i] <= amount:
-                pick = make_pair(i, amount-coins[i])
-
-            not_pick = make_pair(i-1, amount)
-            dp[i][amount] = pick + not_pick
-            return dp[i][amount]
         
-        tot_ways = make_pair(n-1, amount)
-        if tot_ways:
-            return tot_ways
-        return 0
+        outerdp = [0] * (amount + 1)
+        
+        for T in range(amount + 1):
+            outerdp[T] = int(T % coins[0] == 0)
+
+        for row in range(1, len(coins)):
+            innerdp = outerdp.copy()
+            for col in range(amount + 1):
+                notTake = outerdp[col]
+                take = 0
+                if coins[row] <= col:
+                    take = innerdp[col - coins[row]]
+                innerdp[col] = take + notTake
+            outerdp = innerdp
+
+        return outerdp[amount]
