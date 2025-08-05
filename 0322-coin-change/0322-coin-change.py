660eac487f964464184ddee3d048b3a6ee641995
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2025-08-05 09:57:56 -0500

    Time: 1676 ms (5%), Space: 18.6 MB (35.13%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,17 +1,14 @@
-from typing import List
-
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+
         coins.sort()
         outerdp = [float("inf")] * (amount + 1)
-        outerdp[0] = 0  # base case: 0 coins to make amount 0
+        outerdp[0] = 0 
 
-        # First row initialization using only coins[0]
-        for i in range(coins[0], amount + 1):
+        for i in range(amount + 1):
             if i % coins[0] == 0:
                 outerdp[i] = i // coins[0]
 
-        # Process remaining coins
         for row in range(1, len(coins)):
             innerdp = outerdp.copy()
             for col in range(amount + 1):
