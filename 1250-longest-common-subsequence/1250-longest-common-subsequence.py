b9daa5ddf97461949b1141d8707fa204cdce9c8e
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2025-08-06 17:24:30 -0500

    Time: 339 ms (95.85%), Space: 18.1 MB (89.37%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.py
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.py
@@ -1,24 +1,16 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        outerdp = [0] * (len(text2) + 1)
+        for i in range(len(text1)):
+            innerdp = [0] * (len(text2) + 1)
+            for j in range(len(text2)):
+                if text1[i] == text2[j]:
+                    innerdp[j+1] = outerdp[j] + 1
+                else:
+                    innerdp[j+1] = max(innerdp[j],outerdp[j+1])
+            outerdp = innerdp
+        return outerdp[-1]
 
-        memo = {}
 
-        def recursion(indexOne,indexTwo):
-            nonlocal text1, text2, memo
 
-            if (indexOne,indexTwo) in memo:
-                return memo[(indexOne,indexTwo)]
-
-            if indexOne >= len(text1) or indexTwo >= len(text2):
-                return 0
-            elif text1[indexOne] == text2[indexTwo]:
-                return (1 + recursion(indexOne+1,indexTwo+1))
-            else:
-                leftTree  = recursion(indexOne+1,indexTwo)
-                rightTree = recursion(indexOne,indexTwo+1)
-                memo[indexOne,indexTwo] = max(leftTree,rightTree)
-                return max(leftTree,rightTree)
-            return 
-
-        return recursion(0,0)
         
