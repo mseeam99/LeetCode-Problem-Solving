6c47e2b18af8de0b816202ac11d6f850d8209006
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2025-08-06 10:46:06 -0500

    Time: 0 ms (100%), Space: 18.2 MB (44.79%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -0,0 +1,29 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+
+        def binarySearch(array,target):
+            leftPointer = 0
+            rightPointer = len(array)-1
+            while leftPointer <= rightPointer:
+                middlePointer = int(leftPointer+(rightPointer-leftPointer)//2)
+                if array[middlePointer] == target:
+                    return True
+                elif array[middlePointer] < target:
+                    leftPointer = middlePointer+1
+                elif array[middlePointer] > target:
+                    rightPointer = middlePointer-1
+                    
+
+        for row in matrix:
+            ans = binarySearch(row,target)
+            if ans == True:
+                return True
+
+        return False
+                
+
+
+
+
+                
+        
