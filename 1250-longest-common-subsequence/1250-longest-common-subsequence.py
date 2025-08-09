d28ec1c902482ee22aa41530aacc3807ee48d9a4
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2025-08-09 17:14:29 -0500

    Time: 455 ms (72.73%), Space: 43 MB (41.89%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.py
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.py
@@ -1,16 +1,35 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        outerdp = [0] * (len(text2) + 1)
-        for i in range(len(text1)):
-            innerdp = [0] * (len(text2) + 1)
-            for j in range(len(text2)):
-                if text1[i] == text2[j]:
-                    innerdp[j+1] = outerdp[j] + 1
+        s = text1
+        t = text2
+        lengthS = len(s)
+        lengthT = len(t)
+
+        # DP initialization
+        dp = [[0] * (lengthT + 1) for _ in range(lengthS + 1)]
+
+        # Fill DP table
+        for row in range(1, lengthS + 1):
+            for col in range(1, lengthT + 1):
+                if s[row - 1] == t[col - 1]:
+                    dp[row][col] = 1 + dp[row - 1][col - 1]
                 else:
-                    innerdp[j+1] = max(innerdp[j],outerdp[j+1])
-            outerdp = innerdp
-        return outerdp[-1]
+                    dp[row][col] = max(dp[row - 1][col], dp[row][col - 1])
 
+        # Backtrack
+        i, j = lengthS, lengthT
+        longestCommonSubsequence = ""
+        while i > 0 and j > 0:
+            if s[i - 1] == t[j - 1]:
+                longestCommonSubsequence += s[i - 1]
+                i -= 1
+                j -= 1
+            elif dp[i - 1][j] > dp[i][j - 1]:
+                i -= 1
+            elif dp[i - 1][j] < dp[i][j - 1]:
+                j -= 1
+            else:  # tie case
+                i -= 1
 
-
-        
+        longestCommonSubsequence = longestCommonSubsequence[::-1]
+        return len(longestCommonSubsequence)
