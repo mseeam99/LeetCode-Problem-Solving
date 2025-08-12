e657f39e879a944fe3ded3b3b929c8188b923f6d
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2025-08-12 17:26:15 -0500

    Time: 231 ms (6.4%), Space: 51.7 MB (5.64%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
@@ -7,7 +7,6 @@ class Solution:
         if intervals:
             answer.append(intervals[0])
         count = 0
-        print(intervals)
         for i in range(1,len(intervals)):
             if answer:
                 if answer[-1][1] > intervals[i][0]:
@@ -15,7 +14,6 @@ class Solution:
                     count += 1
                     continue
             answer.append(intervals[i])  
-        print(answer)      
         return count
                 
 
