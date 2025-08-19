8b7cac23466bd0eb8d482640470a99284ccb77bc
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2025-08-18 22:46:00 -0500

    Time: 51 ms (71.34%), Space: 18.9 MB (26.89%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.py
+++ 0729-my-calendar-i/0729-my-calendar-i.py
@@ -3,32 +3,30 @@ class MyCalendar:
     def __init__(self):
         self.array = []
 
-    def binarySearch(self, target, array):    
-        left = 0
-        right = len(array)
+    def bisectLeft(self, target):    
+        left, right = 0, len(self.array)
         while left < right:
-            middle = left + (right - left) // 2 
-            if array[middle][1] == target:
-                return (middle,"EXIST")
-            if target > array[middle][1]:
-                left = middle + 1
-                continue
-            if target < array[middle][1]:
-                right = middle 
-                continue
-            left += 1
-        return (left,"NOTEXIST")
+            mid = (left + right) // 2
+            if self.array[mid][0] < target:
+                left = mid + 1
+            else:
+                right = mid
+        return left 
 
     def book(self, startTime: int, endTime: int) -> bool:
         if len(self.array) == 0:
             self.array.append([startTime,endTime])
             return True
         else:
-            copy = self.array.copy()
-            self.array.append([startTime,endTime])
-            self.array.sort()
-            for i in range(1,len(self.array)):
-                if self.array[i][0] < self.array[i-1][1]:
-                    self.array = copy
-                    return False
+            i = self.bisectLeft(startTime)
+            
+            if i and self.array[i - 1][1] > startTime:
+                return False
+
+            if i < len(self.array) and endTime > self.array[i][0]:
+                return False
+
+            self.array.insert(i, [startTime, endTime])
+
             return True
+
