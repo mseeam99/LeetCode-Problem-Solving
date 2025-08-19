9f1883dd026e28701068a7f65a9a43a0f6cd7728
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2025-08-18 22:52:28 -0500

    Time: 35 ms (85.09%), Space: 18.6 MB (93.69%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.py
+++ 0729-my-calendar-i/0729-my-calendar-i.py
@@ -14,18 +14,21 @@ class MyCalendar:
         return left 
 
     def book(self, startTime: int, endTime: int) -> bool:
+
         if len(self.array) == 0:
             self.array.append([startTime,endTime])
             return True
+
         else:
+
             i = self.bisectLeft(startTime)
             
-            if i and self.array[i - 1][1] > startTime:
+            if i > 0 and self.array[i - 1][1] > startTime:
                 return False
 
-            if i < len(self.array) and endTime > self.array[i][0]:
+            if i < len(self.array) and i <= len(self.array)-1 and endTime > self.array[i][0]:
                 return False
-
+           
             self.array.insert(i, [startTime, endTime])
 
             return True
