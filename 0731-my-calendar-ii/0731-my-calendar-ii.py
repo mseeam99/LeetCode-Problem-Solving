18a83c101285ea6608e46061f7e4843e85123540
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2025-08-20 14:43:11 -0500

    Time: 1854 ms (10.83%), Space: 18.6 MB (62.79%) - LeetHub

--- 0731-my-calendar-ii/0731-my-calendar-ii.py
+++ 0731-my-calendar-ii/0731-my-calendar-ii.py
@@ -5,12 +5,10 @@ class MyCalendarTwo:
         self.count = 0
 
     def book(self, startTime: int, endTime: int) -> bool:
-
         if startTime not in self.hashMap:
             self.hashMap[startTime] = 1
         else:
             self.hashMap[startTime] += 1
-        
         if endTime not in self.hashMap:
             self.hashMap[endTime] = -1
         else:
@@ -18,40 +16,18 @@ class MyCalendarTwo:
 
 
         for key,val in sorted(self.hashMap.items()):
-
             self.count += val
-
             if self.count >= 3:
-
                 if self.hashMap[startTime] == 1:
                     del self.hashMap[startTime]
                 else:
                     self.hashMap[startTime] -= 1
-
                 if self.hashMap[endTime] == -1:
                     del self.hashMap[endTime]
                 else:
                     self.hashMap[endTime] += 1
                 self.count = 0
                 return False
-                
+
         self.count = 0
         return True 
-
-            
-                
-        
-
-
-
-
-
-
-
-
-
-
-
-        
-        
-
