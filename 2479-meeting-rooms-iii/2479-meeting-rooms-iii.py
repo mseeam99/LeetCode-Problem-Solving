59ec5f2a83696e78898433fb5fc8d7d930409ddd
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2025-08-27 10:32:03 -0500

    Time: 277 ms (32.45%), Space: 51.9 MB (5.26%) - LeetHub

---
+++ 2479-meeting-rooms-iii/2479-meeting-rooms-iii.py
@@ -0,0 +1,61 @@
+class Solution:
+    def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
+        
+        if n == 0:
+            return 0
+
+        meetings.sort()
+        hashMap = {}
+        print(meetings)
+
+        availableRoomsArray = []
+        for i in range(n):
+            availableRoomsArray.append(i)
+        currentlyUsingRoomsArray = []
+        heapq.heapify(availableRoomsArray)
+        heapq.heapify(currentlyUsingRoomsArray)
+
+
+        for i in range(len(meetings)):
+
+        
+            startTime = meetings[i][0]
+            endTime = meetings[i][1]
+
+            # can use a room which finished before or just before interval starts
+            while currentlyUsingRoomsArray and currentlyUsingRoomsArray[0][0] <= startTime:
+                finishingTime, roomNumber = heapq.heappop(currentlyUsingRoomsArray)
+                heapq.heappush(availableRoomsArray,roomNumber)
+                
+            if availableRoomsArray:
+                roomNumber = heapq.heappop(availableRoomsArray)
+                heapq.heappush(currentlyUsingRoomsArray,(endTime,roomNumber))
+                if roomNumber not in hashMap:
+                    hashMap[roomNumber] = 1
+                else:
+                    hashMap[roomNumber] += 1
+            else:
+                # meeting will be dalayed and pop the first available room and use it, current end time + delay
+                finishingTime, roomNumber = heapq.heappop(currentlyUsingRoomsArray)
+                duration = endTime - startTime
+                willEndAt = finishingTime + duration
+                heapq.heappush(currentlyUsingRoomsArray, (willEndAt, roomNumber))
+
+                if roomNumber not in hashMap:
+                    hashMap[roomNumber] = 1
+                else:
+                    hashMap[roomNumber] += 1
+
+        maxRoomNumber = float("-inf")
+        maxValue = float("-inf")
+        for key,val in hashMap.items():
+            if val > maxValue:
+                maxValue = val
+                maxRoomNumber = key
+
+        print(hashMap)
+
+        return maxRoomNumber
+                    
+
+            
