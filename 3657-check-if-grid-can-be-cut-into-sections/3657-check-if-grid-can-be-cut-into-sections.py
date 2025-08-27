f524d732f7592285baf67615bb69333aa9bdc875
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2025-08-27 16:40:20 -0500

    Time: 571 ms (30.42%), Space: 83.8 MB (28.3%) - LeetHub

---
+++ 3657-check-if-grid-can-be-cut-into-sections/3657-check-if-grid-can-be-cut-into-sections.py
@@ -0,0 +1,23 @@
+class Solution:
+    def checkValidCuts(self, n: int, rectangles: List[List[int]]) -> bool:
+        leftToRightArray = [] # need x axis vvalue
+        bottomToTopArray = [] # need y axis value
+        for i in range(len(rectangles)):
+            x1, y1, x2, y2 = rectangles[i][0],rectangles[i][1],rectangles[i][2],rectangles[i][3]
+            leftToRightArray.append([x1,x2])
+            bottomToTopArray.append([y1,y2])
+        a = self.minInterval(leftToRightArray)
+        b = self.minInterval(bottomToTopArray)
+        return a or b
+        
+    def minInterval(self,array):
+        array.sort(key=lambda x: x[0])
+        count = 0
+        prev = -1
+        for i in range(len(array)):
+            if prev <= array[i][0]:
+                count += 1
+            prev = max(prev,array[i][1])
+        return count >= 3
+
+
