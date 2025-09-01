c10568d84a3630e3d6d7867dc84525da9b6450ce
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2025-09-01 17:32:11 -0500

    Time: 3 ms (59.36%), Space: 17.8 MB (32.71%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -12,10 +12,10 @@ class Solution:
         while left <= right:
             middle = floor((left+right) // 2)
             val = middle * middle
-            if val <= x:
-                answer = middle
-                left += 1
+            if val == x:
+                return middle
             elif val < x: 
+                answer = middle
                 left = middle + 1
             elif val > x: 
                 right = middle - 1
