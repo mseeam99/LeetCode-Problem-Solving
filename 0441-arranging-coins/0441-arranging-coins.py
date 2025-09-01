456f978a2643ef065957341f80b8b1de5c95ca95
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2025-09-01 16:52:28 -0500

    Time: 627 ms (9.6%), Space: 17.7 MB (81.72%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -0,0 +1,8 @@
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        
+        totalUsed = 0
+        for i in range(1,n+1):
+            totalUsed += i
+            if n - totalUsed < i+1:
+                return i
