3d191df770aabd4614f1373b59f3ad8ed7fca42f
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2025-09-01 17:15:42 -0500

    Time: 0 ms (100%), Space: 17.8 MB (31.3%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.py
+++ 0367-valid-perfect-square/0367-valid-perfect-square.py
@@ -5,18 +5,13 @@ class Solution:
         right = num
 
         while left <= right:
-
             middle = (left + right) // 2
             val = (middle * middle)
-
             if val == num:
                 return True
-
             elif val < num:
                 left = middle + 1
             elif val > num:
-                
                 right = middle - 1
-
         return False
         
