7bd77c1697d34c33e44fbe21804c2d5460c4289f
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2025-11-09 13:31:45 -0600

    Time: 0 ms (100%), Space: 18 MB (31.53%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.py
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.py
@@ -6,8 +6,6 @@ class Solution:
         for i in range(len(arr)):
             hashMap[arr[i]] = i
 
-        print(hashMap)
-
         for i in range(len(arr)):
             if (arr[i]*2) in hashMap:
                 if hashMap[(arr[i]*2)] != i:
