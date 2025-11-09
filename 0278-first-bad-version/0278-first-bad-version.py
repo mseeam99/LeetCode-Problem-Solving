827326997a8ac13936136cafe9f003f9a15cd616
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2025-11-08 22:54:22 -0600

    Time: 39 ms (44.13%), Space: 17.6 MB (92.15%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.py
+++ 0278-first-bad-version/0278-first-bad-version.py
@@ -3,17 +3,13 @@
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-
         left = 0
         right = n
-
         while left <= right:
             middle = left + (right-left) // 2
-
             if isBadVersion(middle) == True:
                 right = middle - 1
             else:
                left = middle + 1
-
         return right+1
         
