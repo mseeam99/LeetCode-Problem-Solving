dd4f3a77aa7f1df771c799ab6ca4068238e9306d
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2025-11-18 09:26:12 -0600

    Time: 47 ms (30%), Space: 28 MB (40%) - LeetHub

---
+++ 2437-maximum-number-of-groups-entering-a-competition/2437-maximum-number-of-groups-entering-a-competition.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maximumGroups(self, grades: List[int]) -> int:
+        if len(grades) == 1:
+            return 1
+            
+        grades.sort()
+        def linearIterCheck(group):
+            if (group * (group + 1) / 2) <= len(grades):
+                return True
+            return False
+
+        left = 0
+        right = len(grades)-1
+        groupNumber = 0
+        while left <= right:
+            middle = left + (right-left) // 2
+            if linearIterCheck(middle) == True:
+                left = middle + 1
+                groupNumber = middle
+            else:
+                right = middle - 1
+        return groupNumber
