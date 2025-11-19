6bf8c4490464e746c55ca68531238c3fcfeaa0d7
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2025-11-18 18:09:36 -0600

    Time: 0 ms (100%), Space: 29.5 MB (81.34%) - LeetHub

--- 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.py
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.py
@@ -3,18 +3,11 @@ class Solution:
         
         left = 0
         right = len(arr) - 1
-        
         while left <= right:
-
             middle = left + (right-left) // 2
-
             if arr[middle-1] < arr[middle] and arr[middle] > arr[middle+1]:
                 return middle
-            if arr[middle-1] > arr[middle]: # Decreasing
+            if arr[middle-1] > arr[middle]:
                 right = middle 
             else:          
                 left = middle + 1
-         
-
-
-
