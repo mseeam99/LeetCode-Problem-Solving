b1dac56ef5f83a3b5f151d900254b1cc9fcfc6b3
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2025-11-23 22:13:16 -0600

    Time: 36 ms (92.07%), Space: 53.3 MB (88.1%) - LeetHub

---
+++ 2667-create-hello-world-function/2667-create-hello-world-function.js
@@ -0,0 +1,20 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+
+
+    
+    return function(...args) {
+
+        string = "Hello World"
+
+        return string
+        
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
