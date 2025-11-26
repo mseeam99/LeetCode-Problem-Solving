e6a99d96cdd1a1e5e5cf9f2c696af74fb09a3f9a
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2025-11-26 13:00:43 -0600

    Time: 3 ms (71.62%), Space: 19.1 MB (87.17%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,17 +1,10 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-
-        hashMap = {}
-
+        one = 0
+        two = 0
         for i in range(len(nums)):
-            if nums[i] not in hashMap:
-                hashMap[nums[i]] = 1
-            else:
-                hashMap[nums[i]] += 1
+            one = (one ^ nums[i]) & (~two)
+            two = (two ^ nums[i]) & (~one)
+        return one
 
-        keys = list(hashMap.keys())
-        values = list(hashMap.values())
-
-        for i in range(len(values)):
-            if values[i] == 1:
-                return keys[i]
+   
