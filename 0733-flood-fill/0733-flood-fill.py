d348c5296d2278cfaadf26cc0e1cc2bb973199d3
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2025-11-27 13:38:59 -0600

    Time: 3 ms (17%), Space: 18.1 MB (52.07%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,26 +1,16 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-
         seen = set()
         fixedColor = image[sr][sc]
-
         def recursion(row,col):
-
             if (row,col) in seen or row < 0 or row >= len(image) or col < 0 or col >= len(image[0]) or image[row][col] != fixedColor:
                 return
-            
             image[row][col] = color
             seen.add((row,col))
-
-            
             recursion(row,col+1)
             recursion(row+1,col)
             recursion(row,col-1)
             recursion(row-1,col)
-
-        
-
-        
         recursion(sr,sc)
         return image
 
