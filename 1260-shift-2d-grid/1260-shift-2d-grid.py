bdd4eed6822ce9606b3bc6c389e0fc1952afdecc
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2025-11-28 15:05:27 -0600

    Time: 386 ms (5.08%), Space: 18.1 MB (93.23%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.py
+++ 1260-shift-2d-grid/1260-shift-2d-grid.py
@@ -1,19 +1,14 @@
 class Solution:
     def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
-
         elementTrack = grid[0][0]
         nextElementTrack = grid[0][0]
-
         def shiftValues():
             lastVal = grid[len(grid)-1][len(grid[0])-1]
             nonlocal elementTrack, nextElementTrack
             for row in range(len(grid)):
                 for col in range(len(grid[row])):
-
                     if row == len(grid)-1 and col == len(grid[0])-1:
                         grid[0][0] = lastVal
-
-
                     if row == len(grid)-1 and col == len(grid[0])-1:
                         break
                     if col == len(grid[0])-1:    
@@ -25,14 +20,9 @@ class Solution:
                         nextElementTrack = grid[row][col+1]
                         grid[row][col+1] = elementTrack
                         elementTrack = nextElementTrack
-                
-                    
 
-           
-
-
-
-        for i in range(k):
+        needToRun = k % (len(grid)*len(grid[0]))
+        for i in range(needToRun):
             shiftValues()
         return grid
 
