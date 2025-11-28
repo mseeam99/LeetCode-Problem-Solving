9ea8ad2c47eed5cdcbffeceede6da40775a69da2
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2025-11-28 03:01:04 -0600

    Time: 14 ms (6.72%), Space: 17.7 MB (84.46%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.py
@@ -0,0 +1,43 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+
+        if n == 1:
+            return "1"
+    
+        answer = "11" # after processing 2
+
+        def takeAndReturn():
+            nonlocal answer
+            tempAnswer = ""
+            pointerOne = 0
+            pointerTwo = 0
+            count = 0
+            while pointerTwo < len(answer):
+                if answer[pointerOne] == answer[pointerTwo]:
+                    pointerTwo+=1
+                    if pointerTwo == len(answer):
+                        count = pointerTwo - pointerOne
+                        tempAnswer+=str(count)
+                        tempAnswer+=str(answer[pointerTwo-1])
+                        break
+                else:
+                    count = pointerTwo - pointerOne 
+                    pointerOne = pointerTwo
+                    tempAnswer+=str(count)
+                    tempAnswer+=str(answer[pointerTwo-1])
+                
+
+            answer = tempAnswer
+            
+        for i in range(3,n+1):
+            takeAndReturn()
+        return answer
+
+        
+
+        
+
+        
+
+
+        
