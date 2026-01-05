3583f7ff0f9db8757faafdc16ee5c8fa12f29dcf
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-01-04 16:20:14 -0800

    Time: 0 ms (100%), Space: 17.3 MB (80.17%) - LeetHub

--- 0258-add-digits/0258-add-digits.py
+++ 0258-add-digits/0258-add-digits.py
@@ -4,14 +4,12 @@ class Solution:
         if len(num) == 1:
             return int(num)
         length = len(num)
-        
         while length > 1:
             current = 0
             for i in range(len(num)):
                 current += int(num[i])
             num = str(current)
             length = len(num)
-            
         return int(num)
 
 
