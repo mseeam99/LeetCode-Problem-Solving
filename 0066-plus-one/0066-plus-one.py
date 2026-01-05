d5d816981cd43c91c0e583275a005aa079195bfa
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-01-04 22:04:09 -0800

    Time: 0 ms (100%), Space: 17.4 MB (44.87%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,10 +1,44 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        for i in range(len(digits)-1,-1,-1):
-            if digits[i] != 9:
-                digits[i]+=1
-                return digits
-            elif digits[i] == 9:
-                digits[i] = 0
-        digits[:] = [1] + digits
-        return digits
+        another = [1]
+        carry = 0
+        digitsIndex = len(digits)-1
+        anotherIndex = len(another)-1
+        answer = []
+        while digitsIndex >= 0 or anotherIndex >= 0:
+
+            if digitsIndex >= 0:
+                one = digits[digitsIndex]
+            else:
+                one = 0
+            
+            if anotherIndex >= 0:
+                two = another[anotherIndex]
+            else:
+                two = 0
+
+            addition = carry + one + two
+            answer.append(addition%10)
+            carry = addition // 10
+
+            digitsIndex-=1
+            anotherIndex-=1
+
+        if carry != 0:
+            string = str(carry)
+            for i in range(len(string)):
+                answer.append(int(string[i]))
+
+
+        return answer[::-1]
+
+
+
+
+
+
+            
+
+            
+
+    
