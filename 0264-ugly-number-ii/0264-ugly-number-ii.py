7f020d9ed81e66f8486cd3246c26c11370c5099d
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-01-22 19:06:03 -0800

    Time: 124 ms (14.28%), Space: 19.9 MB (5.38%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.py
+++ 0264-ugly-number-ii/0264-ugly-number-ii.py
@@ -7,19 +7,18 @@ class Solution:
         myHeap = [1]
         heapq.heapify(myHeap)
 
-        count = 0
+        count = 1
 
         while heapq:
-
-            count += 1
-
             currentValue = heapq.heappop(myHeap)
+            count += 1
             for item in [2,3,5]:    
                 val = currentValue * item
                 if val not in mySet:
                     heapq.heappush(myHeap,val)
-                mySet.add(val)
-            
+                    mySet.add(val)
+                    
+                        
             if count > n:
                 break
             
