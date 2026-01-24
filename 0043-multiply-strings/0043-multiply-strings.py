ec3a236f7f55e44e179555746fc0e7f8323f2750
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-01-23 17:11:15 -0800

    Time: 57 ms (24.93%), Space: 19.5 MB (10.41%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -0,0 +1,72 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+
+        if len(num1) == 1 and len(num2) == 1:
+            return str(int(num1[0]) * int(num2[0]))
+
+        if len(num2) < len(num1):
+            num2, num1 = num1, num2
+
+        num2Index = len(num2)-1
+        answer = ""
+        store = []
+        
+        track = 0
+        while num2Index >= 0:
+            carry = 0
+            for i in range(len(num1)-1,-1,-1):
+                totalValue = (int(num2[num2Index]) * int(num1[i])) + carry
+                carry = totalValue // 10
+                currentAddingValue = (totalValue - (carry*10))
+                answer += str(currentAddingValue)
+
+            if carry != 0:
+                answer += str(carry)
+                
+            answer = answer[::-1]
+            for i in range(track):
+                answer += "0"           
+            track += 1
+
+            
+
+
+            store.append(answer)
+            answer = ""
+            num2Index -= 1
+        
+        
+        answer = 0
+        for i in range(len(store)):
+            answer += int(store[i])
+        answer = str(answer)
+        
+        return answer
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+
+
+
+
+
+        
