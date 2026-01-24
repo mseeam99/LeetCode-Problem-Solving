c3526bd733ec416228897a835a841d46e7065b92
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-01-24 14:00:45 -0800

    Time: 0 ms (100%), Space: 19.4 MB (6.48%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -6,12 +6,6 @@ class Solution:
         dp[0] = 0
         dp[1] = 1
         dp[2] = 2
-
-    
-
         for i in range(3,len(dp)):
             dp[i] = dp[i-1] + dp[i-2]
-
         return dp[-1]
-
-        
