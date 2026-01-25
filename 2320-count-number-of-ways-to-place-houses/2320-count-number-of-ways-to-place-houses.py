9bf559de9b024f5ff3e36fb72cbcbdab2fc79ee9
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-01-24 19:29:28 -0800

    Time: 67 ms (52.17%), Space: 19.7 MB (42.93%) - LeetHub

--- 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.py
+++ 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.py
@@ -7,17 +7,12 @@ class Solution:
         dp[0] = 1
         dp[1] = 2
         
-        
-
-        for i in range(2,n+1):
-            
+        for i in range(2,n+1):    
             lastValue = dp[i-1]
             veryLeftValue = dp[i-2]
             dp[i] = (lastValue + veryLeftValue) % modVal
-
         total = (dp[-1] * dp[-1]) % modVal
 
-        
         return total
 
 
