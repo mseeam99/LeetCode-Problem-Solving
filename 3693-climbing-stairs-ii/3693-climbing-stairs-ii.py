48800d66c4576079a141f0f6c606d1cd8b3f70cb
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-01-25 14:11:19 -0800

    Time: 585 ms (61.27%), Space: 33.5 MB (36.76%) - LeetHub

--- 3693-climbing-stairs-ii/3693-climbing-stairs-ii.py
+++ 3693-climbing-stairs-ii/3693-climbing-stairs-ii.py
@@ -1,21 +1,17 @@
 class Solution:
     def climbStairs(self, n: int, costs: List[int]) -> int:
 
-        dp = [float('inf')] * (n + 1)
+        INF = float('inf')
+        dp = [INF] * (n + 1)
         dp[0] = 0
 
-        for i in range((n+1)):
+        for i in range(0, n):              # current step
+            for k in (1, 2, 3):            # jump length
+                j = i + k                 # destination step
+                if j <= n:
+                    dp[j] = min(dp[j], dp[i] + costs[j - 1] + k * k)
 
-            for j in [1,2,3]:
-                prevStep = i - j
-                if prevStep >= 0 and prevStep <= len(costs)-1:
-                    cost = costs[i-1] + ((j)**2)
-                
-                    dp[i] = min(dp[i],(dp[prevStep]+cost))
-            
-        return dp[-1]
-
-
+        return dp[n]
 
 
         
