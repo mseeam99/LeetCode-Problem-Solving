cf79adc3cdc56341f58170efc8b7f76f422b6401
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-01-25 13:57:34 -0800

    Time: 765 ms (46.59%), Space: 33.7 MB (27.84%) - LeetHub

--- 3693-climbing-stairs-ii/3693-climbing-stairs-ii.py
+++ 3693-climbing-stairs-ii/3693-climbing-stairs-ii.py
@@ -4,7 +4,7 @@ class Solution:
         dp = [float('inf')] * (n + 1)
         dp[0] = 0
 
-        for i in range(1,(n+1)):
+        for i in range((n+1)):
 
             for j in [1,2,3]:
                 prevStep = i - j
