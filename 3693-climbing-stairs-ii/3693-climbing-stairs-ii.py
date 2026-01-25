99cff00e1af28cb87efa231d8c2e1bb1fc09f416
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-01-25 13:55:34 -0800

    Time: 770 ms (46.14%), Space: 33.5 MB (36.76%) - LeetHub

--- 3693-climbing-stairs-ii/3693-climbing-stairs-ii.py
+++ 3693-climbing-stairs-ii/3693-climbing-stairs-ii.py
@@ -6,15 +6,11 @@ class Solution:
 
         for i in range(1,(n+1)):
 
-          
-
             for j in [1,2,3]:
                 prevStep = i - j
                 if prevStep >= 0 and prevStep <= len(costs)-1:
                     cost = costs[i-1] + ((j)**2)
-                    
-
-
+                
                     dp[i] = min(dp[i],(dp[prevStep]+cost))
             
         return dp[-1]
