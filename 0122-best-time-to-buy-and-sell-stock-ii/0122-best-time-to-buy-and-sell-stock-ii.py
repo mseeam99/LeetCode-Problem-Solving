871fd89d76f9a6e065ad6c17da707b1ad82e4b21
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-01-26 13:40:36 -0800

    Time: 3 ms (39.13%), Space: 20.4 MB (15.32%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,21 +1,16 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        
         previousLowestValue = prices[0]
         maxProfitTotal = 0
         profit = 0
-
         for i in range(1,len(prices)):
-
             currentDayValue = prices[i]
             profit = currentDayValue-previousLowestValue
             if profit > 0:
                 maxProfitTotal += profit
                 previousLowestValue = prices[i]
                 continue
-            
             previousLowestValue = min(previousLowestValue,prices[i])
-
         return maxProfitTotal
 
 
