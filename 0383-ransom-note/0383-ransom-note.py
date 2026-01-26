4353f34c99a05117332c22d671ccebf84d3ce83f
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-01-25 20:45:07 -0800

    Time: 35 ms (9.01%), Space: 19.8 MB (13.11%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,29 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+
+        hashMap = {}
+
+        for i in range(len(magazine)):
+            if magazine[i] not in hashMap:
+                hashMap[magazine[i]] = 1
+            else:
+                hashMap[magazine[i]] += 1
+
+        for i in range(len(ransomNote)):
+            if ransomNote[i] in hashMap:
+                if hashMap[ransomNote[i]] <= 0:
+                    return False
+                else:
+                    hashMap[ransomNote[i]] -= 1
+            else:
+                return False
+
+        for key,val in hashMap.items():
+            if val < 0:
+                return False
+        
+        return True
+            
+
+            
+        
