0cd5da4b5efe1dffef6cad75013f4f84d2740815
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-01-27 17:33:45 -0800

    Time: 67 ms (5.05%), Space: 39.5 MB (5.22%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,17 +1,27 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        previousLowestValue = prices[0]
-        maxProfitTotal = 0
-        profit = 0
-        for i in range(1,len(prices)):
-            currentDayValue = prices[i]
-            profit = currentDayValue-previousLowestValue
-            if profit > 0:
-                maxProfitTotal += profit
-                previousLowestValue = prices[i]
-                continue
-            previousLowestValue = min(previousLowestValue,prices[i])
-        return maxProfitTotal
 
-
+        memo = {}
         
+        def recursion(index,canBuy):
+
+            if index >= len(prices):
+                return 0
+
+            if (index,canBuy) in memo:
+                return memo[(index,canBuy)]
+            
+            if canBuy == True:
+                pick = -prices[index] + recursion(index+1,False)
+                notPick = 0 + recursion(index+1,True)
+                result = max(pick,notPick)
+                memo[(index,canBuy)] = result
+                return result
+            else:
+                pick = prices[index] + recursion(index+1,True)
+                notPick = 0 + recursion(index+1,False)
+                result = max(pick,notPick)
+                memo[(index,canBuy)] = result
+                return result
+                
+        return recursion(0,True)
