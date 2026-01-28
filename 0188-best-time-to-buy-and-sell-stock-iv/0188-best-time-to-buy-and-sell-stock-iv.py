43a01c4cb671576993f08d3a5ac9d5078f18124f
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-01-28 13:46:48 -0800

    Time: 272 ms (10.6%), Space: 109.3 MB (5.13%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
@@ -0,0 +1,27 @@
+class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+
+        memo = {}
+        
+        def recursion(index,canBuy,allowed):
+            if (index >= len(prices)) or (allowed <= 0):
+                return 0
+
+            if (index,canBuy,allowed) in memo:
+                return memo[(index,canBuy,allowed)]
+
+            if canBuy == True:
+                pick = -prices[index] + recursion(index+1,False,allowed)
+                notPick = 0 + recursion(index+1,True,allowed)
+                profit = max(pick,notPick)
+                memo[(index,canBuy,allowed)] = profit
+                return profit
+            else:
+                pick = prices[index] + recursion(index+1,True,allowed-1)
+                notPick = 0 + recursion(index+1,False,allowed)
+                profit = max(pick,notPick)
+                memo[(index,canBuy,allowed)] = profit
+                return profit
+            
+
+        return recursion(0,True,k)
