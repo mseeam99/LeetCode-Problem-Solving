e9b133c39a7afa9248fb0fc3b86a990fe10180c5
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-01-28 15:23:26 -0800

    Time: 643 ms (6.58%), Space: 237.5 MB (5.54%) - LeetHub

---
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        
+        memo = {}
+
+        def recursion(index,canBuy):
+            if (index >= len(prices)):
+                return 0
+            
+            if (index,canBuy) in memo:
+                return memo[(index,canBuy)]
+
+            if canBuy == True:
+                pick = -prices[index] + recursion(index+1,False)
+                notPick = 0 + recursion(index+1,True)
+                maxProfit = max(pick,notPick)
+                memo[(index,canBuy)] = maxProfit
+                return maxProfit
+            else:
+                pick = (prices[index]-fee) + recursion(index+1,True)
+                notPick = 0 + recursion(index+1,False)
+                maxProfit = max(pick,notPick)
+                memo[(index,canBuy)] = maxProfit
+                return maxProfit
+
+
+        return recursion(0,True)
+
+        
