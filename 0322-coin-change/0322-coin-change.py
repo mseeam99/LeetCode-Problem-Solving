c6e08aa166bc557ff110c23760df1a8c4fec1d1c
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-01-29 15:14:39 -0800

    Time: 1776 ms (5.01%), Space: 498.7 MB (5.09%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,21 +1,21 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        INF = 10**9
+
+        INF = float("+inf")
         memo = {}
 
         def dfs(i, rem):
+
             if rem == 0:
                 return 0
+                
             if i == len(coins) or rem < 0:
                 return INF
 
             if (i, rem) in memo:
                 return memo[(i, rem)]
 
-            # pick coin i (unlimited -> stay on i)
             pick = 1 + dfs(i, rem - coins[i])
-
-            # skip coin i
             skip = dfs(i + 1, rem)
 
             memo[(i, rem)] = min(pick, skip)
