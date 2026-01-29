230dd24a44eb840479e642df5165a5e6c73753c2
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-01-29 15:55:25 -0800

    Time: 1292 ms (17.91%), Space: 63 MB (36.62%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,19 +1,28 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        
-        outerdp = [0] * (amount + 1)
-        
-        for T in range(amount + 1):
-            outerdp[T] = int(T % coins[0] == 0)
-        
-        for row in range(1, len(coins)):
-            innerdp = outerdp.copy()
-            for col in range(amount + 1):
-                notTake = outerdp[col]
-                take = 0
-                if coins[row] <= col:
-                    take = innerdp[col - coins[row]]
-                innerdp[col] = take + notTake
-            outerdp = innerdp
 
-        return outerdp[-1]
+        answer = 0
+
+        memo = [0] * (len(coins)+1)
+        for i in range(len(coins)+1):
+            memo[i] = [float("-inf")]*(amount+1)
+                
+        def recursion(index,currentAmount):
+            nonlocal answer, memo
+
+            if (index >= len(coins)) or (currentAmount > amount):
+                return 0
+            
+            if currentAmount == amount:
+                return 1
+                
+            if memo[index][currentAmount] != float("-inf"):
+                return memo[index][currentAmount]
+
+            pick = recursion(index,currentAmount + coins[index])
+            notPick = recursion(index+1,currentAmount)
+
+            memo[index][currentAmount] = pick + notPick
+            return memo[index][currentAmount]
+            
+        return recursion(0,0)
