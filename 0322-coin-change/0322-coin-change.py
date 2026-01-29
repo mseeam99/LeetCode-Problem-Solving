d2764dcd967177923b31ed4c9c4b8404de3f770c
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-01-29 15:19:10 -0800

    Time: 1749 ms (5.01%), Space: 437.5 MB (5.09%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,25 +1,24 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-
-        INF = float("+inf")
+        INF = 10**9
         memo = {}
 
         def dfs(i, rem):
 
-            if rem == 0:
+            if rem == amount:
                 return 0
                 
-            if i == len(coins) or rem < 0:
+            if rem > amount or i == len(coins):
                 return INF
 
             if (i, rem) in memo:
                 return memo[(i, rem)]
 
-            pick = 1 + dfs(i, rem - coins[i])
+            pick = 1 + dfs(i, rem + coins[i])
             skip = dfs(i + 1, rem)
 
             memo[(i, rem)] = min(pick, skip)
             return memo[(i, rem)]
 
-        ans = dfs(0, amount)
+        ans = dfs(0, 0)
         return -1 if ans >= INF else ans
