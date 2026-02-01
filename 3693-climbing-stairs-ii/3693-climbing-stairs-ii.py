18c35c98df47d1e4f802e4ae232bfe6c65759989
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-02-01 03:01:32 -0600

    Time: 5305 ms (5.06%), Space: 794.6 MB (5.06%) - LeetHub

--- 3693-climbing-stairs-ii/3693-climbing-stairs-ii.py
+++ 3693-climbing-stairs-ii/3693-climbing-stairs-ii.py
@@ -4,8 +4,10 @@ class Solution:
         memo = {}
 
         def recursion(index, prevIndex):
+
             if index > n:
                 return float("inf")
+                
             if index == n:
                 return 0
 
@@ -16,17 +18,17 @@ class Solution:
 
             if index + 1 <= n:
                 j = index + 1
-                jumpOneScore = costs[j - 1] + (1 * 1) 
+                jumpOneScore = costs[j - 1] + (1 * 1) #difference 1
                 answer = min(answer, jumpOneScore + recursion(j, index))
 
             if index + 2 <= n:
                 j = index + 2
-                jumpTwoScore = costs[j - 1] + (2 * 2)
+                jumpTwoScore = costs[j - 1] + (2 * 2)  #difference 2
                 answer = min(answer, jumpTwoScore + recursion(j, index))
 
             if index + 3 <= n:
                 j = index + 3
-                jumpThreeScore = costs[j - 1] + (3 * 3)
+                jumpThreeScore = costs[j - 1] + (3 * 3) #difference 3
                 answer = min(answer, jumpThreeScore + recursion(j, index))
 
             memo[(index, prevIndex)] = answer
