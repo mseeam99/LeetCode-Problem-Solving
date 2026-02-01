59346aaca23a5e760125f053b13e695850e96d97
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-02-01 16:10:54 -0600

    Time: 0 ms (100%), Space: 19.5 MB (12.57%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,24 +1,29 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        #Pick or not pick problem (0/1 Knapsack)
-        
+
         memo = {}
 
         def recursion(index):
             nonlocal nums, memo
 
-            if index >= len(nums):
+            if index > len(nums):
                 return 0
             
+            if index == len(nums):
+                return 0
+
             if index in memo:
                 return memo[index]
 
-            pick = nums[index] + recursion(index+2)
-            notPick = recursion(index+1)
-
-            memo[index] = max(pick,notPick)
-            return memo[index]
+            pick    = nums[index] + recursion(index+2)
+            notPick = 0 + recursion(index+1)
+            maxRobbery = max(pick, notPick)
+            memo[index] = maxRobbery
+            return maxRobbery
+        
+        return recursion(0)
             
-        ans = recursion(0)
-        return ans
 
+
+            
+        
