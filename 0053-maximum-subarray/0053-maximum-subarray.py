e2410b69ee2c68a2a502a06cb84c8274714a8492
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-02-01 18:17:17 -0600

    Time: 103 ms (8.26%), Space: 66.4 MB (7.09%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,9 +1,27 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-
-
-
         
+        maxValue = float("-inf")
+
+        def recursion(index, currentSum):
+            nonlocal maxValue
+            if index == len(nums):
+                return
+
+            # choice 1: extend subarray
+            if currentSum < 0:
+                currentSum = 0
+            currentSum = currentSum + nums[index]
+            maxValue = max(maxValue, currentSum)
+
+            recursion(index + 1, currentSum)
+
+        recursion(0, 0)
+        return maxValue
+
+
+
+        '''
         currentSum = 0
         maxSum = nums[0]
         for i in range(len(nums)):
@@ -12,7 +30,7 @@ class Solution:
             currentSum += nums[i]
             maxSum = max(maxSum,currentSum)
         return maxSum
-       
+        '''
 
 
 
