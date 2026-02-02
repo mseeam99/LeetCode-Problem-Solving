f7d8f6bd6602d794d6b559eedbfb9a03bfb47323
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-02-01 18:00:59 -0600

    Time: 0 ms (100%), Space: 19.4 MB (25.77%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -1,8 +1,6 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-
         memo = {}
-
         def recursion(index):
             nonlocal memo
             if index == 0:
@@ -11,14 +9,10 @@ class Solution:
                 return 1
             if index == 2:
                 return 1
-            
             if index in memo:
                 return memo[index]
-
             memo[index] = recursion(index-1) + recursion(index-2) + recursion(index-3)
-
             return memo[index]
-        
         return recursion(n)
             
 
