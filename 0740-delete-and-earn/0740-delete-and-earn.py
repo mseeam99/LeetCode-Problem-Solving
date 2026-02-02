7a301cae65e348a8aebc5f07e2b8cb5a040eed7c
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-02-01 21:09:06 -0600

    Time: 26 ms (6.76%), Space: 26.8 MB (7.38%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.py
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -1,55 +1,37 @@
-from typing import List
-from collections import Counter
-
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
 
         count = Counter(nums)
-        values = sorted(count.keys())   # unique sorted values
-
         elementSet = set()
         memo = {}
+        nums = list(set(nums))
 
-        def recursion(index: int, prevPicked: int) -> int:
-            if index >= len(values):
+        def recursion(index):
+            if index >= len(nums):
                 return 0
 
-            key = (index, prevPicked)
+            blocked = nums[index] in elementSet
+            key = (index, blocked)
             if key in memo:
                 return memo[key]
 
-            v = values[index]
-
-            # not pick
-            notPick = recursion(index + 1, 0)
-
-            # pick (only if not blocked)
             pick = 0
 
-            blocked = False
-            if prevPicked == 1 and index > 0 and values[index] == values[index - 1] + 1:
-                blocked = True
+            if not blocked:
+                elementSet.add(nums[index])
+                elementSet.add(nums[index] - 1)
+                elementSet.add(nums[index] + 1)
 
-            if (not blocked) and (v not in elementSet):
-                # your style
-                elementSet.add(v)
-                elementSet.add(v - 1)
-                elementSet.add(v + 1)
+                earn = nums[index] * count[nums[index]]
+                pick = earn + recursion(index + 1)
 
-                earn = v * count[v]
-                pick = earn + recursion(index + 1, 1)
+                elementSet.discard(nums[index])
+                elementSet.discard(nums[index] - 1)
+                elementSet.discard(nums[index] + 1)
 
-                # backtrack EXACTLY what you added
-                if elementSet:
-                    elementSet.remove(v)
-                if elementSet:
-                    elementSet.remove(v - 1)
-                if elementSet:
-                    elementSet.remove(v + 1)
+            notPick = recursion(index + 1)
 
             memo[key] = max(pick, notPick)
             return memo[key]
 
-        return recursion(0, 0)
+        return recursion(0)
