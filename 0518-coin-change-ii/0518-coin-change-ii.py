77bbd2726d4b16ef9177c73b4ba5b7b1fedf342c
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-02-02 19:17:09 -0600

    Time: 1708 ms (5.73%), Space: 448.2 MB (5.03%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,28 +1,34 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
 
-        answer = 0
+        memo = {}
 
-        memo = [0] * (len(coins)+1)
-        for i in range(len(coins)+1):
-            memo[i] = [float("-inf")]*(amount+1)
-                
-        def recursion(index,currentAmount):
-            nonlocal answer, memo
+        def recursion(index,currentSum):
 
-            if (index >= len(coins)) or (currentAmount > amount):
+            nonlocal memo
+
+            if index >= len(coins):
                 return 0
             
-            if currentAmount == amount:
-                return 1
-                
-            if memo[index][currentAmount] != float("-inf"):
-                return memo[index][currentAmount]
-
-            pick = recursion(index,currentAmount + coins[index])
-            notPick = recursion(index+1,currentAmount)
-
-            memo[index][currentAmount] = pick + notPick
-            return memo[index][currentAmount]
+            if currentSum > amount:
+                return 0
             
+            if currentSum == amount:
+                return 1
+
+            if (index,currentSum) in memo:
+                return memo[(index,currentSum)]
+
+            pick    = recursion(index, currentSum + coins[index])
+            notPick = recursion(index+1, currentSum + 0)
+            ways = pick + notPick
+            memo[(index,currentSum)] = ways
+            return ways
+
         return recursion(0,0)
+
+
+            
+            
+
+        
