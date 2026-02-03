0571c3de4d3700160fd95a17e9c66624d8965fdb
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-02-02 18:57:55 -0600

    Time: 185 ms (27.09%), Space: 74.2 MB (13.61%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -5,29 +5,27 @@ class Solution:
 
         def recursion(index,currentSum):
 
-            if index >= len(nums):
+            if index > len(nums):
+                return 0
+
+            if index == len(nums):
                 if currentSum == target:
                     return 1 
                 else:
                     return 0
-                
-            if (index,currentSum) in memo:
-                return memo[index,currentSum]
-            
-            add = recursion(index + 1, currentSum + nums[index])
-            subtract = recursion(index + 1, currentSum - nums[index])
 
-            memo[index,currentSum] = add + subtract
+            if (index,currentSum) in memo:
+                return memo[(index,currentSum)]
+            
+            add      = recursion(index + 1,currentSum+nums[index])
+            subtract = recursion(index + 1,currentSum-nums[index])
+
+            ways = add + subtract
+
+            memo[(index,currentSum)] = ways
 
             return add + subtract
 
 
         return recursion(0,0)
-        
-
-
-
-        
-
-
         
