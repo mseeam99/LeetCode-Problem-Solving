54ac5860d376cc7824b52a6dbb08d11ec469d291
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-02-08 02:25:50 -0600

    Time: 820 ms (5%), Space: 308.2 MB (5.37%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,9 +1,29 @@
+from typing import List
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        previousLowestValue = prices[0]
-        maxProfit = 0
-        for i in range(1,len(prices)):
-            currentDayValue = prices[i]
-            maxProfit = max(maxProfit,(currentDayValue-previousLowestValue))
-            previousLowestValue = min(previousLowestValue,prices[i])
-        return maxProfit
+        n = len(prices)
+        memo = {}
+        NEG = -10**18
+
+        def dp(i: int, holding: int) -> int:
+            if i == n:
+                return 0 if holding == 0 else NEG
+
+            key = (i, holding)
+            if key in memo:
+                return memo[key]
+
+            if holding == 0:
+                buy = -prices[i] + dp(i + 1, 1)   # pick: buy
+                skip = dp(i + 1, 0)              # not pick
+                memo[key] = max(buy, skip)
+            else:
+                sell = prices[i]                 # pick: sell (end transaction)
+                hold = dp(i + 1, 1)              # not pick
+                memo[key] = max(sell, hold)
+
+            return memo[key]
+
+        ans = dp(0, 0)
+        return max(0, ans)
