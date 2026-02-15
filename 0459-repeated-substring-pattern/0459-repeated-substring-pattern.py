e7ecf363f3b6fc0a2f602967d5c5e2c2a6bdc9b5
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-02-15 00:22:52 -0600

    Time: 32 ms (27.64%), Space: 19.5 MB (23.21%) - LeetHub

---
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.py
@@ -0,0 +1,17 @@
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        currentString = ""
+        for j in range(len(s)):
+            currentString += s[j]
+            if len(s) % len(currentString) != 0:
+                continue
+            multiplyBy = len(s) // len(currentString)
+            if multiplyBy >= 2 and (currentString * multiplyBy == s):
+                return True
+        return False
+            
+
+                
+            
+
+
