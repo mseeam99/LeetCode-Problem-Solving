b61b6d6b0738e4ca8fae1c3d235292ca89d08ced
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-02-14 23:06:45 -0600

    Time: 0 ms (100%), Space: 19.6 MB (22.12%) - LeetHub

--- 0293-flip-game/0293-flip-game.py
+++ 0293-flip-game/0293-flip-game.py
@@ -5,13 +5,10 @@ class Solution:
 
         for i in range(len(currentState)-1):
             if currentState[i] == "+" and currentState[i+1] == "+":
-
                 leftPart   = currentState[:i]
                 curentPart = "--"
                 rightPart  = currentState[i+2:]
-
                 string = leftPart + curentPart + rightPart
                 answerList.append(string)
-
         return answerList
             
