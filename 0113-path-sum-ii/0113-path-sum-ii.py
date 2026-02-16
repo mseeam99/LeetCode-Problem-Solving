32cdaac10656fa82d3a55b996276e286be14bbeb
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-02-16 00:03:03 -0600

    Time: 0 ms (100%), Space: 20.4 MB (24.01%) - LeetHub

---
+++ 0113-path-sum-ii/0113-path-sum-ii.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+
+        totalAnswer = []
+
+        def recursion(root,currentSum,currentArray):
+            if root == None:
+                return
+
+            currentSum += root.val
+            currentArray.append(root.val)
+            
+            if root.left == None and root.right == None and currentSum == targetSum:
+                totalAnswer.append(currentArray[:])
+
+            recursion(root.left,currentSum,currentArray)
+            recursion(root.right,currentSum,currentArray)
+
+            currentArray.pop()
+            
+    
+
+        recursion(root,0,[])
+        return totalAnswer
+
+        
+        
+
+        
