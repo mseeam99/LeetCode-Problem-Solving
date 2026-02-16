020734bafa1a5b73881513cff4dfd79abf017e2c
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-02-15 19:46:27 -0600

    Time: 99 ms (6.57%), Space: 36.3 MB (6.93%) - LeetHub

---
+++ 2900-longest-unequal-adjacent-groups-subsequence-i/2900-longest-unequal-adjacent-groups-subsequence-i.py
@@ -0,0 +1,24 @@
+class Solution:
+    def getLongestSubsequence(self, words: List[str], groups: List[int]) -> List[str]:
+        
+        memo = {}
+
+        def recursion(index: int, previousIndex: int) -> List[str]:
+            if index >= len(words):
+                return []
+
+            key = (index, previousIndex)
+            if key in memo:
+                return memo[key]
+
+            best = recursion(index + 1, previousIndex)
+
+            if previousIndex == -1 or groups[index] != groups[previousIndex]:
+                pick = [words[index]] + recursion(index + 1, index)
+                
+                best = pick
+
+            memo[key] = best
+            return best
+
+        return recursion(0, -1)
