4911a11cd7a5090271c69eabb4120798d0853add
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-02-20 06:49:36 -0600

    Time: 502 ms (46.45%), Space: 74.4 MB (34.89%) - LeetHub

--- 0741-cherry-pickup/0741-cherry-pickup.py
+++ 0741-cherry-pickup/0741-cherry-pickup.py
@@ -1,8 +1,14 @@
 class Solution:
     def cherryPickup(self, grid: List[List[int]]) -> int:
 
-        @lru_cache(None)
+        memo = {}
+       
         def recursion(r1,c1,r2):
+
+            if (r1,c1,r2) in memo:
+                return memo[(r1,c1,r2)]
+
+
             c2 = (r1+c1) - r2
 
             if r1 >= len(grid) or c1 >= len(grid) or r2 >= len(grid) or c2 >= len(grid):
@@ -26,6 +32,10 @@ class Solution:
                 recursion(r1,c1+1,r2+1), # right, down
                 recursion(r1,c1+1,r2),   # right, right
             )
+
+            memo[(r1,c1,r2)] = gain + best
+
+
 
             return gain + best
 
