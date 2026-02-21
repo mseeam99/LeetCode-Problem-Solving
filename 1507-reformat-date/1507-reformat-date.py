c6ac996b00b46ac1fa7c23dcae7713aa336366ec
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-02-21 16:48:13 -0600

    Time: 0 ms (100%), Space: 19.3 MB (71.9%) - LeetHub

---
+++ 1507-reformat-date/1507-reformat-date.py
@@ -0,0 +1,49 @@
+class Solution:
+    def reformatDate(self, date: str) -> str:
+
+        hashMap = {}
+        hashMap["Jan"] = "01"
+        hashMap["Feb"] = "02"
+        hashMap["Mar"] = "03"
+        hashMap["Apr"] = "04"
+        hashMap["May"] = "05"
+        hashMap["Jun"] = "06"
+        hashMap["Jul"] = "07"
+        hashMap["Aug"] = "08"
+        hashMap["Sep"] = "09"
+        hashMap["Oct"] = "10"
+        hashMap["Nov"] = "11"
+        hashMap["Dec"] = "12"
+
+        answer = ""
+        array = date.split(" ")
+        print(array)
+        for i in range(len(array)-1,-1,-1):
+            if i == 2:
+                answer += array[i]
+                answer += "-"
+            elif i == 1:
+                answer += hashMap[str(array[i])]
+                answer += "-"
+            elif i == 0:
+
+                s = ""
+                currentString = array[0]
+                for j in range(len(currentString)):
+                    if currentString[j].isnumeric():
+                        s+=currentString[j]
+
+                if len(s)==2:
+                    answer += s
+                else:
+                    p = "0"+s
+                    answer += p
+                  
+                
+
+        return answer
+
+        
+       
+
+        
