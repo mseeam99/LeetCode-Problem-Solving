9f7940736eaf0e7148f8bec04ccf036d4a81b1da
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-02-20 19:26:52 -0600

    Time: 11 ms (11.79%), Space: 19.4 MB (12.63%) - LeetHub

---
+++ 1446-consecutive-characters/1446-consecutive-characters.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maxPower(self, s: str) -> int:
+
+        maxLength = float("-inf")
+
+        leftPointer = 0
+        rightPointer = 0
+
+        maxLength = float("-inf")
+
+
+        while leftPointer <= len(s)-1:
+
+            currentChar = s[leftPointer]
+
+            while rightPointer <= len(s)-1 and s[rightPointer] == currentChar:
+                rightPointer += 1
+                
+            maxLength = max(maxLength, rightPointer-leftPointer)
+            leftPointer = rightPointer
+
+
+
+        return maxLength
+
+        
