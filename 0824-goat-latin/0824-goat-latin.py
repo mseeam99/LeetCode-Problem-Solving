e0859258db635495d29bbed0190ef83ef8c9ccb1
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-02-21 16:25:49 -0600

    Time: 0 ms (100%), Space: 19.4 MB (18.59%) - LeetHub

---
+++ 0824-goat-latin/0824-goat-latin.py
@@ -0,0 +1,41 @@
+class Solution:
+    def toGoatLatin(self, sentence: str) -> str:
+
+        array = sentence.split(" ")
+
+        for i in range(len(array)):
+            changedString = ""
+
+            currentWord = array[i]
+
+            if currentWord[0].lower() == "a" or currentWord[0].lower() == "e" or currentWord[0].lower() == "i" or currentWord[0].lower() == "o" or currentWord[0].lower() == "u":
+                modifiedWord = currentWord+"ma"
+                aAdded = "a" * (i+1)
+                modifiedWord += aAdded
+                if i != len(array)-1:
+                    modifiedWord+=" "
+                array[i] = modifiedWord
+            else:
+                for j in range(1,len(currentWord)):
+                    changedString += currentWord[j]
+                changedString += currentWord[0]
+                currentWord = changedString
+                modifiedWord = currentWord+"ma"
+                aAdded = "a" * (i+1)
+                modifiedWord += aAdded
+                if i != len(array)-1:
+                    modifiedWord+=" "
+                array[i] = modifiedWord
+            
+        
+        return "".join(array)
+
+                
+
+
+
+
+
+
+
+        
