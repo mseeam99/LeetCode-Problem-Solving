a640282499a894c31cd289fd536928b684887eea
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-02-28 19:01:00 -0600

    Time: 3 ms (15.87%), Space: 19.4 MB (35.49%) - LeetHub

---
+++ 2423-remove-letter-to-equalize-frequency/2423-remove-letter-to-equalize-frequency.py
@@ -0,0 +1,44 @@
+class Solution:
+    def equalFrequency(self, word: str) -> bool:
+
+        def checkfrequency(string):
+            hashMap = {}
+            for i in range(len(string)):
+                if string[i] not in hashMap:
+                    hashMap[string[i]] = 1
+                else:
+                    hashMap[string[i]] += 1
+            minValue = min(hashMap.values())
+            for key,val in hashMap.items():
+                if val != minValue:
+                    return False
+            return True
+                
+
+        
+        for i in range(len(word)):
+            tempString = ""
+            for j in range(0,i):
+                tempString += word[j]
+            for j in range(i+1,len(word)):
+                tempString += word[j]
+            
+            print(tempString)
+            ans = checkfrequency(tempString)
+            if ans == True:
+                return True
+
+        return False
+            
+
+
+        
+        
+
+
+
+        
+        
+        
+                
+        
