d1386de9271138373835b19550eb6e71bfb9202a
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-03-01 15:44:07 -0600

    Time: 0 ms (100%), Space: 19.2 MB (85.04%) - LeetHub

---
+++ 0830-positions-of-large-groups/0830-positions-of-large-groups.py
@@ -0,0 +1,20 @@
+class Solution:
+    def largeGroupPositions(self, s: str) -> List[List[int]]:
+
+        answer = []
+
+        lastChar = s[0]
+        pointer = 0
+        count = 0
+
+        while pointer <= len(s)-1:
+            while pointer <= len(s)-1 and s[pointer] == lastChar:
+                count+=1
+                pointer+=1
+            if count >= 3:
+                answer.append([pointer-count,pointer-1])
+            if pointer <= len(s)-1:
+                lastChar = s[pointer]
+            count = 0
+        
+        return answer
