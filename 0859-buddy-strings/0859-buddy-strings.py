bba4d814a91f95259b6c887365737c7b3c71a628
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-03-01 19:00:04 -0600

    Time: 3 ms (38.48%), Space: 19.5 MB (31.85%) - LeetHub

--- 0859-buddy-strings/0859-buddy-strings.py
+++ 0859-buddy-strings/0859-buddy-strings.py
@@ -3,7 +3,6 @@ class Solution:
     
         if len(s) != len(goal):
             return False
-        
         if s == goal:
             hashMap = {}
             for i in range(len(s)):
@@ -11,42 +10,22 @@ class Solution:
                     hashMap[s[i]] = 1
                 else:
                     hashMap[s[i]] += 1
-            
             for key,val in hashMap.items():
                 if val > 1:
                     return True
             return False
         else:
-
             firstIndex = -1
             secondIndex = -1
-
             for i in range(len(s)):
-                
                 if s[i] != goal[i]:
-
                     if firstIndex != -1 and secondIndex != -1:
                         return False
-
                     if firstIndex == -1:
                         firstIndex = i
                     else:
                         secondIndex = i
-            
             if s[firstIndex] == goal[secondIndex] and s[secondIndex] == goal[firstIndex]:
                 return True
             else:
                 return False
-
-            
-
-                
-
-
-
-
-
-
-
-        
-        
