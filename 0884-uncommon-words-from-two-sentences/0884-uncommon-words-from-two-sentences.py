57ccedc49bac30a24a38259bf4f89d8de68ee372
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-03-01 18:35:04 -0600

    Time: 0 ms (100%), Space: 19.4 MB (69.05%) - LeetHub

--- 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.py
+++ 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.py
@@ -1,39 +1,23 @@
 class Solution:
     def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
 
-        if len(s1) < len(s2):
-            s1,s2 = s2,s1
-        
-        s1 = s1.split(" ")
-        s2 = s2.split(" ")
-        
-        hashMapOne = {}
-        hashMapTwo = {}
 
-        for i in range(len(s1)):
-            if s1[i] not in hashMapOne:
-                hashMapOne[s1[i]] = 1
-            else:
-                hashMapOne[s1[i]] += 1
-        
-        for i in range(len(s2)):
-            if s2[i] not in hashMapTwo:
-                hashMapTwo[s2[i]] = 1
-            else:
-                hashMapTwo[s2[i]] += 1
+        stringArray = s1 + " " + s2
+        stringArray = stringArray.split(" ")
+        hashMap = {}
 
+        for i in range(len(stringArray)):
+            if stringArray[i] not in hashMap:
+                hashMap[stringArray[i]] = 1
+            else:
+                hashMap[stringArray[i]] += 1
+        
         answer = []
-        for key,val in hashMapOne.items():
-            if hashMapOne[key] == 1 and key not in hashMapTwo:
+        for key, val in hashMap.items():
+            if val == 1:
                 answer.append(key)
-        
-        for key,val in hashMapTwo.items():
-            if hashMapTwo[key] == 1 and key not in hashMapOne:
-                answer.append(key)
-
-
 
         return answer
-        
+                
 
         
