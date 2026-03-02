d2fea4103ff027f44e04e4d7873d5d17fadda146
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-03-02 03:30:57 -0600

    Time: 1 ms (80.23%), Space: 19.5 MB (64.53%) - LeetHub

--- 1065-index-pairs-of-a-string/1065-index-pairs-of-a-string.py
+++ 1065-index-pairs-of-a-string/1065-index-pairs-of-a-string.py
@@ -1,15 +1,41 @@
+class TrieNode:
+    def __init__(self):
+        self.hashMap = {}
+        self.isWord = False
+
 class Solution:
     def indexPairs(self, text: str, words: List[str]) -> List[List[int]]:
-        mySet = set(words)
+        root = TrieNode()
+        for i in range(len(words)):
+            node = root 
+            for j in range(len(words[i])):
+                if words[i][j] not in node.hashMap:
+                    node.hashMap[words[i][j]] = TrieNode()
+                node = node.hashMap[words[i][j]]
+            node.isWord = True
+
         answer = []
         for i in range(len(text)):
-            currentString = ""
-            for j in range(i,len(text)):
-                currentString += text[j]
-                if currentString in mySet:
-                    answer.append([i,j])
+            node = root            
+            for j in range(i, len(text)):
+                char = text[j]    
+                if char not in node.hashMap:
+                    break   
+                node = node.hashMap[char]
+                if node.isWord:
+                    answer.append([i, j])
+        
         return answer
+                
 
+
+        
+
+        
+
+
+
+        
 
                 
         
