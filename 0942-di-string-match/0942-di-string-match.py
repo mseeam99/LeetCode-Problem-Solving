5cae0238451de4f3c3ea0eba467f3e0270550040
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-03-02 04:14:39 -0600

    Time: 5 ms (17.5%), Space: 20 MB (62.74%) - LeetHub

---
+++ 0942-di-string-match/0942-di-string-match.py
@@ -0,0 +1,17 @@
+class Solution:
+    def diStringMatch(self, s: str) -> List[int]:
+        
+        answer = []
+        lo = 0
+        hi = len(s)
+
+        for i in range(len(s)):
+            if s[i] == "I":
+                answer.append(lo)
+                lo+=1
+            else:
+                answer.append(hi)
+                hi-=1
+
+        answer.append(lo)
+        return answer
