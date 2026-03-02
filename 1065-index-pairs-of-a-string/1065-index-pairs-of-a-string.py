d7c28b26fc9a10332cb231dc46a3b6fb176b3f7e
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-03-02 03:02:22 -0600

    Time: 11 ms (16.28%), Space: 19.5 MB (48.26%) - LeetHub

---
+++ 1065-index-pairs-of-a-string/1065-index-pairs-of-a-string.py
@@ -0,0 +1,18 @@
+class Solution:
+    def indexPairs(self, text: str, words: List[str]) -> List[List[int]]:
+
+        mySet = set(words)
+
+        answer = []
+
+        for i in range(len(text)):
+            currentString = ""
+            for j in range(i,len(text)):
+                currentString += text[j]
+                if currentString in mySet:
+                    answer.append([i,j])
+        return answer
+
+
+                
+        
