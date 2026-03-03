ef2fb320386faca7dbea6a658a6cf499e754f33d
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-03-03 04:19:11 -0600

    Time: 0 ms (100%), Space: 19.3 MB (90.88%) - LeetHub

--- 2586-count-the-number-of-vowel-strings-in-range/2586-count-the-number-of-vowel-strings-in-range.py
+++ 2586-count-the-number-of-vowel-strings-in-range/2586-count-the-number-of-vowel-strings-in-range.py
@@ -1,7 +1,6 @@
 class Solution:
     def vowelStrings(self, words: List[str], left: int, right: int) -> int:
 
-
         count = 0
         for i in range(left,right+1):
             if words[i][0] == "a" or words[i][0] == "e" or words[i][0] == "i" or words[i][0] == "o" or words[i][0] == "u":
