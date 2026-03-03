2d0c5aa97e61046c326dc76565e7780cea22bd9b
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-03-03 03:59:53 -0600

    Time: 3 ms (38.74%), Space: 19.4 MB (80.55%) - LeetHub

---
+++ 3813-vowel-consonant-score/3813-vowel-consonant-score.py
@@ -0,0 +1,17 @@
+class Solution:
+    def vowelConsonantScore(self, s: str) -> int:
+        vCount = 0
+        cCount = 0
+        for i in range(len(s)):
+            if s[i] == "a" or s[i] == "e" or s[i] == "i" or s[i] == "o" or s[i] == "u":
+                vCount+=1
+            elif "a" <= s[i] <= "z":
+                cCount+=1
+        if cCount != 0:
+            return floor(vCount/cCount)
+        else:
+            return 0
+
+
+
+        
