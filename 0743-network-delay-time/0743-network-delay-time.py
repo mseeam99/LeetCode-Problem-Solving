f296e4d72415a83c5e11fe4af5e01f34e5dd1725
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-03-06 17:32:30 -0600

    Time: 371 ms (13.91%), Space: 21.6 MB (18.33%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,56 +1,29 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-
+        
         hashMap = {}
         for i in range(len(times)):
             u,v,w = times[i]
             if u not in hashMap:
                 hashMap[u] = []
-            hashMap[u].append([v, w])
+            hashMap[u].append([w,v])
 
         time = 0
         minHeap = [(0,k)]
-        visitedSet = set()
-
+        mySet = set()
         while minHeap:
             weight1, node1 = heapq.heappop(minHeap)
-            if node1 in visitedSet:
+            if node1 in mySet:
                 continue
-            visitedSet.add(node1)
+            mySet.add(node1)
             time = max(time,weight1)
             if node1 in hashMap:
-                theArray = hashMap[node1]
-                for i in range(len(theArray)):
-                    node2, weight2 = theArray[i][0], theArray[i][1]
-                    if node2 not in visitedSet:
-                        heapq.heappush(minHeap,(weight1+weight2,node2))
-            
-        if len(visitedSet) == n:
+                theList = hashMap[node1]
+                for i in range(len(theList)):
+                    weight2, node2 = theList[i]
+                    heapq.heappush(minHeap,(weight1+weight2,node2))
+        
+        if len(mySet) == n:
             return time
         else:
             return -1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        return time
-        
-    
-       
-        
