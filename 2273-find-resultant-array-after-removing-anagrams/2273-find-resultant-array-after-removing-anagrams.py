81daeb84998101b3102dbf7ce35a183989346c22
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-03-10 10:37:41 -0500

    Time: 14 ms (24.91%), Space: 19.5 MB (9.17%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.py
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.py
@@ -1,13 +1,33 @@
 class Solution:
     def removeAnagrams(self, words: List[str]) -> List[str]:
-        answer = []
-        answer.append(words[0])
+        def compare(string1,string2):
+            if len(string1) != len(string2):
+                return False
+            hashMap = {}
+            for i in range(len(string1)):
+                if string1[i] not in hashMap:
+                    hashMap[string1[i]] = 1
+                else:
+                    hashMap[string1[i]] += 1
+            for i in range(len(string2)):
+                if string2[i] in hashMap:
+                    hashMap[string2[i]] -= 1
+            for key,val in hashMap.items():
+                if val != 0:
+                    return False
+            return True
+                
+        answer = [words[0]]
         for i in range(1,len(words)):
-            if Counter(answer[-1]) == Counter(words[i]):
-                pass
-            else:
-                answer.append(words[i])
+            if compare(words[i],answer[-1]) == True:
+                continue
+            answer.append(words[i])
+        
         return answer
+
+
+
+
 
 
 
