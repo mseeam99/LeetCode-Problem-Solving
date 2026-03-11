df1c3ab19952ce745f3e410747218658ebeab2a5
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-03-11 06:13:33 -0500

    Time: 12 ms (41.87%), Space: 19.2 MB (89.35%) - LeetHub

--- 3258-count-substrings-that-satisfy-k-constraint-i/3258-count-substrings-that-satisfy-k-constraint-i.py
+++ 3258-count-substrings-that-satisfy-k-constraint-i/3258-count-substrings-that-satisfy-k-constraint-i.py
@@ -1,25 +1,18 @@
 class Solution:
     def countKConstraintSubstrings(self, s: str, k: int) -> int:
-
         answer = 0
-
         for i in range(len(s)):
             zeroCount = 0
             oneCount = 0
             for j in range(i,len(s)):
-
-                
                 currentDigit = s[j]
                 if currentDigit == "1":
                     oneCount += 1
                 elif currentDigit == "0":
                     zeroCount += 1
-
                 if zeroCount > k and oneCount > k:
                     break
-                    
-                answer += 1 
-
+                answer += 1
         return answer
 
 
