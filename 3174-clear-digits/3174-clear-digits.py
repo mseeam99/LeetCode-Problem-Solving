17447df81e1a6c291c86f6240145d02ae43935a1
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-03-10 22:05:34 -0500

    Time: 0 ms (100%), Space: 19.3 MB (90.15%) - LeetHub

--- 3174-clear-digits/3174-clear-digits.py
+++ 3174-clear-digits/3174-clear-digits.py
@@ -1,15 +1,9 @@
 class Solution:
     def clearDigits(self, s: str) -> str:
-
         answer = []
-
         for i in range(len(s)):
             if "a" <= s[i] <= "z":
                 answer.append(s[i])
             else:
                 answer.pop()
-
         return "".join(answer)
-
-
-        
