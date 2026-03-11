3f6b8841037ab30f5ec94aa90b7d049ebcdec103
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-03-11 08:34:56 -0500

    Time: 4 ms (19.27%), Space: 19.2 MB (94.41%) - LeetHub

--- 3545-minimum-deletions-for-at-most-k-distinct-characters/3545-minimum-deletions-for-at-most-k-distinct-characters.py
+++ 3545-minimum-deletions-for-at-most-k-distinct-characters/3545-minimum-deletions-for-at-most-k-distinct-characters.py
@@ -1,25 +1,14 @@
 class Solution:
     def minDeletion(self, s: str, k: int) -> int:
-
         frequencyArray = [0]*27
-
         for i in range(len(s)):
             current = ord(s[i])+1 - 97
             frequencyArray[current] += 1
-
         frequencyArray.sort()
         frequencyArray = frequencyArray[::-1]
-
-        print(frequencyArray)
-
         count = 0
-
         for i in range(k,len(frequencyArray)):
             count += frequencyArray[i]
-
-
-
-
         return count 
 
         
