d889669ecd0654b7f0eb6097c478cad3b1be26bf
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-03-12 16:40:39 -0500

    Time: 0 ms (100%), Space: 19.2 MB (96.01%) - LeetHub

---
+++ 1556-thousand-separator/1556-thousand-separator.py
@@ -0,0 +1,15 @@
+class Solution:
+    def thousandSeparator(self, n: int) -> str:
+
+        n = str(n)
+        answer = ""
+        count = 0
+        for i in range(len(n)-1,-1,-1):
+            if count == 3:
+                answer += "."
+                count = 0
+            count += 1 
+            answer += n[i]
+
+        return answer[::-1]
+        
