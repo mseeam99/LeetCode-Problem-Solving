31a00e7f8281cd9847d9baed90e855f302825b7d
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-03-12 06:30:10 -0500

    Time: 0 ms (100%), Space: 19.4 MB (61.18%) - LeetHub

---
+++ 3662-filter-characters-by-frequency/3662-filter-characters-by-frequency.py
@@ -0,0 +1,9 @@
+class Solution:
+    def filterCharacters(self, s: str, k: int) -> str:
+        hashMap = Counter(s)
+        answer = ""
+        for i in range(len(s)):
+            if hashMap[s[i]] < k:
+                answer += s[i]
+        return answer
+        
