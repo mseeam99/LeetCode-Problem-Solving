98f794b9d4b70ad4db4616ede48b67d486fd938b
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-03-12 07:18:58 -0500

    Time: 0 ms (100%), Space: 19.3 MB (79.63%) - LeetHub

--- 3581-count-odd-letters-from-number/3581-count-odd-letters-from-number.py
+++ 3581-count-odd-letters-from-number/3581-count-odd-letters-from-number.py
@@ -1,6 +1,5 @@
 class Solution:
     def countOddLetters(self, n: int) -> int:
-
         n = str(n)
         hashMap = {}
         hashMap["0"] = "zero"
@@ -14,28 +13,12 @@ class Solution:
         hashMap["8"] = "eight"
         hashMap["9"] = "nine"
         string = ""
-
-        
-
         for i in range(len(n)):
             if n[i] in hashMap:
                 string += hashMap[n[i]]
-
-        print(string)
-
         counter = Counter(string)
-        print(counter)
-
         answer = 0
-
         for key,val in counter.items():
             if int(val) % 2 != 0:
                 answer += 1
-        
         return answer
-
-
-
-
-
-        
