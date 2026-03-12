bb9cbfb2399340c76aad1bff5ad80bf39df512a4
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-03-12 07:01:48 -0500

    Time: 5 ms (11.8%), Space: 19.4 MB (61.18%) - LeetHub

--- 3582-generate-tag-for-video-caption/3582-generate-tag-for-video-caption.py
+++ 3582-generate-tag-for-video-caption/3582-generate-tag-for-video-caption.py
@@ -1,15 +1,11 @@
 class Solution:
     def generateTag(self, caption: str) -> str:
-
         answer = "#"
         for i in range(len(caption)):
-
             if len(answer) >= 100:
                 break
-
             if caption[i].isalpha() == False:
                 continue
-
             elif caption[i-1] == " ":
                 if answer[len(answer)-1] == "#":
                     answer += caption[i].lower()
@@ -20,9 +16,5 @@ class Solution:
                     answer += caption[i].lower()
                     continue
                 answer += caption[i].lower()
-
-
-
-
         return answer
         
