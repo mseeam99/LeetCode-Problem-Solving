f93d2d80d176b0b90c6da8307aea9dc53b274fdf
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-03-12 09:02:48 -0500

    Time: 0 ms (100%), Space: 19.2 MB (91.27%) - LeetHub

--- 3136-valid-word/3136-valid-word.py
+++ 3136-valid-word/3136-valid-word.py
@@ -1,6 +1,5 @@
 class Solution:
     def isValid(self, word: str) -> bool:
-
         if len(word) < 3:
             return False
         hashMap = {}
@@ -11,18 +10,13 @@ class Solution:
         hashMap["u"] = 1
         vowelFound = False
         consoFound = False
-
         for i in range(len(word)):
             if (word[i].isalnum() == False):
                 return False
-
             if word[i].lower() in hashMap:
                 vowelFound = True
             elif word[i].lower() not in hashMap and word[i].isdigit() == False:
                 consoFound = True
-            
-
-
         if (consoFound == True and vowelFound == True):
             return True 
         else:
