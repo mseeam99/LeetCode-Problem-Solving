0ee634162a55c798651b665a047f8313c11edbd4
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-03-13 07:27:44 -0500

    Time: 15 ms (39.55%), Space: 19.4 MB (27.07%) - LeetHub

---
+++ 1154-day-of-the-year/1154-day-of-the-year.py
@@ -0,0 +1,53 @@
+import calendar 
+class Solution:
+    def dayOfYear(self, date: str) -> int:
+        hashMap = {
+            "01": 0,
+            "02": 31,
+            "03": 59,
+            "04": 90,
+            "05": 120,
+            "06": 151,
+            "07": 181,
+            "08": 212,
+            "09": 243,
+            "10": 273,
+            "11": 304,
+            "12": 334
+        }
+
+        year, month, day = date.split("-")
+        totalDays = hashMap[month] + int(day)
+        if calendar.isleap(int(year)) and int(month) > 2:
+            totalDays += 1
+        return totalDays
+
+            
+
+
+
+        
+
+        
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+            
+
+
+
+
+
+
+        
