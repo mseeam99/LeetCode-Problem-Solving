7aeb3893c85b8480b39fbb23a45a3b0e58b9178c
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-03-13 08:09:15 -0500

    Time: 11 ms (28.57%), Space: 19.1 MB (99.37%) - LeetHub

--- 1165-single-row-keyboard/1165-single-row-keyboard.py
+++ 1165-single-row-keyboard/1165-single-row-keyboard.py
@@ -3,16 +3,12 @@ class Solution:
         hashMap = {}
         for i in range(len(keyboard)):
             hashMap[keyboard[i]] = i
-
         time = 0
         for i in range(len(word)):
             if i == 0:
                 time += hashMap[word[i]]
             else:
                 time += abs(hashMap[word[i]] - hashMap[word[i-1]])
-
-
-
         return time
 
         
