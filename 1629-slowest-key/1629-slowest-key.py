f5abe41fc2117ed54f128a8249fc253fb628e339
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-03-15 16:32:03 -0500

    Time: 3 ms (42.51%), Space: 19.3 MB (87%) - LeetHub

--- 1629-slowest-key/1629-slowest-key.py
+++ 1629-slowest-key/1629-slowest-key.py
@@ -1,9 +1,7 @@
 class Solution:
     def slowestKey(self, releaseTimes: List[int], keysPressed: str) -> str:
-
         maxTime = 0
         maxChar = 0
-
         for i in range(len(releaseTimes)):
             if i == 0:
                 newMaxTime = abs(releaseTimes[i]-0)
@@ -13,17 +11,12 @@ class Solution:
                 elif newMaxTime == maxTime:
                     maxChar = max(maxChar, keysPressed[i])
                 continue
-                   
-
-                
-            
             newMaxTime = abs(releaseTimes[i]-releaseTimes[i-1])
             if newMaxTime > maxTime:
                 maxTime = newMaxTime
                 maxChar = keysPressed[i]
             elif newMaxTime == maxTime:
                     maxChar = max(maxChar, keysPressed[i])
-               
         return maxChar
             
 
