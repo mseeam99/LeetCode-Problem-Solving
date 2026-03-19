b54123f4384229dec4cfdf27d368ab872c196f10
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-03-19 16:30:04 -0500

    Time: 357 ms (21.94%), Space: 21.8 MB (15.27%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,29 +1,36 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        
-        hashMap = {}
-        for i in range(len(times)):
-            u,v,w = times[i]
-            if u not in hashMap:
-                hashMap[u] = []
-            hashMap[u].append([w,v])
 
-        time = 0
-        minHeap = [(0,k)]
+        hashMap = defaultdict(list)
+        for i in range(len(times)):
+            u,v,w = times[i][0],times[i][1],times[i][2]
+            hashMap[u].append([v,w])
+
+        minHeap = [[0,k]]
+        heapq.heapify(minHeap)
         mySet = set()
+        maxTime = float("-inf")
         while minHeap:
-            weight1, node1 = heapq.heappop(minHeap)
-            if node1 in mySet:
+            time, source = heapq.heappop(minHeap)
+            if source in mySet:
                 continue
-            mySet.add(node1)
-            time = max(time,weight1)
-            if node1 in hashMap:
-                theList = hashMap[node1]
-                for i in range(len(theList)):
-                    weight2, node2 = theList[i]
-                    heapq.heappush(minHeap,(weight1+weight2,node2))
-        
+            mySet.add(source)
+            maxTime = max(maxTime,time)
+            listOfSourceAndTime = hashMap[source]
+            for i in range(len(listOfSourceAndTime)):
+                if listOfSourceAndTime[i][0] not in mySet:
+                    newSource = listOfSourceAndTime[i][0]
+                    newTime = listOfSourceAndTime[i][1]
+                    newTime += time
+                    heapq.heappush(minHeap,[newTime,newSource])    
+               
+
         if len(mySet) == n:
-            return time
+            return maxTime
         else:
             return -1
+
+
+
+
+        
