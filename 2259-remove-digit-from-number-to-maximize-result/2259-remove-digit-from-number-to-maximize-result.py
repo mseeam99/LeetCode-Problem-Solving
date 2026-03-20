2daeb3c98f2c29e2a17389bc938a24f558f49e0b
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-03-19 21:00:05 -0500

    Time: 0 ms (100%), Space: 19 MB (99.86%) - LeetHub

--- 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.py
+++ 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.py
@@ -3,12 +3,12 @@ class Solution:
         for i in range(len(number)-1):
             if number[i] == digit and number[i+1]>digit:
                 return number[:i] + number[i+1:]
+
         lastidx = 0
         for i in range(len(number)-1,-1,-1):
             if number[i] == digit:
                 lastidx = i
                 break
-        
         return number[:lastidx] + number[lastidx+1:]
                 
         
