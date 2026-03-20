1bd48e58a050829346bceeba28f883e92868ba1f
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-03-20 16:20:24 -0500

    Time: 7 ms (18.02%), Space: 20.1 MB (23.86%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -5,15 +5,12 @@ class Solution:
         graph = defaultdict(list)
         for start, end in tickets:
             graph[start].append(end)
-
         result = []
-
         def dfs(src):
             while graph[src]:
                 next_dst = graph[src].pop()
                 dfs(next_dst)
             result.append(src)
-
         dfs("JFK")
         return result[::-1]
         
