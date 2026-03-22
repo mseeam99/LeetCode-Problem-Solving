3ef7adf17e86f3ddb03950a6027c8a25df645c5c
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-03-22 14:44:36 -0500

    Time: 182 ms (19.87%), Space: 21.8 MB (22.91%) - LeetHub

--- 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.py
+++ 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.py
@@ -1,19 +1,16 @@
 class Solution:
     def minCost(self, grid: List[List[int]]) -> int:
 
-        minHeap = [[0,0,0]] #cost,row,col
+        minHeap = [[0,0,0]]
         heapq.heapify(minHeap)
         visitedSet = set()
         directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
        
-
-
         while minHeap:
             cost,row,col = heapq.heappop(minHeap)
             if (row,col) in visitedSet:
                 continue
             visitedSet.add((row,col))
-
 
             if row == len(grid)-1 and col == len(grid[0])-1:
                 return cost
@@ -21,8 +18,6 @@ class Solution:
             if row < 0 or row >= len(grid) or col < 0 or col >= len(grid[0]):
                 continue
             
-
-
             for i in range(len(directions)):
                 rowIdx, colIdx = directions[i][0],directions[i][1]
                 newRow, newCol = row + rowIdx, col + colIdx
@@ -35,6 +30,8 @@ class Solution:
                     newCost = cost
                 else:
                     newCost = cost + 1
+
+                
                 heapq.heappush(minHeap,[newCost,newRow,newCol])
 
         
