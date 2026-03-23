1b548c400a79a3539071a8d6aa984f9f2e53e5b6
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-03-23 15:25:36 -0500

    Time: 731 ms (8.34%), Space: 22.1 MB (5.09%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
@@ -6,28 +6,20 @@ class Solution:
             start,end,weight = edges[i][0],edges[i][1],edges[i][2]
             hashMap[start].append([weight,end])
             hashMap[end].append([weight,start])
-            # start -> [[weight,end]]
-        
-        #print(hashMap)
-
+            
         answer = []
         for i in range(n):
             answer.append(set())
 
         def function(it):
-            minHeap = [[0,it]] # weight, end
+            minHeap = [[0,it]]
             heapq.heapify(minHeap)
             visitedSet = set()
-
             while minHeap:
-
                 weight,end = heapq.heappop(minHeap)
-
                 if end in visitedSet or weight > distanceThreshold:
                     continue
                 visitedSet.add(end)
-
-
                 theList = hashMap[end]
                 for i in range(len(theList)):
                     newWeight, newEnd = theList[i][0]+weight,theList[i][1]
@@ -36,13 +28,9 @@ class Solution:
                             answer[it].add(newEnd)
                     heapq.heappush(minHeap,[newWeight, newEnd])
         
-        
-
         for it in range(n):
             function(it)
 
-        
-        print(answer)
         minIdx = 0
         minLength = float("inf")
         for i in range(len(answer)):
@@ -50,19 +38,3 @@ class Solution:
                 minLength = len(answer[i])
                 minIdx = i
         return minIdx
-
-
-
-
-
-                
-
-
-
-
-
-
-
-
-
-        
