a7d1b2deb28f77a0a3e88ee087eae487859ff82b
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-03-24 15:48:09 -0500

    Time: 4582 ms (5.01%), Space: 21.5 MB (10.34%) - LeetHub

--- 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.py
+++ 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.py
@@ -11,32 +11,25 @@ class Solution:
         def function(s, t):
             if (s, t) in memo:
                 return memo[(s, t)]
-
             if s == t:
                 memo[(s, t)] = 0
                 return 0
-
             minHeap = [[0, s]]  # cost, destination
             heapq.heapify(minHeap)
             visitedSet = set()
-
             while minHeap:
                 cost, destination = heapq.heappop(minHeap)
-
                 if destination == t:
                     memo[(s, t)] = cost
                     return cost
-
                 if destination in visitedSet:
                     continue
                 visitedSet.add(destination)
-
                 theOtherDestinationsList = hashMap[destination]
                 for i in range(len(theOtherDestinationsList)):
                     newCost = theOtherDestinationsList[i][0] + cost
                     newDestination = theOtherDestinationsList[i][1]
                     heapq.heappush(minHeap, [newCost, newDestination])
-
             memo[(s, t)] = -1
             return -1
 
