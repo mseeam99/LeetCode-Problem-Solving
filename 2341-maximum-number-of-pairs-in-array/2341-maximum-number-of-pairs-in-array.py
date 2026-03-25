d862a2728fb3d97aa8eb5fb50813cdbe0f125533
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-03-24 23:33:39 -0500

    Time: 0 ms (100%), Space: 19.5 MB (21%) - LeetHub

---
+++ 2341-maximum-number-of-pairs-in-array/2341-maximum-number-of-pairs-in-array.py
@@ -0,0 +1,25 @@
+class Solution:
+    def numberOfPairs(self, nums: List[int]) -> List[int]:
+
+        stack = []
+        heapq.heapify(nums)
+        poppedCount = 0
+
+        while nums:
+
+            number = heapq.heappop(nums)
+
+            if stack and number == stack[-1]:
+                stack.pop()
+                poppedCount += 1
+            else:
+                stack.append(number)
+
+        return [poppedCount,len(stack)]
+            
+
+
+
+
+
+        
