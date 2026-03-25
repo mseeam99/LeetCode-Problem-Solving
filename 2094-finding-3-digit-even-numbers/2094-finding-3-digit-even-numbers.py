0f59f9e43b3e586c0ca1c4f57324a8c406b2f1d7
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-03-24 23:25:24 -0500

    Time: 6246 ms (5.04%), Space: 20.7 MB (12.88%) - LeetHub

---
+++ 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.py
@@ -0,0 +1,43 @@
+class Solution:
+    def findEvenNumbers(self, digits: List[int]) -> List[int]:
+
+        mySet = set()
+        used = [False]*len(digits)
+
+        def recursion(string):
+
+
+            if len(string) == 3:
+                if int(string) % 2 == 0 and string[0] != "0":
+                    mySet.add(int(string))
+                return
+
+            
+            for i in range(len(digits)):
+
+                if used[i] == True:
+                    continue
+
+                if len(string) == 0 and digits[i] == 0:
+                    continue
+
+                # last digit must be even
+                if len(string) == 2 and digits[i] % 2 != 0:
+                    continue
+
+                used[i] = True
+                
+                recursion(string + str(digits[i]))
+
+                used[i] = False
+
+
+
+
+        recursion("")
+        print(mySet)
+        return sorted(list(mySet))
+
+
+
+        
