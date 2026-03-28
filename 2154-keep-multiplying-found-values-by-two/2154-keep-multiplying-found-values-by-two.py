ef7e46d257e1aa9fdd4b8d2979c0e7aec99a211b
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-03-27 21:25:00 -0500

    Time: 0 ms (100%), Space: 19.4 MB (60.38%) - LeetHub

---
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findFinalValue(self, nums: List[int], original: int) -> int:
+
+        hashMap = {}
+        for i in range(len(nums)):
+            if nums[i] not in hashMap:
+                hashMap[nums[i]] = 1
+            else:
+                hashMap[nums[i]] += 1
+
+        booleanRunning = True
+        while booleanRunning:
+            if original not in hashMap:
+                booleanRunning = False
+                break
+            original = original * 2
+
+        return original 
+
+
+           
+                
+        
