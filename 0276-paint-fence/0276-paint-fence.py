cbd6b6a373d49a3d9fece4023d0e13ba9679e7e1
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-04-03 23:28:38 -0500

    Time: 0 ms (100%), Space: 19.3 MB (55.19%) - LeetHub

---
+++ 0276-paint-fence/0276-paint-fence.py
@@ -0,0 +1,29 @@
+class Solution:
+    def numWays(self, n: int, k: int) -> int:
+
+        memo = {}
+
+        def recursion(index):
+
+            if index == 1:
+                return k
+            if index == 2:
+                return k*k
+
+            if index in memo:
+                return memo[index]
+
+
+            memo[index] = (k-1) * (recursion(index-1)+recursion(index-2))
+
+            return memo[index]
+
+        
+        return recursion(n)
+
+
+
+
+
+
+     
