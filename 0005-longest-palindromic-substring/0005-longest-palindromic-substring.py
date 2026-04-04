5926bbcc1ffb2198936613aa70bd7ea781582de2
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-04-04 17:42:33 -0500

    Time: 270 ms (57.09%), Space: 19.4 MB (52.93%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,25 +1,54 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        maxLPalindromeLength = float("-inf")
-        maxLPalindromeString = ""
-        for i in range(len(s)):
-            leftPointer = i
-            rightPointer = i
-            while leftPointer >= 0 and rightPointer <= len(s)-1 and s[leftPointer] == s[rightPointer]:
-                leftPointer -= 1
-                rightPointer += 1
-            currentLength = rightPointer - leftPointer - 1 
-            if currentLength > maxLPalindromeLength:
-                maxLPalindromeLength = currentLength
-                maxLPalindromeString = s[leftPointer + 1:rightPointer]
 
-            leftPointer = i
-            rightPointer = i + 1
-            while leftPointer >= 0 and rightPointer <= len(s)-1 and s[leftPointer] == s[rightPointer]:
-                leftPointer -= 1
-                rightPointer += 1
-            currentLength = rightPointer - leftPointer - 1
-            if currentLength > maxLPalindromeLength:
-                maxLPalindromeLength = currentLength
-                maxLPalindromeString = s[leftPointer + 1:rightPointer]        
-        return maxLPalindromeString
+        if len(s) == 1:
+            return s
+        elif len(s) == 2:
+            if s[0] != s[1]:
+                return s[0]
+            else:
+                return s
+
+        longestString = ""
+
+        def oddExpandTwoSideAndCheck(i):
+            nonlocal longestString
+            tempLongestString = ""
+            leftIndex = i-1
+            rightIndex = i+1
+            while leftIndex >= 0 and rightIndex <= len(s)-1:
+                if s[leftIndex] == s[rightIndex]:
+                    leftIndex-=1 
+                    rightIndex+=1
+                else:
+                    break
+            tempLongestString = s[leftIndex+1:rightIndex]            
+            if len(tempLongestString) >= len(longestString):
+                longestString = tempLongestString
+                
+
+        def evenExpandTwoSideAndCheck(i):
+            nonlocal longestString
+            tempLongestString = ""
+            leftIndex = i-1
+            rightIndex = i
+            while leftIndex >= 0 and rightIndex <= len(s)-1:
+                if s[leftIndex] == s[rightIndex]:
+                    leftIndex-=1 
+                    rightIndex+=1
+                else:
+                    break
+            tempLongestString = s[leftIndex+1:rightIndex]
+            if len(tempLongestString) >= len(longestString):
+                longestString = tempLongestString
+                
+        for i in range(len(s)):
+            
+            oddExpandTwoSideAndCheck(i)
+            evenExpandTwoSideAndCheck(i)
+
+        
+
+        return longestString
+
+        
