92506635d0457873ab8e6d229a063e4499199d21
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-04-03 21:08:42 -0500

    Time: 3 ms (55.32%), Space: 21.5 MB (14.85%) - LeetHub

--- 0256-paint-house/0256-paint-house.py
+++ 0256-paint-house/0256-paint-house.py
@@ -5,26 +5,25 @@ class Solution:
 
         def recursion(index,color):
 
-            if (index, color) in memo:
-                return memo[(index, color)]
+            if (index,color) in memo:
+                return memo[(index,color)]
 
             if index >= len(costs):
                 return 0
-
-            totalCost = 0
+            
+            minimumCost = 0
 
             if color == 0:
-                totalCost = costs[index][color] + min(recursion(index+1,1) , recursion(index+1,2))
+                minimumCost = costs[index][color] + min(recursion(index+1,1),recursion(index+1,2))
             elif color == 1:
-                totalCost = costs[index][color] + min(recursion(index+1,0) , recursion(index+1,2))
-            else:
-                totalCost = costs[index][color] + min(recursion(index+1,0) , recursion(index+1,1))
+                minimumCost = costs[index][color] + min(recursion(index+1,0),recursion(index+1,2))
+            if color == 2:
+                minimumCost = costs[index][color] + min(recursion(index+1,0),recursion(index+1,1))
             
-            memo[(index, color)] = totalCost
-            return memo[(index, color)]
-           
+            memo[(index,color)] = minimumCost
+            return minimumCost
+
         return min(recursion(0,0),recursion(0,1),recursion(0,2))
             
 
-        
         
