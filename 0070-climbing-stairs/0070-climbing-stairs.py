223b86027d109db30c35e3037e8d9dc11a609094
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-04-05 23:45:23 -0500

    Time: 0 ms (100%), Space: 19.3 MB (32.38%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,6 +1,7 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-
+        '''
+        #memo
         memo = {}
         def recursion(index):
             nonlocal memo
@@ -13,14 +14,23 @@ class Solution:
             ways = recursion(index+1) + recursion(index+2)
             memo[index] = ways
             return ways
-            
-            
-
-
-
-
         return recursion(0)
+        '''
 
+        #tabulation
+        if n == 0:
+            return 0 
+        elif n == 1:
+            return 1
+        if n == 2:
+            return 2  
+        dp = [0] * (n+1)
+        dp[0] = 0
+        dp[1] = 1
+        dp[2] = 2
+        for i in range(3,n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
 
         
         
