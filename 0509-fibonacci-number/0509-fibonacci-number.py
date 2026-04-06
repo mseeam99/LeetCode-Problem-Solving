7700f94f6d1cf16c0bb1e53f99be3034ee41bc1e
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-04-05 23:09:08 -0500

    Time: 51 ms (47.56%), Space: 19.3 MB (10.25%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,34 +1,15 @@
 class Solution:
     def fib(self, n: int) -> int:
-
         if n == 0:
-            return 0 
-
+            return 0
         if n == 1:
-            return 1 
-
-        memo = {}
-
-        def recursion(index):
-            nonlocal memo
-
-            if index == 0:
-                return 0
-
-            if index == 1:
-                return 1
-            
-            if index in memo:
-                return memo[index]
-            
-            oneStepLast = recursion(index-1)
-            twoStepLast = recursion(index-2)
-
-            memo[index] = oneStepLast + twoStepLast
-
-            return memo[index]
-            
-        return recursion(n)
+            return 1
+        dp = [0] * (n+1)
+        dp[0] = 0
+        dp[1] = 1
+        for i in range(2,n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[-1]
 
 
         
