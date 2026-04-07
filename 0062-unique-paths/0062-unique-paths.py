012196796217abdf7339d6e5e551b231bf7287a5
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-04-07 17:33:44 -0500

    Time: 3 ms (20.09%), Space: 19.3 MB (55.46%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -20,21 +20,21 @@ class Solution:
 
         return recursion(m-1,n-1)
         '''
-
+        '''
         dp = []
         for i in range(m):
-            tempDp = [-1]*n
+            tempDp = [0]*n
             dp.append(tempDp)
 
         def recursion(row,col):
-            if dp[row][col] != -1:
+            if dp[row][col] != 0:
                 return dp[row][col]
-
-            if row == 0 and col == 0:
-                return 1
 
             if row < 0 or col < 0:
                 return 0
+
+            if row == 0 and col == 0:
+                return 1
 
             left = recursion(row,col-1)
             up   = recursion(row-1,col)
@@ -43,8 +43,42 @@ class Solution:
             return left+up
         
         ans = recursion(m-1,n-1)
-        
+
         for i in range(len(dp)):
             print(dp[i])
 
         return ans
+        '''
+
+
+
+        dp = []
+        for i in range(m):
+            tempDp = [0]*n
+            dp.append(tempDp)
+
+        for i in range(len(dp)):
+            for j in range(len(dp[i])):
+                if i == 0 or j == 0:
+                    dp[i][j] = 1
+                else:
+                    dp[i][j] = 0
+
+        for i in range(m):
+            for j in range(n):
+                if i == 0 or j == 0:
+                    continue
+                else:
+                    dp[i][j] = dp[i][j-1] + dp[i-1][j]
+
+        return dp[m-1][n-1]
+
+
+
+
+
+
+
+        
+
+
