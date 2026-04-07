e798c617599993bf01c54cffde9eeeb4d2ce4e36
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-04-07 18:28:32 -0500

    Time: 0 ms (100%), Space: 19.3 MB (55.46%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -53,7 +53,8 @@ class Solution:
         for i in range(m):
             curr = [0] * n
             for j in range(n):
-                if i == 0 or j == 0:
+                
+                if i == 0 and j == 0:
                     curr[j] = 1
                 else:
                     up   = prev[j]
