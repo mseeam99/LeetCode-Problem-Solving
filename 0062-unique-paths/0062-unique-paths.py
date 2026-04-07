34ceb5decd2d381ae2df02fa090dc5f335ef59c7
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-04-07 17:17:02 -0500

    Time: 3 ms (20.09%), Space: 19.5 MB (30.61%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,8 +1,7 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-
+        '''        
         memo = {}
-
         def recursion(row,col):
 
             if row == 0 and col == 0:
@@ -14,9 +13,29 @@ class Solution:
             if (row,col) in memo:
                 return memo[row,col]
         
-            right = recursion(row,col-1)
-            down = recursion(row-1,col)
-            memo[(row,col)] = right+down
-            return right+down
+            left = recursion(row,col-1)
+            up   = recursion(row-1,col)
+            memo[(row,col)] = left+up
+            return left+up
+
+        return recursion(m-1,n-1)
+        '''
+
+        dp = []
+        for i in range(m):
+            tempDp = [-1]*n
+            dp.append(tempDp)
+
+        def recursion(row,col):
+            if dp[row][col] != -1:
+                return dp[row][col]
+            if row == 0 and col == 0:
+                return 1
+            if row < 0 or col < 0:
+                return 0
+            left = recursion(row,col-1)
+            up   = recursion(row-1,col)
+            dp[row][col] = left+up
+            return left+up
 
         return recursion(m-1,n-1)
