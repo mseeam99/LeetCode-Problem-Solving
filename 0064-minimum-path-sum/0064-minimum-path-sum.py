e400fdcae8d3f237fd8cde0381cd83d634beb50f
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-04-07 21:58:21 -0500

    Time: 54 ms (5.06%), Space: 32 MB (8.04%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -4,23 +4,26 @@ class Solution:
         memo = {}
 
         def recursion(row,col):
+
             nonlocal memo
 
             if row < 0 or col < 0 or row >= len(grid) or col >= len(grid[0]):
                 return float("inf")
 
-            if row == len(grid)-1 and col == len(grid[0])-1:
+            if row == 0 and col == 0:
                 return grid[row][col]
             
+
             if (row,col) in memo:
                 return memo[(row,col)]
 
-            right =  grid[row][col] + recursion(row,col+1)
-            down  =  grid[row][col] + recursion(row+1,col)
+
+            right =  grid[row][col] + recursion(row,col-1)
+            down  =  grid[row][col] + recursion(row-1,col)
             memo[(row,col)] = min(right,down)
             return min(right,down)
         
-        return recursion(0,0)
+        return recursion(len(grid)-1,len(grid[0])-1)
        
 
             
