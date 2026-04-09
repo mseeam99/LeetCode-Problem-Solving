e12292ddd176c4321a9b1498f97b96f118819fb8
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-04-09 18:44:18 -0500

    Time: 8 ms (16.41%), Space: 19.9 MB (91.02%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,50 +1,34 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
 
-        dp = []
-        for i in range(len(triangle)):
-            tempArray = [0] * len(triangle[i])
-            dp.append(tempArray)
-        for i in range(len(dp)):
-            for j in range(len(dp[i])):
-                dp[i][j] = 0
-        dp[0][0] = triangle[0][0]
-
-        for i in range(len(dp)):
-            print(dp[i])
-
-
-
-
-
-
-
-
-
+        prev = [0] * len(triangle[0])
+        prev[0] = triangle[0][0]
+       
         for i in range(1,len(triangle)):
+
+            temp = [0] * len(triangle)
 
             for j in range(len(triangle[i])):
 
                 if i-1 >= 0 and i-1 <= len(triangle)-1 and j >= 0 and j <= len(triangle[i-1])-1:
-                    straightUp = dp[i-1][j]
+                    straightUp = prev[j]
                 else:
                     straightUp = float("inf")
 
                 if i-1 >= 0 and i-1 <= len(triangle)-1 and j-1 >= 0 and j-1 <= len(triangle[i-1])-1:
-                    rightSidUp =  dp[i-1][j-1]
+                    rightSidUp =  prev[j-1]
                 else:
                     rightSidUp =  float("inf")
 
                 minPath = triangle[i][j] + min(straightUp,rightSidUp)
 
-                dp[i][j] = minPath
+                temp[j] = minPath
+
+            prev = temp
                 
 
-        print()
-        for i in range(len(dp)):
-            print(dp[i])
 
-        return min(dp[-1])
+        return min(prev)
 
 
         
