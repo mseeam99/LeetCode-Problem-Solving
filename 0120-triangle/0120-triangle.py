d35cb8abba0ecdfc32ba66bb999a5317f244e07e
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-04-09 18:45:33 -0500

    Time: 3 ms (71.53%), Space: 20 MB (71.35%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,33 +1,21 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-
         prev = [0] * len(triangle[0])
         prev[0] = triangle[0][0]
-       
         for i in range(1,len(triangle)):
-
             temp = [0] * len(triangle)
-
             for j in range(len(triangle[i])):
-
                 if i-1 >= 0 and i-1 <= len(triangle)-1 and j >= 0 and j <= len(triangle[i-1])-1:
                     straightUp = prev[j]
                 else:
                     straightUp = float("inf")
-
                 if i-1 >= 0 and i-1 <= len(triangle)-1 and j-1 >= 0 and j-1 <= len(triangle[i-1])-1:
                     rightSidUp =  prev[j-1]
                 else:
                     rightSidUp =  float("inf")
-
                 minPath = triangle[i][j] + min(straightUp,rightSidUp)
-
                 temp[j] = minPath
-
             prev = temp
-                
-
-
         return min(prev)
 
 
