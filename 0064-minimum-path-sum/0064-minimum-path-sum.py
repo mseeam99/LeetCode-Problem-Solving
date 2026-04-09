c0e8be96d330cfc3ffbb3fd8ccf5b744bf14d314
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-04-08 23:47:16 -0500

    Time: 10 ms (61.78%), Space: 20.4 MB (95.73%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -17,6 +17,8 @@ class Solution:
         return recursion(len(grid)-1,len(grid[0])-1)
         '''
         
+        '''
+        #Tabulation
         dp = []
         for i in range(len(grid)):
             currentRow = [0] * len(grid[i])
@@ -24,26 +26,59 @@ class Solution:
 
         for i in range(len(grid)):
             for j in range(len(grid[i])):
-
                 if i == 0 and j == 0:
                     dp[i][j] = grid[i][j]
                     continue
-
                 if i-1 < 0:
                     upMax = float("inf")
                 else:
                     upMax = dp[i-1][j]
-
                 if j-1 < 0:
                     leftMax = float("inf")
                 else:
                     leftMax = dp[i][j-1]
-                
                 up   = grid[i][j] + upMax
                 left = grid[i][j] + leftMax
                 dp[i][j] = min(up,left)
-
         return dp[len(dp)-1][len(dp[0])-1]
+        '''
+
+
+        
+        #Tabulation
+
+        prev = grid[0][:]
+
+        for i in range(1,len(prev)):
+
+            prev[i] += prev[i-1]
+        
+        for i in range(1,len(grid)):
+
+            temp = [0] * len(grid[i])
+
+            for j in range(len(grid[i])):
+
+                if j == 0:
+                    temp[j] = grid[i][j] + prev[j]
+                    continue
+            
+                up   = grid[i][j] + prev[j]
+                left = grid[i][j] + temp[j-1]
+                
+                temp[j] = min(up,left)
+            
+            prev = temp
+
+        print(prev)
+
+        return prev[-1]
+
+
+
+
+
+                
                 
                
 
