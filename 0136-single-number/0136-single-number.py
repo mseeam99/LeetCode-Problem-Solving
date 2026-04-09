d4d412cdcecfb4abd34cce7aa1ea5c977c8bc2eb
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-04-09 18:54:39 -0500

    Time: 0 ms (100%), Space: 21.7 MB (7.24%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,7 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        xor = 0
-        for i in range(len(nums)):
-            xor = xor ^ nums[i]
-        return xor
-        
+
+        hashMap = Counter(nums)
+        for k,v in hashMap.items():
+            if v == 1:
+                return k
