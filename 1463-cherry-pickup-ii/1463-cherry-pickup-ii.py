50a1159fd64dd0f03b9a21c89911c0bfcb17c09b
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-04-09 22:52:43 -0500

    Time: 1083 ms (37.82%), Space: 74.5 MB (18.61%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.py
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.py
@@ -1,41 +1,24 @@
 class Solution:
     def cherryPickup(self, grid: List[List[int]]) -> int:
-
         memo = {}
-
         def recurison(row,col1,col2):
-
             nonlocal memo
-
             if (row,col1,col2) in memo:
                 return memo[(row,col1,col2)]
-
             if row < 0 or row >= len(grid) or col1 < 0 or col1 >= len(grid[0]) or col2 < 0 or col2 >= len(grid[0]):
                 return 0
-        
-
-
             maxVal = float("-inf")
-            
             for i in [-1,0,1]:
-                
                 for j in [-1,0,1]:
-
                     value = 0
-
                     if col1 == col2:
                         value = grid[row][col1]                
                     else:
                         value = grid[row][col1] + grid[row][col2]
-
                     value += recurison(row+1,col1+i,col2+j)
-
                     maxVal = max(maxVal,value)
-
             memo[(row,col1,col2)] = maxVal
-
             return maxVal
-
         return recurison(0,0,len(grid[0])-1)
 
 
