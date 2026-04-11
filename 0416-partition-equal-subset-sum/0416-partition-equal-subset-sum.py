fe6dc32b85335b64ad64cb050d5cff216c85d736
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-04-10 23:58:21 -0500

    Time: 1186 ms (36.08%), Space: 19.3 MB (88.67%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,33 +1,49 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-
+        '''
         if sum(nums) % 2 != 0:
             return False
-
         target = sum(nums)//2
-
         dp = []
         for i in range(len(nums)):
             tempArray = [False]*(target+1)
             tempArray[0] = True
             dp.append(tempArray)
-
         dp[0][0] = True
-        
         for i in range(1,len(nums)):
             for j in range(1,target+1):
-
                 notPick    =  dp[i-1][j]
-                
                 pick = False
                 if nums[i] <= j:
                     pick    =  dp[i-1][j-nums[i]]
-
                 dp[i][j] = pick or notPick
-
-        
-
         return dp[-1][-1]
+        '''
+
+        if sum(nums) % 2 != 0:
+            return False
+        target = sum(nums)//2
+
+        prev = [False]*(target+1)
+        prev[0] = True
+        
+        for i in range(len(nums)):
+
+            temp = [False]*(target+1)
+            temp[0] = True
+
+            for j in range(1,target+1):
+
+                notPick    =  prev[j]
+                pick = False
+                if nums[i] <= j:
+                    pick    =  prev[j-nums[i]]
+
+                temp[j] = pick or notPick
+
+            prev = temp
+
+        return prev[-1]
 
 
                  
