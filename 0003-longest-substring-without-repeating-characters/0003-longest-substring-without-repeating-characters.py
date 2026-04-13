46ec68e255e377d4dd3c01e8fb12f87250abca7b
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-04-13 18:42:53 -0500

    Time: 11 ms (76.99%), Space: 19.3 MB (41.82%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,18 +1,22 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        n = len(s)
-        maxLength = 0
-        charSet = set()
-        left = 0
-        
-        for right in range(n):
-            if s[right] not in charSet:
-                charSet.add(s[right])
-                maxLength = max(maxLength, right - left + 1)
+        hashMap = {}
+        leftPointer = 0
+        rightPointer = 0
+        maxLengh = 0
+        while leftPointer <= len(s)-1 and rightPointer <= len(s)-1:
+            
+            if s[rightPointer] not in hashMap:
+                hashMap[s[rightPointer]] = rightPointer
+                maxLengh = max(maxLengh,((rightPointer-leftPointer)+1))
             else:
-                while s[right] in charSet:
-                    charSet.remove(s[left])
-                    left += 1
-                charSet.add(s[right])
+                leftPointer = max(leftPointer,hashMap[s[rightPointer]] + 1)
+                hashMap[s[rightPointer]] = rightPointer
+                maxLengh = max(maxLengh,((rightPointer-leftPointer)+1))
+                
+            rightPointer += 1
+        return maxLengh
+
+
+
         
-        return maxLength
