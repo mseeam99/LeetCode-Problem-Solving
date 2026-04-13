6192c4bcd5ad5f571c11ed4dd950b66cf35e49a0
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-04-13 17:06:11 -0500

    Time: 64 ms (24.66%), Space: 28.5 MB (97.16%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,27 +1,23 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
 
-        memo = {}
+        minimum = prices[0]
+        profit = 0
 
-        def recursion(index,holding):
-            if index >= len(prices):
-                return 0
-
-            if (index,holding) in memo:
-                return memo[(index,holding)]
-
-            if holding == False:
-                buy = -prices[index] + recursion(index+1,True)
-                skip = recursion(index+1,False)
-                memo[(index,holding)] = max(buy, skip)
-            elif holding == True:
-                sell = prices[index]
-                skip = recursion(index+1,True)
-
-                memo[(index,holding)] = max(sell, skip)
-
-            return memo[(index,holding)]
-
-        return recursion(0,False)
+        for i in range(len(prices)):
             
+            profit = max(profit,(prices[i]-minimum))
+
+            minimum = min(minimum,prices[i])
+
+        return profit
+
+
+
+
+
+
+
+
+
         
