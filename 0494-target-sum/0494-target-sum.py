25395116e0abe0bbe1ff21984d986a1f2ac8c3d6
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-04-13 06:19:54 -0500

    Time: 39 ms (79.37%), Space: 19.3 MB (92.07%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,29 +1,55 @@
 class Solution:
     def findTargetSumWays(self, nums, target):
-
+        '''
+        total = sum(nums)
+        if total < abs(target) or (total + target) % 2 != 0:
+            return 0
+        find = (total + target) // 2
         memo = {}
-
         def recursion(index, currentSum):
-
             if (index, currentSum) in memo:
                 return memo[(index, currentSum)]
-
             if index == 0:
-                ways = 0
+                if currentSum == 0 and nums[0] == 0:
+                    return 2
+                if currentSum == 0:
+                    return 1
+                if currentSum == nums[0]:
+                    return 1
+                return 0
+            notPick = recursion(index - 1, currentSum)
+            pick = 0
+            if nums[index] <= currentSum:
+                pick = recursion(index - 1, currentSum - nums[index])
+            memo[(index, currentSum)] = pick + notPick
+            return memo[(index, currentSum)]
+        return recursion(len(nums) - 1, find)
+        '''
 
-                if currentSum + nums[0] == 0:
-                    ways += 1
+        total = sum(nums)
+        if total < abs(target) or (total + target) % 2 != 0:
+            return 0
+        find = (total + target) // 2
+        n = len(nums)
+        dp = [[0] * (find + 1) for _ in range(n)]
+        if nums[0] == 0:
+            dp[0][0] = 2
+        else:
+            dp[0][0] = 1
 
-                if currentSum - nums[0] == 0:
-                    ways += 1
+        if nums[0] != 0 and nums[0] <= find:
+            dp[0][nums[0]] = 1
+        for i in range(1, n):
+            for currentSum in range(find + 1):
+                notPick = dp[i - 1][currentSum]
+                pick = 0
+                if nums[i] <= currentSum:
+                    pick = dp[i - 1][currentSum - nums[i]]
+                dp[i][currentSum] = pick + notPick
+        return dp[n - 1][find]
 
-                return ways
+        
 
-            add = recursion(index - 1, currentSum + nums[index])
-            subtract = recursion(index - 1, currentSum - nums[index])
+        
 
-            memo[(index, currentSum)] = add + subtract
 
-            return add + subtract
-
-        return recursion(len(nums) - 1, target)
