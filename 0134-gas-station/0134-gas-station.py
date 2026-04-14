8818d8ac22f6885dcdc5010e5fa2a7e79c9eb41e
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-04-13 21:49:58 -0500

    Time: 11 ms (99.69%), Space: 26 MB (74.3%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -1,19 +1,25 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        
-        if sum(gas) < sum(cost):
+
+        totalGas  = sum(gas)
+        totalCost = sum(cost)
+
+        if totalGas < totalCost:
             return -1
+        
+        startingIndex = 0
+        currentCost = 0
 
-        index = 0
-        totalSum = 0
+        for i in range(len(gas)):
 
-        for i in range(len(cost)):
-            totalSum += gas[i] - cost[i]
+            currentCost += gas[i]-cost[i]
 
-            if totalSum < 0:
-                totalSum = 0
-                index = i+1
 
-        return index
+            if currentCost < 0:
+                startingIndex = i + 1
+                currentCost = 0
 
-      
+
+        return startingIndex
+        
+        
