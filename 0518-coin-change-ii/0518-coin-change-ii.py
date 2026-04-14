75ac169e961d478bc71c64a039adc518ae90b884
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-04-13 22:33:14 -0500

    Time: 498 ms (41.45%), Space: 66.8 MB (22.59%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,31 +1,57 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-
+        '''
         memo = {}
-
         def recursion(index,currentSum):
-
             nonlocal memo
 
-            if index >= len(coins):
-                return 0
+            if index == 0:
+                return currentSum % coins[0] == 0
             
-            if currentSum > amount:
-                return 0
-            
-            if currentSum == amount:
-                return 1
-
             if (index,currentSum) in memo:
                 return memo[(index,currentSum)]
 
-            pick    = recursion(index, currentSum + coins[index])
-            notPick = recursion(index+1, currentSum + 0)
+            pick    = recursion(index, currentSum - coins[index])
+            notPick = recursion(index-1, currentSum)
+
             ways = pick + notPick
             memo[(index,currentSum)] = ways
+
             return ways
 
-        return recursion(0,0)
+        return recursion(len(coins)-1,amount)
+        '''
+        
+        dp = []
+        for i in range(len(coins)):
+            tempArray = [0]*(amount+1)
+            dp.append(tempArray)
+        
+        for i in range(len(coins)):
+            dp[i][0] = 1
+
+        for t in range(amount + 1):
+            if t % coins[0] == 0:
+                dp[0][t] = 1
+
+        
+        for i in range(1,len(dp)):
+
+            for j in range(amount+1):
+
+                pick = 0
+                if coins[i] <= j:
+                    pick    = dp[i][j - coins[i]]
+                notPick = dp[i-1][j]
+
+                dp[i][j] = pick + notPick
+
+        
+        return dp[-1][amount]
+                
+
+
+
 
 
             
