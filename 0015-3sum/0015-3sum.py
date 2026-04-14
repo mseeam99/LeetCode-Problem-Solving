beb495f289f5cf6b8bb001a85b24465ac935f5fe
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-04-13 19:11:54 -0500

    Time: 1007 ms (16.19%), Space: 23.2 MB (10.8%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,51 +1,25 @@
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        '''
-        answer = set()
-        for i in range(len(nums)-2):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
-            leftPointer = i+1
-            rightPointer = leftPointer+1
-            while leftPointer < len(nums)-1:
-                value = nums[i] + nums[leftPointer] + nums[rightPointer]
-                if value == 0:
-                    answer.add(tuple(sorted([nums[i],nums[leftPointer], nums[rightPointer]])))
-                    if rightPointer != len(nums)-1:
-                        rightPointer+=1
-                    elif rightPointer == len(nums)-1:
-                        leftPointer +=1
-                        rightPointer = leftPointer+1
-                else:
-                    if rightPointer != len(nums)-1:
-                        rightPointer+=1
-                    elif rightPointer == len(nums)-1:
-                        leftPointer +=1
-                        rightPointer = leftPointer+1
-        answer = list(answer)
-        return answer
-        '''
-        
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
         nums.sort()
-        answer = set()
+
+        hashMap = {}
+        for i in range(len(nums)):
+            hashMap[nums[i]] = i   # store last index of each value
+
+        answers = []
+
         for i in range(len(nums)):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            leftPointer = i+1
-            rightPointer = len(nums)-1
-            while leftPointer < rightPointer:
-                if nums[i] + nums[leftPointer] + nums[rightPointer] == 0:
-                    answer.add((nums[i],nums[leftPointer],nums[rightPointer]))
-                    leftPointer+=1
-                    rightPointer-=1
-                elif nums[i] + nums[leftPointer] + nums[rightPointer] < 0:
-                    leftPointer+=1
-                elif nums[i] + nums[leftPointer] + nums[rightPointer] > 0:
-                    rightPointer-=1
-        answer = list(answer)
-        return answer
 
+            for j in range(i + 1, len(nums)):
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
 
-        
+                currentSum = nums[i] + nums[j]
+                diffNeed = -currentSum
 
-        
+                if diffNeed in hashMap and hashMap[diffNeed] > j:
+                    answers.append([nums[i], nums[j], diffNeed])
+
+        return answers
