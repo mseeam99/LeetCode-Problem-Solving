ce016d05520a844ba304e15afb535855b672a2a6
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-04-18 18:11:10 -0500

    Time: 1124 ms (13.05%), Space: 315.8 MB (14.5%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -7,24 +7,24 @@ class Solution:
 
             nonlocal memo
 
-            if (indexOfText1 >= len(text1)) or (indexOfText2 >= len(text2)):
+            if (indexOfText1 < 0) or (indexOfText2 < 0):
                 return 0
 
             if (indexOfText1, indexOfText2) in memo:
                 return memo[(indexOfText1, indexOfText2)]
             
             if (text1[indexOfText1] == text2[indexOfText2]):
-                val = (1 + recursion(indexOfText1+1, indexOfText2+1))
+                val = (1 + recursion(indexOfText1-1, indexOfText2-1))
                 memo[(indexOfText1, indexOfText2)] = val
                 return val
             else:
-                x = recursion(indexOfText1, indexOfText2+1)
-                y = recursion(indexOfText1+1, indexOfText2)
+                x = recursion(indexOfText1, indexOfText2-1)
+                y = recursion(indexOfText1-1, indexOfText2)
                 val = max(x,y)
                 memo[(indexOfText1, indexOfText2)] = val
                 return val
 
-        return recursion(0,0)
+        return recursion(len(text1)-1,len(text2)-1)
             
 
 
