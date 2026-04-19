c005b22a32a782ecf6a598a0ec3e741036efae7d
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-04-19 18:48:49 -0500

    Time: 431 ms (45.38%), Space: 21.2 MB (60.34%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minInsertions(self, s: str) -> int:
+        
+
+        def lcs(stringOne, stringTwo):
+
+            dp = []
+            for i in range(len(stringOne) + 1):
+                row = [0] * (len(stringTwo) + 1)
+                dp.append(row)
+
+            for i in range(1, len(stringOne) + 1):
+                for j in range(1, len(stringTwo) + 1):
+                    if stringOne[i - 1] == stringTwo[j - 1]:
+                        dp[i][j] = 1 + dp[i - 1][j - 1]
+                    else:
+                        dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+            ans = dp[len(stringOne)][len(stringTwo)]
+            return len(stringOne) - ans
+
+
+        return lcs(s, s[::-1])
