9b2bcdef29548db6a52436c50f5d629ce4d4f215
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-04-26 12:41:02 -0500

    Time: 491 ms (33.86%), Space: 76.1 MB (32.44%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -1,39 +1,50 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
+        # s * t = m * n array
+        '''
+                def recursion(index1,index2):
+                    if index2 == 0:
+                        return 1
+                    if index1 == 0:
+                        return 0
+                    if dp[index1][index2] != -1:
+                        return dp[index1][index2]
+                    pick = 0
+                    notPick = 0
+                    if s[index1-1] == t[index2-1]:
+                        pick = recursion(index1-1,index2-1) + recursion(index1-1,index2)
+                    else:
+                        notPick = recursion(index1-1,index2)
+                    dp[index1][index2] = pick + notPick
+                    return dp[index1][index2]
+                return recursion(len(s),len(t))
+        '''
+                
 
         dp = []
         for i in range(len(s)+1):
-            tempArray = [-1] * (len(t)+1)
+            tempArray = [0] * (len(t)+1)
             dp.append(tempArray)
-         
-        # s * t = m * n array
-
-
-        def recursion(index1,index2):
-
-            if index2 == 0:
-                return 1
-
-            if index1 == 0:
-                return 0
-
-            if dp[index1][index2] != -1:
-                return dp[index1][index2]
-
-            pick = 0
-            notPick = 0
-
-            if s[index1-1] == t[index2-1]:
-                pick = recursion(index1-1,index2-1) + recursion(index1-1,index2)
-            else:
-                notPick = recursion(index1-1,index2)
-
-            dp[index1][index2] = pick + notPick
-            return dp[index1][index2]
-
 
         
-        return recursion(len(s),len(t))
+        for i in range(len(s)+1):
+            dp[i][0] = 1
+
+        for i in range(1,len(s)+1):
+            for j in range(1,len(t)+1):
+                pick, notPick = 0,0
+                if s[i-1] == t[j-1]:
+                    pick = dp[i-1][j-1] + dp[i-1][j]
+                else:
+                    notPick = dp[i-1][j]
+
+                dp[i][j] = pick + notPick
+
+        return dp[-1][-1]
+
+
+
+
 
 
 
