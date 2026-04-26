3084021c5e5d42bb6134811bb55d49fd9436d9e6
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-04-26 14:35:19 -0500

    Time: 23 ms (97.73%), Space: 22.4 MB (85.54%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,68 +1,36 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
 
-        if (len(word1) == 0 and len(word2) > 0) or (len(word2) == 0 and len(word1) > 0):
-            return len(max(word1,word2))
-
-        if not word1 and not word2:
-            return 0
-        
         dp = []
-        for i in range(len(word1)):
-            tempArray = [0] * (len(word2))
+        for i in range(len(word1) + 1):
+            tempArray = [-1] * (len(word2) + 1)
             dp.append(tempArray)
 
         def recursion(idxOne,idxTwo):
 
-            if idxOne < 0:
-                return idxTwo + 1
-            
-            if idxTwo < 0:
-                return idxOne + 1
-            
-            if dp[idxOne][idxTwo] != 0:
+            if idxOne == 0:
+                dp[idxOne][idxTwo] = idxTwo
+                return idxTwo
+
+            if idxTwo == 0:
+                dp[idxOne][idxTwo] = idxOne
+                return idxOne
+
+            if dp[idxOne][idxTwo] != -1:
                 return dp[idxOne][idxTwo]
 
-            if word1[idxOne] == word2[idxTwo]:
+            if word1[idxOne-1] == word2[idxTwo-1]:
+                dp[idxOne][idxTwo] = recursion(idxOne-1,idxTwo-1)
+            else:
+                insert = 1 + recursion(idxOne,idxTwo-1)
+                delete = 1 + recursion(idxOne-1,idxTwo)
+                rplace = 1 + recursion(idxOne-1,idxTwo-1)
+                dp[idxOne][idxTwo] = min(insert,delete,rplace)
 
-                dp[idxOne][idxTwo] =   0 + recursion(idxOne-1,idxTwo-1)
-                return dp[idxOne][idxTwo]
+            return dp[idxOne][idxTwo]
 
-            insert = 1 + recursion(idxOne,idxTwo-1)
-            delete = 1 + recursion(idxOne-1,idxTwo)
-            rplace = 1 + recursion(idxOne-1,idxTwo-1)
+        recursion(len(word1), len(word2))
 
-            dp[idxOne][idxTwo] = min(insert,delete,rplace)
+        return dp[-1][-1]
 
-            return min(insert,delete,rplace)
-
-        
-        recursion(len(word1)-1,len(word2)-1)
-
-
-        return dp[len(word1)-1][len(word2)-1]
-
-
-
-
-
-
-
-            
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+       
