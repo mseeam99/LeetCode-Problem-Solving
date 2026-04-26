40b23c898770219717a0cc429f3cf6378c2b993f
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-04-26 12:49:53 -0500

    Time: 333 ms (69.01%), Space: 19.5 MB (93.37%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -20,24 +20,20 @@ class Solution:
                 return recursion(len(s),len(t))
         '''
             
-        dp = []
-        for i in range(len(s)+1):
-            tempArray = [0] * (len(t)+1)
-            dp.append(tempArray)
-    
-        for i in range(len(s)+1):
-            dp[i][0] = 1
-
+        prev = [0] * (len(t) + 1)
+        prev[0] = 1
         for i in range(1,len(s)+1):
+            curr = [0]*(len(t)+1)
+            curr[0] = 1
             for j in range(1,len(t)+1):
                 pick, notPick = 0,0
                 if s[i-1] == t[j-1]:
-                    pick = dp[i-1][j-1] + dp[i-1][j]
+                    pick = prev[j-1] + prev[j]
                 else:
-                    notPick = dp[i-1][j]
-                dp[i][j] = pick + notPick
-
-        return dp[len(s)][len(t)]
+                    notPick = prev[j]
+                curr[j] = pick + notPick
+            prev = curr
+        return prev[-1]
 
 
 
