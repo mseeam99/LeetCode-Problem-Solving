83f815b0b26847cf9d59cd0e6a71d71d8ee3a432
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-04-27 04:20:33 -0500

    Time: 82 ms (32.87%), Space: 27.2 MB (30.36%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
@@ -1,6 +1,33 @@
 class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
 
+        dp = []
+        for i in range(len(prices)+1):
+            SecondDimension = []
+            for j in range(2):
+                thirdDimension = []
+                for k in range(k+1):
+                    thirdDimension.append(0)
+                SecondDimension.append(thirdDimension)
+            dp.append(SecondDimension)
+
+        for index in range(len(prices)-1,-1,-1):
+            for ifBuyingPossible in range(2):
+                for capacity in range(1,k+1):
+                    if ifBuyingPossible == 1:
+                        pick = -prices[index] + dp[index+1][0][capacity]
+                        notPick = 0 + dp[index+1][1][capacity]
+                        result = max(pick,notPick)
+                        dp[index][ifBuyingPossible][capacity] = result
+                    elif ifBuyingPossible == 0:
+                        pick = prices[index] + dp[index+1][1][capacity-1]
+                        notPick = 0 + dp[index+1][0][capacity]
+                        result = max(pick,notPick)
+                        dp[index][ifBuyingPossible][capacity] = result
+        return dp[0][1][k]   
+
+
+        '''
         memo = {}
         
         def recursion(index,canBuy,allowed):
@@ -23,4 +50,5 @@ class Solution:
                 memo[(index,canBuy,allowed)] = profit
                 return profit
             
-        return recursion(0,True,k)
+        return recursion(0,True,k)  
+        '''
