55b5f03414addeecde87cccefe3f11509841b1e8
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-04-27 04:07:42 -0500

    Time: 937 ms (15.94%), Space: 68.7 MB (16.45%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -1,30 +1,56 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
 
-        memo = {}
+        dp = []
+        for i in range(len(prices)+1):
+            SecondDimension = []
+            for j in range(2):
+                thirdDimension = []
+                for k in range(3):
+                    thirdDimension.append(0)
+                SecondDimension.append(thirdDimension)
+            dp.append(SecondDimension)
 
+        for index in range(len(prices)-1,-1,-1):
+
+            for ifBuyingPossible in range(2):
+
+                for capacity in range(1,3):
+
+                    if ifBuyingPossible == 1:
+                        pick = -prices[index] + dp[index+1][0][capacity]
+                        notPick = 0 + dp[index+1][1][capacity]
+                        result = max(pick,notPick)
+                        dp[index][ifBuyingPossible][capacity] = result
+                       
+                    elif ifBuyingPossible == 0:
+                        pick = prices[index] + dp[index+1][1][capacity-1]
+                        notPick = 0 + dp[index+1][0][capacity]
+                        result = max(pick,notPick)
+                        dp[index][ifBuyingPossible][capacity] = result
+
+        return dp[0][1][2]
+                       
+
+
+
+'''
         def recursion(index, ifBuyingPossible, capacity):
-
+            if dp[index][ifBuyingPossible][capacity] != 0:
+                return dp[index][ifBuyingPossible][capacity]
             if (index >= len(prices)) or (capacity <= 0):
                 return 0
-
-            if (index,ifBuyingPossible,capacity) in memo:
-                return memo[(index,ifBuyingPossible,capacity)]
-
-            if ifBuyingPossible == True:
-                pick = -prices[index] + recursion(index+1,False,capacity)
-                notPick = 0 + recursion(index+1,True,capacity)
+            if ifBuyingPossible == 1:
+                pick = -prices[index] + recursion(index+1,0,capacity)
+                notPick = 0 + recursion(index+1,1,capacity)
                 result = max(pick,notPick)
-                memo[(index,ifBuyingPossible,capacity)] = result
+                dp[index][ifBuyingPossible][capacity] = result
                 return result
-
-            elif ifBuyingPossible == False:
-                pick = prices[index] + recursion(index+1,True,capacity-1)
-                notPick = 0 + recursion(index+1,False,capacity)
+            elif ifBuyingPossible == 0:
+                pick = prices[index] + recursion(index+1,1,capacity-1)
+                notPick = 0 + recursion(index+1,0,capacity)
                 result = max(pick,notPick)
-                memo[(index,ifBuyingPossible,capacity)] = result
+                dp[index][ifBuyingPossible][capacity] = result
                 return result
-
-        return recursion(0,True,2)
-
-        
+        return recursion(0,1,2)
+'''
