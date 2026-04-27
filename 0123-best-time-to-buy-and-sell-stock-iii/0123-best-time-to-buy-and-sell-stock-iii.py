e65b31d4c78fc32525cb4c3fa9bed2f408df5917
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-04-27 04:07:57 -0500

    Time: 919 ms (16.14%), Space: 68.7 MB (16.45%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -12,28 +12,20 @@ class Solution:
             dp.append(SecondDimension)
 
         for index in range(len(prices)-1,-1,-1):
-
             for ifBuyingPossible in range(2):
-
                 for capacity in range(1,3):
-
                     if ifBuyingPossible == 1:
                         pick = -prices[index] + dp[index+1][0][capacity]
                         notPick = 0 + dp[index+1][1][capacity]
                         result = max(pick,notPick)
                         dp[index][ifBuyingPossible][capacity] = result
-                       
                     elif ifBuyingPossible == 0:
                         pick = prices[index] + dp[index+1][1][capacity-1]
                         notPick = 0 + dp[index+1][0][capacity]
                         result = max(pick,notPick)
                         dp[index][ifBuyingPossible][capacity] = result
-
         return dp[0][1][2]
                        
-
-
-
 '''
         def recursion(index, ifBuyingPossible, capacity):
             if dp[index][ifBuyingPossible][capacity] != 0:
