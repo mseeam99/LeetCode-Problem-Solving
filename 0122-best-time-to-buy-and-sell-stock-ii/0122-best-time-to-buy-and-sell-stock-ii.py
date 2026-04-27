d59fc2fb3986656503017ad7ba2d1b2cb4ca6176
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-04-27 03:37:34 -0500

    Time: 30 ms (5.05%), Space: 23.4 MB (5.06%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,33 +1,23 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-
         dp = []
         for i in range(len(prices)+1):
             tempArray = [0]*(2)
             dp.append(tempArray)
-
         dp[len(prices)][0] = dp[len(prices)][1] = 0
-
         for index in range(len(prices)-1,-1,-1):
-
             for canBuy in range(0,2):
-
                 if canBuy == 1:
                     pick = -prices[index] + dp[index+1][0]
                     notPick = 0 + dp[index+1][1]
                     result = max(pick,notPick)
                     dp[index][canBuy] = result
-                   
                 else:
                     pick = prices[index] + dp[index+1][1]
                     notPick = 0 + dp[index+1][0]
                     result = max(pick,notPick)
                     dp[index][canBuy] = result
-
         return min(dp[0])
-                    
-
-
         '''
         def recursion(index,canBuy):
 
