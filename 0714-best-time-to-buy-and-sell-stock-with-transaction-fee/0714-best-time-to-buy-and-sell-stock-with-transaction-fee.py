609a48626d186e2654cfd8dd6c4e664968aff24e
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-04-27 21:31:59 -0500

    Time: 262 ms (24.48%), Space: 32.4 MB (23.1%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,26 +1,43 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
-        
-        memo = {}
+        dp = []
+        for i in range(len(prices)+1):
+            tempArray = [0]*(2+1)
+            dp.append(tempArray)
 
-        def recursion(index,canBuy):
-            if (index >= len(prices)):
+        for i in range(len(prices)-1,-1,-1):
+            for j in range(2):
+                if j == 1: # buying
+                    pick = -prices[i] + dp[i+1][0]
+                    notPick = 0 + dp[i+1][1]
+                    dp[i][j] = max(pick,notPick)
+                elif j == 0: # selling
+                    pick = (+prices[i] + dp[i+1][1]) - fee
+                    notPick = 0 + dp[i+1][0]
+                    dp[i][j] =  max(pick,notPick)
+        return dp[0][1]
+'''
+        def recursion(index,ifAllowedToBuy):
+
+            if index >= len(prices):
                 return 0
+
+            if ifAllowedToBuy == 1: # buying
+               
+                pick = -prices[index] + recursion(index+1,0)
+                notPick = 0 + recursion(index+1,1)
+                return max(pick,notPick)
+                
+
+            elif ifAllowedToBuy == 0: # selling
+                pick = (+prices[index] + recursion(index+1,1)) - fee
+                notPick = 0 + recursion(index+1,0)
+                return max(pick,notPick)
+
             
-            if (index,canBuy) in memo:
-                return memo[(index,canBuy)]
+            
+        return recursion(0,1)
 
-            if canBuy == True:
-                pick = -prices[index] + recursion(index+1,False)
-                notPick = 0 + recursion(index+1,True)
-                maxProfit = max(pick,notPick)
-                memo[(index,canBuy)] = maxProfit
-                return maxProfit
-            else:
-                pick = (prices[index]-fee) + recursion(index+1,True)
-                notPick = 0 + recursion(index+1,False)
-                maxProfit = max(pick,notPick)
-                memo[(index,canBuy)] = maxProfit
-                return maxProfit
 
-        return recursion(0,True)
+
+'''
