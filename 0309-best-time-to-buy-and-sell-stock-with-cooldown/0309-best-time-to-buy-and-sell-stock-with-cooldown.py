bf764d1e0795b232b5d938501a719abc07ee3098
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-04-27 21:17:30 -0500

    Time: 7 ms (29.84%), Space: 20.2 MB (40.85%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,27 +1,61 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        dp = []
+        for i in range(len(prices)+2):
+            secondDimension = [0]*(2)
+            dp.append(secondDimension)
+                    
+        #index -> i
+        #allowedToBuy -> j
 
-        memo = {}
+        for i in range(len(prices)-1,-1,-1):
+
+            for j in range(2):
+
+                if j == 1:
+                    pick = -prices[i] + dp[i+1][0]
+                    notPick = 0 + dp[i+1][1]
+                    dp[i][j] = max(pick,notPick)
+                   
+                else:
+                    pick = (prices[i] + dp[i+2][1])
+                    notPick = (0 + dp[i+1][0])
+                    dp[i][j] = max(pick,notPick)
+
+        
+        for i in range(len(dp)):
+
+            print(dp[i])
+
+        return dp[0][1]
+
+
+
+               
+
+
+'''
 
         def recursion(index,allowedToBuy,waiting):
 
             if (index >= len(prices)):
                 return 0
-
-            if (index,allowedToBuy,waiting) in memo:
-                return memo[(index,allowedToBuy,waiting)]
             
-            if allowedToBuy == True:
-                pick = -prices[index] + recursion(index+1,False,False)
-                notPick = 0 + recursion(index+1,True,False)
+            if dp[index][allowedToBuy][waiting] != 0:
+                return dp[index][allowedToBuy][waiting]
+            
+            if allowedToBuy == 1:
+                pick = -prices[index] + recursion(index+1,0,1)
+                notPick = 0 + recursion(index+1,1,1)
                 profit = max(pick,notPick)
-                memo[(index,allowedToBuy,waiting)] = profit
+                dp[index][allowedToBuy][waiting] = profit
                 return profit
             else:
-                pick = prices[index] + recursion(index+2,True,True)
-                notPick = 0 + recursion(index+1,False,True)
+                pick = prices[index] + recursion(index+2,1,0)
+                notPick = 0 + recursion(index+1,0,0)
                 profit = max(pick,notPick)
-                memo[(index,allowedToBuy,waiting)] = profit
+                dp[index][allowedToBuy][waiting] = profit
                 return profit
 
-        return recursion(0,True,False)        
+        return recursion(0,1,1)       
+'''
