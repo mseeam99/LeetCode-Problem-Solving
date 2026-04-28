f280a562f77c6fbb69f5665df23019675694cb2a
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-04-27 21:17:59 -0500

    Time: 4 ms (41.75%), Space: 20.1 MB (41.73%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -4,35 +4,18 @@ class Solution:
         for i in range(len(prices)+2):
             secondDimension = [0]*(2)
             dp.append(secondDimension)
-                    
-        #index -> i
-        #allowedToBuy -> j
-
+            
         for i in range(len(prices)-1,-1,-1):
-
             for j in range(2):
-
                 if j == 1:
                     pick = -prices[i] + dp[i+1][0]
                     notPick = 0 + dp[i+1][1]
                     dp[i][j] = max(pick,notPick)
-                   
                 else:
                     pick = (prices[i] + dp[i+2][1])
                     notPick = (0 + dp[i+1][0])
                     dp[i][j] = max(pick,notPick)
-
-        
-        for i in range(len(dp)):
-
-            print(dp[i])
-
         return dp[0][1]
-
-
-
-               
-
 
 '''
 
