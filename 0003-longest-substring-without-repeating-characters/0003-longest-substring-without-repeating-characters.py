af1042a534372a2edbff693ee672e55a36fcbc09
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-05-03 01:47:33 -0500

    Time: 19 ms (22.99%), Space: 19.3 MB (30.94%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,7 +1,7 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
 
-        mySet = set()
+        hashMap = {}
 
         maxLength = 0
         leftPointer = 0
@@ -9,14 +9,18 @@ class Solution:
 
         while rightPointer <= len(s)-1:
 
-            while s[rightPointer] in mySet:
-                mySet.remove(s[leftPointer])
-                leftPointer += 1
-
-            mySet.add(s[rightPointer])
+            hashMap[s[rightPointer]] = hashMap.get(s[rightPointer], 0) + 1
             
+            
+            while hashMap[s[rightPointer]] > 1:
+                hashMap[s[leftPointer]] -= 1
+                if hashMap[s[leftPointer]] == 0:
+                    del hashMap[s[leftPointer]]
+                leftPointer += 1
+            
+ 
             maxLength = max(maxLength,rightPointer-leftPointer+1)
-            rightPointer+=1
+            rightPointer +=1 
 
         return maxLength
 
