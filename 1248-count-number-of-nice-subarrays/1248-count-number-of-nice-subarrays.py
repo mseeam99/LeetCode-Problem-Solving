267a5a97881e01e78ed14e719e28d2703c6ae4dc
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-05-04 15:22:19 -0500

    Time: 202 ms (5.27%), Space: 24.9 MB (58.27%) - LeetHub

---
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
@@ -0,0 +1,30 @@
+class Solution:
+    def numberOfSubarrays(self, nums: List[int], k: int) -> int:
+        
+
+        def helper(val):
+
+            leftPointer = 0
+            rightPointer = 0
+            oddCount = 0
+            subarrayCount = 0
+
+            while rightPointer <= len(nums)-1:
+                
+                if nums[rightPointer] % 2 != 0:
+                    oddCount += 1
+                    
+                while oddCount > val:
+                    if nums[leftPointer] % 2 != 0:
+                        oddCount -= 1
+                    leftPointer += 1
+            
+                subarrayCount += ((rightPointer-leftPointer)+1)
+                
+                rightPointer += 1
+                
+            return subarrayCount
+
+            
+        return helper(k) - helper(k-1)
+      
