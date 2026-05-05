98435ddda08c383463be9490f35b8f2467df1b33
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-05-04 20:54:32 -0500

    Time: 139 ms (5.02%), Space: 19.6 MB (43.26%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -1,25 +1,38 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        leftPointer = 0
-        rightPointer = len(s) - 1
-        while leftPointer < rightPointer:
-            if s[leftPointer] == s[rightPointer]:
-                leftPointer+=1
-                rightPointer-=1
-            else:
-                first  = self.isPalindrome(leftPointer+1,rightPointer,s)
-                second = self.isPalindrome(leftPointer,rightPointer-1,s)
-                return first or second
+        
+       
+        
+        def palindrome(s):
+            leftPointer = 0
+            rightPointer = len(s)-1
+            while leftPointer <= rightPointer:
+                if leftPointer == rightPointer:
+                    break
+                if s[leftPointer].isalnum()==False and s[rightPointer].isalnum()==False:
+                    leftPointer += 1
+                    rightPointer -= 1 
+                    continue
+                if s[leftPointer].isalnum()==False:
+                    eftPointer += 1
+                    continue
+                if s[rightPointer].isalnum()==False:
+                    rightPointer -= 1
+                    continue
+                if s[leftPointer].lower() == s[rightPointer].lower():
+                    leftPointer += 1
+                    rightPointer -= 1
+                else:
+                    return False
+            return True
+        
+
+        l = 0
+        r = len(s) - 1
+        while l <= r:
+            if s[l] != s[r]:
+                return palindrome(s[l+1:r+1]) or palindrome(s[l:r])
+            l += 1
+            r -= 1
+
         return True
-
-
-
-    def isPalindrome(self,left,right,s):
-            return s[left:right+1] == s[left:right+1][::-1]
-                    
-
-
-
-        
-
-        
