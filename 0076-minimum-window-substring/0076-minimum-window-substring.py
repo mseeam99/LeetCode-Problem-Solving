0f1e5dcd540e9b777519bd567eda440c130a0fb0
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-05-04 19:47:53 -0500

    Time: 554 ms (7.88%), Space: 19.7 MB (37.25%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -0,0 +1,57 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+
+        hashMap = {}
+        for i in range(len(t)):
+            if t[i] not in hashMap:
+                hashMap[t[i]] = 1
+            else:
+                hashMap[t[i]] += 1
+        
+        leftPointer = 0
+        rightPointer = 0
+        biggerHashMap = {}
+        minLength = float("inf")
+        answer = ""
+
+        while rightPointer <= len(s)-1:
+
+            if s[rightPointer] not in biggerHashMap:
+                biggerHashMap[s[rightPointer]] = 1
+            else:
+                biggerHashMap[s[rightPointer]] += 1
+            
+            # now to check if biggerHashMap has >= elements of hashMap
+            boolean = True
+            while boolean == True:
+                for key,val in hashMap.items():
+                    if key in biggerHashMap and biggerHashMap[key] >= val:
+                        continue
+                    else:
+                        boolean = False
+                        break
+                if boolean == True:
+                    windowLength = rightPointer - leftPointer + 1
+                    if windowLength < minLength:
+                        minLength = windowLength
+                        answer = s[leftPointer:rightPointer + 1]
+
+                    biggerHashMap[s[leftPointer]] -= 1
+                    if biggerHashMap[s[leftPointer]] == 0:
+                        del biggerHashMap[s[leftPointer]]
+                    leftPointer += 1
+            rightPointer += 1
+
+        return answer
+
+
+
+
+
+        
+            
+
+
+
+
+        
