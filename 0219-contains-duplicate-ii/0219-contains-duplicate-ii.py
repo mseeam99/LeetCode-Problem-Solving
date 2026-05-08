2eb9e7ffaaeee58a26a62f6b8c07795753bc2712
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-05-07 20:13:48 -0500

    Time: 83 ms (6.26%), Space: 46 MB (5.03%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,27 +1,18 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
 
-
         hashMap = defaultdict(list)
         returnStatement = False
-
         for i in range(len(nums)):
-
             if nums[i] not in hashMap:
                 hashMap[nums[i]].append(i)
-
             elif nums[i] in hashMap:
                 hashMap[nums[i]].append(i)
-
                 theList = hashMap[nums[i]]
                 for j in range(len(theList)):
-
-                
                     if (abs(theList[j]-i) <= k) and (theList[j]!=i):
                         returnStatement = True
                         return True
-
-
         return returnStatement
 
 
