839b21701463bd4db8e158c4a38493ba7e837eac
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-05-09 01:17:17 -0500

    Time: 624 ms (96.38%), Space: 23.2 MB (32.57%) - LeetHub

--- 3208-alternating-groups-ii/3208-alternating-groups-ii.py
+++ 3208-alternating-groups-ii/3208-alternating-groups-ii.py
@@ -1,27 +1,15 @@
 class Solution:
     def numberOfAlternatingGroups(self, colors: List[int], k: int) -> int:
-        n = len(colors)
-
-        leftPointer = 0
-        rightPointer = 0
-
-        count = 0
-        alternatingLength = 0
-
-        while rightPointer < n + k - 1:
-
-            curr = colors[rightPointer % n]
-            prev = colors[(rightPointer - 1) % n]
-
-            if rightPointer > 0 and curr != prev:
-                alternatingLength += 1
+        last = colors[0]
+        l = 0
+        colors.extend(colors[:k-1])
+        t = 0
+        for c in colors:
+            if c == last:
+                l = 1
             else:
-                alternatingLength = 1
-
-            if rightPointer >= k - 1:
-                if alternatingLength >= k:
-                    count += 1
-
-            rightPointer += 1
-
-        return count
+                l += 1
+                last = c
+                if l >= k:
+                    t += 1
+        return t
