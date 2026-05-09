b8b1fe720d1145819ee8fd5c4b410c97f5e056bb
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-05-08 19:27:06 -0500

    Time: 11 ms (57.98%), Space: 19.3 MB (48.25%) - LeetHub

--- 1100-find-k-length-substrings-with-no-repeated-characters/1100-find-k-length-substrings-with-no-repeated-characters.py
+++ 1100-find-k-length-substrings-with-no-repeated-characters/1100-find-k-length-substrings-with-no-repeated-characters.py
@@ -1,35 +1,20 @@
 class Solution:
     def numKLenSubstrNoRepeats(self, s: str, k: int) -> int:
-
         hashMap = {}
-
         leftPointer = 0
         rightPointer = 0
-
         count = 0
-
         while rightPointer <= len(s)-1:
-
             if s[rightPointer] not in hashMap:
                 hashMap[s[rightPointer]] = 1
             elif s[rightPointer] in hashMap:
                 hashMap[s[rightPointer]] += 1
-
             while rightPointer - leftPointer + 1 > k:
                 hashMap[s[leftPointer]] -= 1
                 if hashMap[s[leftPointer]] == 0:
                     del hashMap[s[leftPointer]]
                 leftPointer += 1
-
             if rightPointer - leftPointer + 1 == k and len(hashMap) == k:
                 count += 1
-
             rightPointer += 1
-
         return count
-
-
-
-
-
-        
