c18cce81fb3933f4110cbe747fc3eb2297c34722
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-05-09 00:27:34 -0500

    Time: 107 ms (41.07%), Space: 19.6 MB (86.08%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -6,23 +6,26 @@ class Solution:
         leftPointer = 0
         rightPointer = 0
 
-        maxFrequency = 0
         maxLength = 0
+        maxFrequency = 0
 
         while rightPointer <= len(s)-1:
 
             if s[rightPointer] not in hashMap:
                 hashMap[s[rightPointer]] = 1
-                maxFrequency = max(maxFrequency,max(hashMap.values()))
             else:
                 hashMap[s[rightPointer]] += 1
-                maxFrequency = max(maxFrequency,max(hashMap.values()))
 
-            while (rightPointer - leftPointer + 1 - maxFrequency > k):
+            maxFrequency = max(maxFrequency, hashMap[s[rightPointer]])
+
+            while rightPointer - leftPointer + 1 - maxFrequency > k :
                 hashMap[s[leftPointer]] -= 1
+                if hashMap[s[leftPointer]] == 0:
+                    del hashMap[s[leftPointer]]
                 leftPointer += 1
 
-            maxLength = max(maxLength,rightPointer + 1 - leftPointer)
+            maxLength = max(maxLength,rightPointer - leftPointer + 1)
+
             rightPointer += 1
 
         return maxLength
