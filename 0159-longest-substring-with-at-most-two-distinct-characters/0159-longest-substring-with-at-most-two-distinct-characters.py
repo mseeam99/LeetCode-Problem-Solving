41967f941469b102739c352f556ca006c24310c1
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-05-08 19:07:21 -0500

    Time: 178 ms (54.23%), Space: 19.7 MB (33.4%) - LeetHub

---
+++ 0159-longest-substring-with-at-most-two-distinct-characters/0159-longest-substring-with-at-most-two-distinct-characters.py
@@ -0,0 +1,37 @@
+class Solution:
+    def lengthOfLongestSubstringTwoDistinct(self, s: str) -> int:
+
+
+        hashMap = {}
+
+        leftPointer = 0
+        rightPointer = 0
+
+        maxLength = 0
+
+        while rightPointer <= len(s)-1:
+
+            if s[rightPointer] not in hashMap:
+                hashMap[s[rightPointer]] = 1
+            else:
+                hashMap[s[rightPointer]] += 1
+
+            while len(hashMap) > 2:
+                hashMap[s[leftPointer]] -= 1
+                if hashMap[s[leftPointer]] == 0:
+                    del hashMap[s[leftPointer]]
+                leftPointer += 1
+
+            maxLength = max(maxLength,rightPointer-leftPointer+1)
+            rightPointer += 1
+        
+        return maxLength
+
+            
+            
+
+                
+
+
+
+        
