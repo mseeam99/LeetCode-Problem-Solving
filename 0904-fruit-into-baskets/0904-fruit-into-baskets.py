28f3cf8e16993cd206d516391d6969031e17ebfa
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-05-09 19:20:30 -0500

    Time: 210 ms (18.89%), Space: 25.9 MB (33.8%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -1,32 +1,21 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-
         leftPointer = 0
         rightPointer = 0
-
         hashMap = {}
-
         maxFruitNumber = float("-inf")
-
         while rightPointer <= len(fruits)-1:
-
             if fruits[rightPointer] not in hashMap:
                 hashMap[fruits[rightPointer]] = 1
             else:
                 hashMap[fruits[rightPointer]] += 1
-
-            
             while len(hashMap)>2:
                 hashMap[fruits[leftPointer]] -= 1
                 if hashMap[fruits[leftPointer]] == 0:
                     del hashMap[fruits[leftPointer]]
                 leftPointer += 1
-
             maxFruitNumber = max(maxFruitNumber, rightPointer-leftPointer + 1)
-
             rightPointer += 1
-
-        
         return maxFruitNumber
 
 
