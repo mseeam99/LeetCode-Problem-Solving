bbf6b4641cd53b0daa2634144983475cbc3e9883
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-05-09 19:20:01 -0500

    Time: 214 ms (16.86%), Space: 26.1 MB (12.72%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -5,7 +5,8 @@ class Solution:
         rightPointer = 0
 
         hashMap = {}
-        maxLength = 0
+
+        maxFruitNumber = float("-inf")
 
         while rightPointer <= len(fruits)-1:
 
@@ -14,18 +15,27 @@ class Solution:
             else:
                 hashMap[fruits[rightPointer]] += 1
 
+            
+            while len(hashMap)>2:
+                hashMap[fruits[leftPointer]] -= 1
+                if hashMap[fruits[leftPointer]] == 0:
+                    del hashMap[fruits[leftPointer]]
+                leftPointer += 1
+
+            maxFruitNumber = max(maxFruitNumber, rightPointer-leftPointer + 1)
+
             rightPointer += 1
 
-            while len(hashMap) > 2:
-                if fruits[leftPointer] in hashMap:
-                    hashMap[fruits[leftPointer]] -= 1
-                    if hashMap[fruits[leftPointer]] == 0:
-                        del hashMap[fruits[leftPointer]] 
-                leftPointer += 1
-    
-            maxLength = max(maxLength, rightPointer-leftPointer + 1)
-    
-        return maxLength-1
-
-
         
+        return maxFruitNumber
+
+
+
+                
+
+
+
+
+
+
+
