f44611fd2760a2fa087fde7f2f2091887c3de172
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-05-13 06:26:52 -0500

    Time: 4 ms (6.26%), Space: 19.3 MB (69.47%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.py
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.py
@@ -9,7 +9,6 @@ class Solution:
 
         changes = 0
 
-
         for i in range(len(s1)):
 
             if s1[i] != s2[i]:
@@ -17,22 +16,16 @@ class Solution:
                 if changes > 2:
                     return False
             
-
             if s1[i] not in hashMapOne:
                 hashMapOne[s1[i]] = 1
             else:
                 hashMapOne[s1[i]] += 1
-
 
             if s2[i] not in hashMapTwo:
                 hashMapTwo[s2[i]] = 1
             else:
                 hashMapTwo[s2[i]] += 1
 
-            
-
         if hashMapOne == hashMapTwo:
             return True
         return False
-
-        
