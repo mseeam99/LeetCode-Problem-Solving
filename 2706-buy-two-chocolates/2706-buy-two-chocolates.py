74d5d419ca9d64de2dcc6a8b92054edbf46e9649
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-05-13 05:41:34 -0500

    Time: 2 ms (40.17%), Space: 19.4 MB (17.63%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.py
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.py
@@ -1,12 +1,9 @@
 class Solution:
     def buyChoco(self, prices: List[int], money: int) -> int:
-
         heapq.heapify(prices)
         firstMin = heapq.heappop(prices)
         secotMin = heapq.heappop(prices)
-
         val = money - (firstMin + secotMin)
         if val < 0:
             return money
         return val
-        
