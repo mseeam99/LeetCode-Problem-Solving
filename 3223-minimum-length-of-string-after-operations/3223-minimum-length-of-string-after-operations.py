bf49ebc04940b63eaedb20feb3edea4871b3849d
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-05-13 06:43:33 -0500

    Time: 132 ms (80.95%), Space: 20.7 MB (66.67%) - LeetHub

---
+++ 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minimumLength(self, s: str) -> int:
+
+        hashMap = Counter(s)
+
+        for key,val in hashMap.items():
+            if val % 2 == 0:
+                hashMap[key] = 2
+            elif val % 2 != 0:
+                hashMap[key] = 1
+        
+
+        totalLength = sum(hashMap.values())
+            
+
+
+
+        return totalLength
+
+
+        
