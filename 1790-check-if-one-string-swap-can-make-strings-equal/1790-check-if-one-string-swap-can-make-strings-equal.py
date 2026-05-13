38564774ca2c528578ec92fa76ed6e6ed3f479e6
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-05-13 06:26:41 -0500

    Time: 1 ms (14.2%), Space: 19.4 MB (6.41%) - LeetHub

---
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.py
@@ -0,0 +1,38 @@
+class Solution:
+    def areAlmostEqual(self, s1: str, s2: str) -> bool:
+
+        if s1 == s2:
+            return True
+
+        hashMapOne = {}
+        hashMapTwo = {}
+
+        changes = 0
+
+
+        for i in range(len(s1)):
+
+            if s1[i] != s2[i]:
+                changes += 1
+                if changes > 2:
+                    return False
+            
+
+            if s1[i] not in hashMapOne:
+                hashMapOne[s1[i]] = 1
+            else:
+                hashMapOne[s1[i]] += 1
+
+
+            if s2[i] not in hashMapTwo:
+                hashMapTwo[s2[i]] = 1
+            else:
+                hashMapTwo[s2[i]] += 1
+
+            
+
+        if hashMapOne == hashMapTwo:
+            return True
+        return False
+
+        
