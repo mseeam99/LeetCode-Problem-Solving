47d7bc9e2bb977ebe3eb2fdb02317680aedaeeb8
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-05-13 07:07:34 -0500

    Time: 111 ms (5.13%), Space: 23.6 MB (15.43%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.py
@@ -0,0 +1,46 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+
+
+        fiveCount = 0
+        tenCount = 0
+        twentyCount = 0
+
+        for i in range(len(bills)):
+
+            if bills[i] == 5:
+                fiveCount += 1
+
+            elif bills[i] == 10:
+                tenCount += 1
+                if fiveCount == 0:
+                    return False
+                if fiveCount > 0:
+                    fiveCount -= 1
+                
+            elif bills[i] == 20:
+                twentyCount += 1
+                if fiveCount == 0:
+                    return False
+                if tenCount >= 1 and fiveCount >= 1:
+                    tenCount -= 1 
+                    fiveCount -= 1
+                elif fiveCount >= 3:
+                    fiveCount -= 3
+                else:
+                    return False
+
+            print("Accepted: ",bills[i])
+            print("Five    : ",fiveCount)
+            print("Ten     : ",tenCount)
+            print("Twenty  : ",twentyCount)
+            print()
+            
+
+        return True
+
+
+
+
+
+        
