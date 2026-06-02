aabd3ff611f0c94142c8ba802f8c6532217ff14f
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-06-01 19:00:18 -0500

    Time: 107 ms (12.8%), Space: 19.9 MB (86.59%) - LeetHub

--- 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.py
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.py
@@ -1,25 +1,18 @@
 class Solution:
     def minimumSteps(self, s: str) -> int:
-        
-
+    
         leftPointer = 0
         rightPointer = 0
 
         swapCount = 0
 
         while rightPointer <= len(s)-1:
-
             if s[rightPointer] == "1":
                 rightPointer += 1
                 continue
-            
-
             swapCount += rightPointer - leftPointer
 
             leftPointer += 1
             rightPointer += 1
-
-
-
 
         return swapCount
