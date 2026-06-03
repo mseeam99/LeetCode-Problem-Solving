47a9a347a21c49fbc50157f491030f86673fe5e6
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-06-02 20:30:51 -0700

    Time: 20 ms (54.95%), Space: 20.1 MB (97.81%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,9 +1,40 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        goal = len(nums)-1
-        for i in range(len(nums)-1,-1,-1):
-            if (i+nums[i]) >= goal:
-                goal = i
-        return True if goal == 0 else False
+        '''
+        memo = {}
+        def recursion(index):
+            nonlocal memo
+            if index in memo:
+                return memo[index]
+            if index == len(nums)-1:
+                return True
+            for i in range(1,nums[index]+1):
+                if recursion(index+i) == True:
+                    memo[index] = True
+                    return True
+            memo[index] = False
+            return False
+        return recursion(0)
+        '''
 
+        maxJump = 0
+        for i in range(len(nums)):
+            
+            if maxJump < i:
+                return False
+
+            maxJump = max(maxJump,i+nums[i])
+
+            if maxJump >= len(nums)-1:
+                return True
+        
+        return False
+
+
+
+
+
+
+
+        
         
