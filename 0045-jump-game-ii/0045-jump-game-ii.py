01549a9aaf1beb46da48cb362ea0db675f41a885
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-06-02 23:52:59 -0700

    Time: 3 ms (89.49%), Space: 20.1 MB (66.88%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,23 +1,18 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        
-        if len(nums) == 1:
-            return 0
+
+        leftPointer = 0
+        rightPointer = 0
 
         jumps = 0
-        current_end = 0
-        farthest = 0
-        i = 0
 
-        while i < len(nums) - 1:
-            # Within the current jump range, find the farthest we can go
-            while i <= current_end:
-                farthest = max(farthest, i + nums[i])
-                i += 1
-                if farthest >= len(nums) - 1:
-                    return jumps + 1
-            # Move to the next jump range
+        while rightPointer < len(nums)-1:
+            farthest = 0
+            for i in range(leftPointer,rightPointer+1):
+                farthest = max(farthest,i+nums[i])
+            leftPointer = rightPointer + 1
+            rightPointer = farthest
             jumps += 1
-            current_end = farthest
 
+        
         return jumps
