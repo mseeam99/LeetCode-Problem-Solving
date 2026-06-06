e4c55a6204d4557975090cbba78fe1dd439bd9b7
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-06-05 19:01:37 -0700

    Time: 2338 ms (5.24%), Space: 31.2 MB (5.65%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
@@ -0,0 +1,65 @@
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+
+        hashMap = Counter(word)
+        word = list(word)
+        for i in range(len(word)):
+            word[i] = (-hashMap[word[i]],word[i])
+        heapq.heapify(word)
+       # print(word)
+
+
+        hashMap = {}
+        hashMap["1"] = "EMPTY"
+        clickedCount = 0
+
+        while len(word):
+
+            count,char = heapq.heappop(word)
+            count = count*(-1)
+
+            if char == "1" or char == "*" or char == "#" or char == "0":
+                continue
+
+            if char in hashMap:
+
+                clickedCount += hashMap[char]
+                    
+            elif char not in hashMap:
+
+
+                if len(hashMap) < 9:
+                    hashMap[char] = 1
+                    clickedCount += 1
+
+                elif len(hashMap) < 17:
+                    hashMap[char] = 2
+                    clickedCount += 2
+
+                elif len(hashMap) < 25:
+                    hashMap[char] = 3
+                    clickedCount += 3
+                
+                else:
+                    clickedCount += 4
+
+                
+
+
+        
+        return clickedCount
+
+
+
+
+
+
+
+
+
+
+
+        
+        
+
+
