91f00c0b928b9fbec0d2478290c53ebbc852f8f9
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-06-08 15:14:59 -0700

    Time: 115 ms (58.74%), Space: 24.5 MB (25.97%) - LeetHub

---
+++ 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minSwaps(self, nums: List[int]) -> int:
+        oneCount = nums.count(1)
+        data = nums+nums
+        if oneCount <= 1:
+            return 0
+        leftPointer = 0
+        rightPointer = oneCount
+        globalMinSwapNeeded = float("inf")
+        curretSwapNeeded = 0
+        for i in range(leftPointer,rightPointer):
+            if data[i] == 0:
+                curretSwapNeeded += 1
+        globalMinSwapNeeded = min(globalMinSwapNeeded,curretSwapNeeded)
+        while rightPointer < len(data):
+            if data[rightPointer] == 0:
+                curretSwapNeeded += 1    
+            if data[leftPointer] == 0:
+                curretSwapNeeded -= 1
+            leftPointer += 1
+            rightPointer += 1
+            globalMinSwapNeeded = min(globalMinSwapNeeded, curretSwapNeeded)
+        return globalMinSwapNeeded
