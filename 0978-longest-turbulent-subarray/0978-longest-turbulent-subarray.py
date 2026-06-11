5da0d0a6bb73d9600de6cdb01a56f76dec2e945a
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-06-11 14:56:28 -0700

    Time: 80 ms (9.79%), Space: 23 MB (58.87%) - LeetHub

--- 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.py
+++ 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.py
@@ -4,36 +4,45 @@ class Solution:
         if len(arr) == 1:
             return 1
 
-        maxLength = 1
+
 
         def compare(left,right):
             if left > right:    #left bigger
                 return -1
             elif left < right:  #right bigger
                 return 1
-            else:
+            elif left == right: #same
                 return 0
         
+        maxLength = 1
 
         leftPointer = 0
-        for rightPointer in range(1, len(arr)):
 
-            curr = compare(arr[rightPointer], arr[rightPointer - 1])
+        for rightPointer in range(1,len(arr)):
+
+            curr = compare(arr[rightPointer],arr[rightPointer-1])
 
             if curr == 0:
                 leftPointer = rightPointer
                 continue
 
             if rightPointer == 1:
-                maxLength = max(maxLength, 2)
+                maxLength = max(maxLength,rightPointer-leftPointer+1)
                 continue
-
-            prev = compare(arr[rightPointer - 1], arr[rightPointer - 2])
+            
+            prev = compare(arr[rightPointer-1],arr[rightPointer-2])
 
             if curr * prev == -1:
-                maxLength = max(maxLength, rightPointer - leftPointer + 1)
+                #good
+                maxLength = max(maxLength,rightPointer-leftPointer+1)
             else:
-                leftPointer = rightPointer - 1
-                maxLength = max(maxLength, 2)
+                leftPointer = rightPointer-1
+                maxLength = max(maxLength,rightPointer-leftPointer+1)
+
+
+        
+        
 
         return maxLength
+
+           
