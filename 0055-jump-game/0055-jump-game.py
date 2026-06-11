ab38a10c32eb1d37f9d5f7b7d3f20925d003dd00
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-06-11 15:32:00 -0700

    Time: 8 ms (96.12%), Space: 20.7 MB (6.33%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -19,29 +19,9 @@ class Solution:
 
         lastIndex = len(nums)-1
         for i in range(len(nums)-1,-1,-1):
-
             if i + nums[i] >= lastIndex:
                 lastIndex = i
-            
-        
-
         if lastIndex == 0:
             return True
         else:
             return False
-
-
-            
-
-            
-            
-
-
-
-        
-
-
-
-
-        
-        
