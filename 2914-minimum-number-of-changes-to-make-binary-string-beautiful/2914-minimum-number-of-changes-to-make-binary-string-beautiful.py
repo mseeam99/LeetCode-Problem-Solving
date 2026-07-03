b3c39d406ba829a898d4ac42480240d8bea8af03
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-07-02 19:25:55 -0500

    Time: 27 ms (35.16%), Space: 19.8 MB (39.84%) - LeetHub

--- 2914-minimum-number-of-changes-to-make-binary-string-beautiful/2914-minimum-number-of-changes-to-make-binary-string-beautiful.py
+++ 2914-minimum-number-of-changes-to-make-binary-string-beautiful/2914-minimum-number-of-changes-to-make-binary-string-beautiful.py
@@ -3,29 +3,15 @@ class Solution:
 
         leftPointer = 0
         rightPointer = 1
-
         changes = 0
 
         while rightPointer <= len(s)-1:
-        
-
             if s[rightPointer] != s[leftPointer]:
                 changes += 1
-
                 leftPointer += 2
                 rightPointer += 2
-
             else:
-
                 leftPointer += 2
                 rightPointer += 2
 
-            
-        
         return changes
-
-
-
-
-
-
