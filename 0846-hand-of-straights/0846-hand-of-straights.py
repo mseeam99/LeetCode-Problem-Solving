3f0c2d35d099d1c6d0b71d9f348538971b4be07b
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-07-02 22:35:49 -0500

    Time: 47 ms (20.64%), Space: 21.6 MB (10.51%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.py
+++ 0846-hand-of-straights/0846-hand-of-straights.py
@@ -6,20 +6,16 @@ class Solution:
 
         hashMap = Counter(hand)
         hand = list(hashMap.keys())
-        
         heapq.heapify(hand)
-
         visitedSet = set()
-
         duplicateStore = []
-
         lastPopped = hand[0]-1
 
         while hand:
 
             poppedVal = heapq.heappop(hand)
-
             hashMap[poppedVal] -= 1
+
             if hashMap[poppedVal] == 0:
                 del hashMap[poppedVal]
             elif hashMap[poppedVal] > 0:
@@ -30,40 +26,17 @@ class Solution:
                 lastPopped = poppedVal
             else:
                 return False
-                    
-            
+                
             if len(visitedSet) == groupSize:
-                print("visited   set: ",visitedSet)
-                print("duplicate lst: ",duplicateStore)
                 for i in range(len(duplicateStore)):
                     heapq.heappush(hand,duplicateStore[i])
                 duplicateStore.clear()
                 visitedSet.clear()
                 lastPopped = None
 
-
-        print(duplicateStore)
         if len(duplicateStore) == 0:
             return True
         else:
             return False
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-    
-        
         
