e68d4ae5e115164153c727d9a32db8f09b884fcf
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-07-05 13:22:27 -0500

    Time: 11 ms (79.76%), Space: 19.7 MB (13.03%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.py
+++ 0649-dota2-senate/0649-dota2-senate.py
@@ -1,25 +1,19 @@
 class Solution:
     def predictPartyVictory(self, senate: str) -> str:
-
         rQueue = deque([])
         dQueue = deque([])
-
         for i in range(len(senate)):
             if senate[i] == "R":
                 rQueue.append(i)
             elif senate[i] == "D":
                 dQueue.append(i)
-
         while rQueue and dQueue:
-
             rIndex = rQueue.popleft()
             dIndex = dQueue.popleft()
-
             if rIndex < dIndex:
                 rQueue.append(rIndex+len(senate))
             else:
                 dQueue.append(dIndex+len(senate))
-
         if len(rQueue) > 0:
             return "Radiant"
         elif len(dQueue) > 0:
