d04ba90fe17400ff092f347dfda6f373ca487c9c
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-07-05 14:02:02 -0500

    Time: 279 ms (5.11%), Space: 48.2 MB (5.41%) - LeetHub

---
+++ 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.py
@@ -0,0 +1,70 @@
+class Solution:
+    def maximumImportance(self, n: int, roads: List[List[int]]) -> int:
+
+        hashMap = defaultdict(list)
+        for i in range(len(roads)):
+            currentMap = roads[i]
+            start = currentMap[0]
+            end = currentMap[1]
+            hashMap[start].append(end)
+            hashMap[end].append(start)
+        assignedList = []
+        for key,val in hashMap.items():
+            length = len(val)
+            theTuple = (-length,key,val)
+            assignedList.append(theTuple)
+        heapq.heapify(assignedList)
+        assignedHashMap = {}
+        while assignedList:
+            theTuple = heapq.heappop(assignedList)
+            length,nodeNumber,connectionList = -theTuple[0], theTuple[1], theTuple[2]
+            assignedHashMap[nodeNumber] = n
+            n-=1
+    
+
+        print(assignedHashMap)
+        total = 0
+
+        for i in range(len(roads)):
+            currentMap = roads[i]
+            start = currentMap[0]
+            end = currentMap[1]
+
+            total += assignedHashMap[start]
+            total += assignedHashMap[end]
+
+
+
+
+
+
+
+        
+
+
+
+
+
+       
+
+
+
+
+        return total
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
