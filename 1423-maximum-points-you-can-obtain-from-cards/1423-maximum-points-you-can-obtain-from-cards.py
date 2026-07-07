1a50332dcde77fcee65d1881b09829a0988287b3
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-07-06 19:09:47 -0500

    Time: 15 ms (88.84%), Space: 30.8 MB (40.23%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
@@ -1,9 +1,25 @@
 class Solution:
     def maxScore(self, cardPoints: List[int], k: int) -> int:
-
+        '''
         if k == len(cardPoints):
             return sum(cardPoints)
+        leftValues = cardPoints[:k]
+        rightValues = cardPoints[-k:]
+        maxSum = 0
+        for i in range(k + 1):
+            leftSum = sum(leftValues[:i])
+            rightCount = k - i
+            rightSum = 0
+            if rightCount > 0:
+                rightSum = sum(rightValues[-rightCount:]) 
+            else:
+                rightSum = 0
+            maxSum = max(maxSum, leftSum + rightSum)
+        return maxSum
+        '''
 
+        if k == len(cardPoints):
+            return sum(cardPoints)        
         totalMaxSum = 0
         summation = sum(cardPoints[0:k])
         totalMaxSum = summation
@@ -17,3 +33,4 @@ class Solution:
             headIndex -= 1
             tailIndex -= 1
         return totalMaxSum
+        
