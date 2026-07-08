8b8243d09de37e40fa7a3bf4722ccaa771406c97
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-07-08 17:05:19 -0500

    Time: 57 ms (82.21%), Space: 23 MB (15.02%) - LeetHub

--- 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.py
+++ 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.py
@@ -3,15 +3,11 @@ class Solution:
 
         if len(s)%2 != 0:
             return False
-
         left = []
         star = []
-
         for i in range(len(s)):
-
             if locked[i] == "0":
                 star.append(i)
-
             elif s[i] == "(":
                 left.append(i)
             
@@ -23,21 +19,9 @@ class Solution:
                 else:
                     return False
         
-
-        while left and star and (left[-1] < star[-1]):
-            
+        while left and star and (left[-1] < star[-1]):    
             left.pop()
             star.pop()
-            
-
         if left:
             return False
-
         return True
-
-                
-
-
-
-            
-
