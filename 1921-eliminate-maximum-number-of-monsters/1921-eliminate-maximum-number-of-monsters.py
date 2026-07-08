f29cc9d20d8542a1afe910373d908973da498ca6
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-07-08 17:43:02 -0500

    Time: 134 ms (8.56%), Space: 33.6 MB (11.67%) - LeetHub

---
+++ 1921-eliminate-maximum-number-of-monsters/1921-eliminate-maximum-number-of-monsters.py
@@ -0,0 +1,25 @@
+class Solution:
+    def eliminateMaximum(self, dist: List[int], speed: List[int]) -> int:
+
+        arrivalTime = []
+        for i in range(len(dist)):
+            arrival = ceil(dist[i] / speed[i])
+            arrivalTime.append(arrival)
+
+        heapq.heapify(arrivalTime)
+        currTimeAsMinuite = 0
+        totalElimination = 0
+
+        while arrivalTime:
+            monsterComingAt = heapq.heappop(arrivalTime)
+            if currTimeAsMinuite >= monsterComingAt:
+                return totalElimination
+
+            currTimeAsMinuite += 1
+            totalElimination += 1
+
+        
+        return totalElimination 
+
+        
+        
