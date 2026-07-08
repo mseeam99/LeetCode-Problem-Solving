853d94c8de41675fff37d69d7e2829dcb97196b9
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-07-07 21:18:06 -0500

    Time: 7 ms (18.58%), Space: 19.2 MB (62.07%) - LeetHub

---
+++ 0763-partition-labels/0763-partition-labels.py
@@ -0,0 +1,36 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+
+        hashMap = {}
+
+        for i in range(len(s)):
+            if s[i] not in hashMap:
+                hashMap[s[i]] = i
+            elif s[i] in hashMap:
+                hashMap[s[i]] = i
+        
+        print(hashMap)
+
+        answer = []
+
+
+        leftPointer = 0
+        rightPointer = 0
+
+        farthest = hashMap[s[0]]
+
+        while rightPointer <= len(s)-1:
+
+            farthest = max(farthest,hashMap[s[rightPointer]])
+
+            if rightPointer == farthest:
+                answer.append(rightPointer - leftPointer + 1)
+                leftPointer = rightPointer + 1
+            
+            rightPointer += 1
+
+        
+        return answer
+
+
+
