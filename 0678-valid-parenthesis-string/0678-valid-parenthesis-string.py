213b5e854b5408ae6c2d76a3025093cfe3677d32
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-07-07 20:58:07 -0500

    Time: 0 ms (100%), Space: 19.4 MB (15.72%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
@@ -1,32 +1,33 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
 
-        leftParen = []
-        star = []
+        countMin = 0
+        countMax = 0
 
         for i in range(len(s)):
-            char = s[i]
-            if char == "(":
-                leftParen.append(i)
-            elif char == "*":
-                star.append(i)
-            elif char == ")":
-                if leftParen:
-                    leftParen.pop()
-                elif star:
-                    star.pop()
-                else:
-                    return False
 
-        while star and leftParen:
-            if leftParen[-1] < star[-1]:
-                star.pop()
-                leftParen.pop()
+            if s[i] == "(":
+                countMin += 1
+                countMax += 1
+
+            elif s[i] == ")":
+                countMin -= 1
+                countMax -= 1
+
             else:
+                countMin -= 1
+                countMax += 1
+
+            if countMin < 0:
+                countMin = 0
+            if countMax < 0:
                 return False
-                
-        if len(leftParen) == 0:
-            return True
-        else:
-            return False
-    
+
+        return countMin == 0
+
+        
+
+
+
+
+        
