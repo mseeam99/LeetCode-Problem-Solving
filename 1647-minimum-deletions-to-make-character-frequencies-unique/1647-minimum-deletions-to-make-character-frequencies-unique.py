842e7b5246124c7fb43963ad3574d05cc88c5aff
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-07-12 23:30:04 -0500

    Time: 79 ms (46.33%), Space: 20.2 MB (46.33%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -1,48 +1,25 @@
 class Solution:
     def minDeletions(self, s: str) -> int:
-
         element_count_hashMap = Counter(s)
-        print(element_count_hashMap)
-
         count_frequency = {}
-
         for key,val in element_count_hashMap.items():
             if val not in count_frequency:
                 count_frequency[val] = 1
             else:
                 count_frequency[val] += 1
-
-        print(count_frequency)
-
         deletions = 0
-
         for char,val in element_count_hashMap.items():
-
-            
-
             frequency = element_count_hashMap[char]
-
             while frequency > 0 and count_frequency[frequency] > 1:
-
                 count_frequency[frequency] -= 1
                 element_count_hashMap[char] -= 1
                 deletions += 1
-
                 frequency -= 1
-
                 if frequency > 0:
                     if frequency in count_frequency:
                         count_frequency[frequency] += 1
                     else:
                         count_frequency[frequency] = 1
-
                 if frequency > 0 and count_frequency[frequency] == 1:
                     break
-
         return deletions
-
-
-
-                
-            
-       
