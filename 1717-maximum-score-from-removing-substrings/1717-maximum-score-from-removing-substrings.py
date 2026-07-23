d809c78bd3baa9d25f389a3c7696838c843212ec
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-07-23 14:15:58 -0500

    Time: 260 ms (23.67%), Space: 21.2 MB (21.26%) - LeetHub

--- 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.py
+++ 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.py
@@ -1,9 +1,7 @@
 class Solution:
     def maximumGain(self, s: str, x: int, y: int) -> int:
-
         points = 0
         stack = []
-
         if x > y:
             for i in range(len(s)):
                 if stack and stack[-1] == "a" and s[i] == "b":
@@ -11,12 +9,10 @@ class Solution:
                     points += x
                 else:
                     stack.append(s[i])
-
             s = ""
             for i in range(len(stack)):
                 s += stack[i]
             stack.clear()
-
             for i in range(len(s)):
                 if stack and stack[-1] == "b" and s[i] == "a":
                     stack.pop()
@@ -30,27 +26,16 @@ class Solution:
                     points += y
                 else:
                     stack.append(s[i])
-
             s = ""
             for i in range(len(stack)):
                 s += stack[i]
             stack.clear()
-
             for i in range(len(s)):
                 if stack and stack[-1] == "a" and s[i] == "b":
                     stack.pop()
                     points += x
                 else:
                     stack.append(s[i])
-
-            
-
-
-
-
-
-        
-
         return points
 
         
