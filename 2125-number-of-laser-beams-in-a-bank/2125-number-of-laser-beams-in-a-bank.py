e103d1e9929e5ee752b442171c7f4f1fff20db17
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-07-27 16:52:28 -0500

    Time: 109 ms (13.73%), Space: 21.3 MB (28.48%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.py
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.py
@@ -1,11 +1,8 @@
 class Solution:
     def numberOfBeams(self, bank: List[str]) -> int:
-
         if len(bank) == 0 or len(bank) == 1:
             return 0
-
         array = []
-
         for row in range(len(bank)):
             count = 0
             for col in range(len(bank[row])):
@@ -13,25 +10,10 @@ class Solution:
                     count += 1
             if count != 0:
                 array.append(count)
-
-        print(array)
-
         if len(array) == 0 or len(array) == 1:
             return 0
-
-      
         totalLaser = 0
         for i in range(1,len(array)):
-            
             totalLaser += array[i] * array[i-1]
-
-
         return totalLaser
             
-
-                    
-
-
-
-
-        
