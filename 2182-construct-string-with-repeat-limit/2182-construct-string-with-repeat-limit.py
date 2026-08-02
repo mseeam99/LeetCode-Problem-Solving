ed34b3d4e066396392964d03461a6e7d269392dc
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-08-02 13:17:31 -0500

    Time: 210 ms (56.69%), Space: 21.4 MB (47.77%) - LeetHub

---
+++ 2182-construct-string-with-repeat-limit/2182-construct-string-with-repeat-limit.py
@@ -0,0 +1,41 @@
+class Solution:
+    def repeatLimitedString(self, s: str, repeatLimit: int) -> str:
+        
+        hashMap = Counter(s)
+        maxHeap = []
+        for char,count in hashMap.items():
+            maxHeap.append((-ord(char),count))
+
+        heapq.heapify(maxHeap)
+        result = []
+       
+        while maxHeap:
+            
+            char, count = heapq.heappop(maxHeap)
+            char = char*-1
+            char = chr(char)
+            
+            nextIteration = min(count,repeatLimit)
+
+            result.append(char*nextIteration)
+            
+            if count-nextIteration > 0 and maxHeap:
+                secondChar, secondCount = heapq.heappop(maxHeap)
+                secondChar = secondChar*-1
+                secondChar = chr(secondChar)
+                result.append(secondChar)
+                if secondCount-1 > 0:
+                    heapq.heappush(maxHeap,(-ord(secondChar),abs(secondCount-1)))
+                heapq.heappush(maxHeap,(-ord(char),abs(count-nextIteration)))
+
+        return "".join(result)
+
+        
+
+
+
+        
+
+
+
+        
