081fae5562838fe5184accb20df17beec2e51267
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-08-02 16:44:35 -0500

    Time: 0 ms (100%), Space: 19.2 MB (70.26%) - LeetHub

---
+++ 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.py
@@ -0,0 +1,77 @@
+class Solution:
+    def matrixScore(self, grid: List[List[int]]) -> int:
+
+        for r in range(len(grid)):
+            currentRow = grid[r]
+            #need to change
+            if currentRow[0] == 0:
+                for c in range(len(grid[r])):
+                    grid[r][c] = grid[r][c] ^ 1
+
+        for c in range(len(grid[0])):
+            theColumnList = []
+            for r in range(len(grid)):
+                theColumnList.append(grid[r][c])
+            oneCount = theColumnList.count(1)
+            zeroCount = theColumnList.count(0)
+            if zeroCount > oneCount:
+                for v in range(len(theColumnList)):
+                    theColumnList[v] = theColumnList[v] ^ 1
+            for r in range(len(grid)):
+                grid[r][c] = theColumnList[r]
+        
+        val = 0
+        for i in range(len(grid)):
+            currentRow = grid[i][::-1]
+            for j in range(len(currentRow)):
+                if currentRow[j] != 0:
+                    val += 2**j
+
+                
+
+
+
+        return val
+
+        
+                
+            
+
+            
+
+
+
+
+
+                
+            
+
+
+
+
+       
+
+
+              
+
+
+
+
+
+
+
+
+                
+        
+
+
+
+
+        
+
+
+
+
+
+
+        
