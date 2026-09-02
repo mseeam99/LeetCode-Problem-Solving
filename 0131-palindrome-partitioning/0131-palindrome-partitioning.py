7bb36d72d293e02923715843a7097f3866e2b204
Iqbal R. Seeam <112004640+mseeam99@users.noreply.github.com>
2026-09-01 20:46:15 -0500

    Time: 35 ms (89.13%), Space: 33.7 MB (62.15%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,28 +1,17 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-
         biggerArray = []
         innerArray = []
-
         def recursion(start):
-
             if start >= len(s):
                 biggerArray.append(innerArray.copy())
                 return
-            
             for end in range(start,len(s)):
-
                 subString = s[start:end+1]
-                
-
                 if subString == subString[::-1]:
                     innerArray.append(subString)
                     recursion(end+1)
                     innerArray.pop()
-
-
-
-
         recursion(0)
         return biggerArray
 
